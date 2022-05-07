59c1d99adb50b045221fe7b7b71bcc35c7995dbf
Tohir <guvalakat1603@gmail.com>
2022-05-07 18:39:17 +0500

    Bismillah

---
+++ src/main/java/uz/yeoju/yeoju_app/YeojuAppApplication.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeojuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeojuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/yeoju/yeoju_app/YeojuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeojuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
