f18a123ab638147badc5d065e44cbf0bc1484ea6
Tohir <guvalakat1603@gmail.com>
2022-05-08 14:21:43 +0500

    created user class

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/User.java
@@ -0,0 +1,76 @@
+package uz.yeoju.yeoju_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "users")
+public class User extends AbsEntity implements UserDetails {
+
+    private String name;
+
+    @Column(unique = true)
+    private String login;
+
+    private String password;
+
+    @Column(unique = true)
+    private String RFID;
+
+    @Column(unique = true)
+    private String email;
+
+    @OneToOne
+    private Gander gander;
+
+    @OneToMany
+    private Set<Role> roles;
+
+    private boolean accountNonExpired=true;
+    private boolean accountNonLocked=true;
+    private boolean credentialsNonExpired=true;
+    private boolean enabled=true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+}
