cdcf4e3f3c74b510c6308e37707128ccfc5c399a
Tohir <guvalakat1603@gmail.com>
2022-05-08 14:56:36 +0500

    create MainService interface

---
+++ src/main/java/uz/yeoju/yeoju_app/service/MainService.java
@@ -0,0 +1,21 @@
+package uz.yeoju.yeoju_app.service;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MainService<T> {
+
+//    Page<T> findAll(Pageable pageable,String searchText);
+//
+//    Page<T> findAll(Pageable pageable);
+
+    List<T> findAll();
+
+    Optional<T> findById(Long id);
+
+    T getById(Long id);
+
+    T saveOrUpdate(T t);
+
+    String deleteById(Long id);
+}
