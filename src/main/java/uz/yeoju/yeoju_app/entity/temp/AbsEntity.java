da5683211790465a4ea773ca0927d0567fcbf79d
Tohir <guvalakat1603@gmail.com>
2022-05-08 13:04:40 +0500

    created AbsEntity abstract class

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/temp/AbsEntity.java
@@ -0,0 +1,30 @@
+package uz.yeoju.yeoju_app.entity.temp;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+
+    @CreatedBy
+    private Date createdBy;
+
+    @LastModifiedBy
+    private Date updatedBy;
+}
