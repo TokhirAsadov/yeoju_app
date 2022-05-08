cc36c84d6805ca5c78480ab544efb79f8c83eb27
Tohir <guvalakat1603@gmail.com>
2022-05-08 15:10:05 +0500

    @Data

--- src/main/java/uz/yeoju/yeoju_app/entity/temp/AbsEntity.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/temp/AbsEntity.java
@@ -1,5 +1,6 @@
 package uz.yeoju.yeoju_app.entity.temp;
 
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.util.Date;
 
+@Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AbsEntity {
