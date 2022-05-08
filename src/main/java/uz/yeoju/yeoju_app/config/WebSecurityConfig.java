ebf222f7673afcfe3057b852d2712a34e5d43c71
Tohir <guvalakat1603@gmail.com>
2022-05-08 15:51:34 +0500

    override methods

--- src/main/java/uz/yeoju/yeoju_app/config/WebSecurityConfig.java
+++ src/main/java/uz/yeoju/yeoju_app/config/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,8 +15,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import uz.yeoju.yeoju_app.secret.JwtFilter;
 import uz.yeoju.yeoju_app.service.authService.AuthService;
-
-import java.util.logging.Filter;
 
 @Configuration
 @EnableWebSecurity
