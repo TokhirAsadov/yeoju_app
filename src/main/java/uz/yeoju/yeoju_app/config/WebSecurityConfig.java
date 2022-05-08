e29e7238b40d70d4a7d5696c26ad92c51b97f97c
Tohir <guvalakat1603@gmail.com>
2022-05-08 15:43:26 +0500

    WebSecurityConfig extended WebSecurityConfigurerAdapter

--- src/main/java/uz/yeoju/yeoju_app/config/WebSecurityConfig.java
+++ src/main/java/uz/yeoju/yeoju_app/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import uz.yeoju.yeoju_app.secret.JwtFilter;
@@ -17,7 +18,7 @@ import uz.yeoju.yeoju_app.service.authService.AuthService;
         prePostEnabled = true,
         jsr250Enabled = true
 )
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     AuthService authService;
@@ -31,5 +32,7 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
 
 }
