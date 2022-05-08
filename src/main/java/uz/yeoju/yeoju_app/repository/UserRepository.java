32a0df5c0f0f6bb264471b77200f41ef7aafb63d
Tohir <guvalakat1603@gmail.com>
2022-05-08 14:38:47 +0500

    added getByLogin() method. It is necessary for us to check user

--- src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    User getByLogin(String login);
 }
