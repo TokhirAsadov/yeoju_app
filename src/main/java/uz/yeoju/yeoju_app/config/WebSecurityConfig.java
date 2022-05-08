268d037ffa4ce07664fc71fedba5947e0be3e621
Tohir <guvalakat1603@gmail.com>
2022-05-08 15:41:39 +0500

    AuthService, JwtFilter autowired. And PasswordEncoder was bean

--- src/main/java/uz/yeoju/yeoju_app/config/WebSecurityConfig.java
+++ src/main/java/uz/yeoju/yeoju_app/config/WebSecurityConfig.java
@@ -1,5 +1,35 @@
 package uz.yeoju.yeoju_app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import uz.yeoju.yeoju_app.secret.JwtFilter;
+import uz.yeoju.yeoju_app.service.authService.AuthService;
 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        prePostEnabled = true,
+        jsr250Enabled = true
+)
 public class WebSecurityConfig {
+
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    public JwtFilter filter(){
+        return new JwtFilter();
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
