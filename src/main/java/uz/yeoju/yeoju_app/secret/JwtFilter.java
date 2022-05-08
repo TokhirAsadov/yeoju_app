f403e7630c0adaf3ed326f3d34c54c16957cb6d7
Tohir <guvalakat1603@gmail.com>
2022-05-08 15:19:43 +0500

    create jwt filter class. it is necessary for us to use security

---
+++ src/main/java/uz/yeoju/yeoju_app/secret/JwtFilter.java
@@ -0,0 +1,25 @@
+package uz.yeoju.yeoju_app.secret;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+import uz.yeoju.yeoju_app.repository.UserRepository;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtProvider provider;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
