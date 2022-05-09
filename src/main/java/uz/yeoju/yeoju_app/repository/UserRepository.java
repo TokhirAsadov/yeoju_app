dae3d29c8f56f2b285b3990a2e114de00d12e613
Tohir <guvalakat1603@gmail.com>
2022-05-09 14:12:15 +0500

    added new functions. They are necessary for us to get reports from database

--- src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    User getByLogin(String login);
+    User getUserByLogin(String login);
+    User getUserByEmail(String email);
+    User getUserByRFID(String RFID);
+    boolean existsUserByLoginOrEmailOrRFID(String login, String email, String RFID);
+
 }
