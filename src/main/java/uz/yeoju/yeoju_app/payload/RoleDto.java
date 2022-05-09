8f5b263650aede1c162f74002ab46906b2c8abd5
Tohir <guvalakat1603@gmail.com>
2022-05-09 11:53:37 +0500

    create RoleDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/RoleDto.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDto {
+    private Long id;
+    private String roleName;
+}
