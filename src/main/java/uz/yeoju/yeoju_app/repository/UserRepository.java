a20b5a01b8c8703d2ba3ef23caafa4566e88034e
Tohir <guvalakat1603@gmail.com>
2022-05-09 14:44:12 +0500

    added new function.It is necessary for us to get user login or email or RFID

--- src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    User getUserByLoginOrEmailOrRFID(String login, String email, String RFID);
     User getUserByLogin(String login);
     User getUserByEmail(String email);
     User getUserByRFID(String RFID);
