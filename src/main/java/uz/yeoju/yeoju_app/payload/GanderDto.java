9c0e1be3dc748d8788fe30300dd15fd349428870
Tohir <guvalakat1603@gmail.com>
2022-05-09 15:59:57 +0500

    changed return type of field

--- src/main/java/uz/yeoju/yeoju_app/payload/GanderDto.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/GanderDto.java
@@ -3,11 +3,12 @@ package uz.yeoju.yeoju_app.payload;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.enums.GanderName;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GanderDto {
     private Long id;
-    private String GanderName;
+    private GanderName ganderName;
 }
