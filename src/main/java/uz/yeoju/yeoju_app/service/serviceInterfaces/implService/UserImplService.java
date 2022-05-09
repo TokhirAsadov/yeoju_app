d0285a5c4c27b62d6e77f70d390246fd0ab8d63c
Tohir <guvalakat1603@gmail.com>
2022-05-09 11:37:47 +0500

    added new functions

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/UserImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/UserImplService.java
@@ -3,4 +3,6 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
 
 public interface UserImplService<T> extends MainService<T> {
+    void saveAndFlush(T t);
+    T getUserByLogin(String login);
 }
