a47a8a89ee3c008f63cbc2a94bb9cc6880ab337d
Tohir <guvalakat1603@gmail.com>
2022-05-09 14:23:21 +0500

    create GanderRepository

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/GanderRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.Gander;
+
+public interface GanderRepository extends JpaRepository<Gander,Long> {
+}
