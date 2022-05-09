4e2fcfa366183c8f6b9d9588fdc4ec42367659e0
Tohir <guvalakat1603@gmail.com>
2022-05-09 11:53:50 +0500

    create GanderDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/GanderDto.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GanderDto {
+    private Long id;
+    private String GanderName;
+}
