b8ea97519f48a5fdb270e90363d8d138ba86fad6
Tohir <guvalakat1603@gmail.com>
2022-05-09 11:07:04 +0500

    create user controller. It is necessary for us to work CRUD

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/UserController.java
@@ -0,0 +1,23 @@
+package uz.yeoju.yeoju_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.service.useService.UserService;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    public final UserService userService ;
+
+    @GetMapping("/getAllUsers")
+    public HttpEntity<?> getAllUsers(){
+        return ResponseEntity.ok(new ApiResponse(true,"There are",userService.findAll()));
+    }
+}
