e60c7e3a936473d0427ddef33793020120e3af45
Tohir <guvalakat1603@gmail.com>
2022-05-09 11:37:26 +0500

    create sign in dto. It is necessary for us to use login

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/SignInDto.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignInDto {
+    private String login;
+    private String password;
+}
