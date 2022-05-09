df1d8cf8f154cfc25da71bd5a2d109f39ecdb990
Tohir <guvalakat1603@gmail.com>
2022-05-09 16:10:14 +0500

    created FacultyRepository. It is necessary for us to connect entity with database

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/FacultyRepository.java
@@ -0,0 +1,11 @@
+package uz.yeoju.yeoju_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.Faculty;
+import uz.yeoju.yeoju_app.entity.Gander;
+import uz.yeoju.yeoju_app.entity.enums.GanderName;
+
+public interface FacultyRepository extends JpaRepository<Faculty,Long> {
+    Faculty getFacultyByName(String name);
+    boolean existsFacultyByName(String name);
+}
