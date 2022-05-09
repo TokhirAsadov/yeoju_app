89dfe6a327f867316c01538acbc2ee976ee69f00
Tohir <guvalakat1603@gmail.com>
2022-05-09 16:00:29 +0500

    added new functions

--- src/main/java/uz/yeoju/yeoju_app/repository/GanderRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/GanderRepository.java
@@ -2,6 +2,9 @@ package uz.yeoju.yeoju_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.Gander;
+import uz.yeoju.yeoju_app.entity.enums.GanderName;
 
 public interface GanderRepository extends JpaRepository<Gander,Long> {
+    Gander getGanderByGanderName(GanderName ganderName);
+    boolean existsGanderByGanderName(GanderName ganderName);
 }
