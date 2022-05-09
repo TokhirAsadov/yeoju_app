44c7b357cf3fb1c86d1d8856c0d325e294ae86cc
Tohir <guvalakat1603@gmail.com>
2022-05-09 16:05:32 +0500

    created faculty entity

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/Faculty.java
@@ -0,0 +1,24 @@
+package uz.yeoju.yeoju_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Faculty extends AbsEntity {
+
+    @Column(unique = true)
+    private String name;
+
+    public Faculty(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+}
