95aad5305510327bb2640c939af2a6707ccb9537
Tohir <guvalakat1603@gmail.com>
2022-05-09 16:20:14 +0500

    created FacultyImplService

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/FacultyImplService.java
@@ -0,0 +1,6 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService;
+
+import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
+
+public interface FacultyImplService<T> extends MainService<T> {
+}
