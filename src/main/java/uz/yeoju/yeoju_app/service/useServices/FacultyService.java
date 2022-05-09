5532103de35b9cd722107687a4dffd8b9b6ea9b3
Tohir <guvalakat1603@gmail.com>
2022-05-09 16:20:48 +0500

    FacultyService full done CRUD functions successfully!!!

---
+++ src/main/java/uz/yeoju/yeoju_app/service/useServices/FacultyService.java
@@ -0,0 +1,110 @@
+package uz.yeoju.yeoju_app.service.useServices;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.Faculty;
+import uz.yeoju.yeoju_app.entity.Gander;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.FacultyDto;
+import uz.yeoju.yeoju_app.payload.GanderDto;
+import uz.yeoju.yeoju_app.repository.FacultyRepository;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.GanderImplService;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class FacultyService implements GanderImplService<FacultyDto> {
+    public final FacultyRepository facultyRepository;
+    @Override
+    public ApiResponse findAll() {
+        return new ApiResponse(true,"List of all faculty", facultyRepository.findAll());
+    }
+
+    @Override
+    public ApiResponse findById(Long id) {
+        return facultyRepository
+                .findById(id)
+                .map(faculty -> new ApiResponse(true, "Fount faculty by id", faculty))
+                .orElseGet(() -> new ApiResponse(false, "Not fount faculty by id"));
+    }
+
+    @Override
+    public ApiResponse getById(Long id) {
+        Faculty faculty = facultyRepository.getById(id);
+        return new ApiResponse(true, "Fount faculty by id", faculty);
+    }
+
+    @Override
+    public ApiResponse saveOrUpdate(FacultyDto dto) {
+        if (dto.getId()==null){
+            return save(dto);
+        }
+        else {
+            return update(dto);
+        }
+    }
+
+    public ApiResponse update(FacultyDto dto){
+        Optional<Faculty> optional = facultyRepository.findById(dto.getId());
+        if (optional.isPresent()){
+            Faculty faculty = optional.get();
+            Faculty facultyByName = facultyRepository.getFacultyByName(dto.getName());
+            if (
+                    Objects.equals(facultyByName.getId(), faculty.getId())
+                    ||
+                    !facultyRepository.existsFacultyByName(dto.getName())
+            ){
+                faculty.setName(dto.getName());
+                facultyRepository.save(faculty);
+                return new ApiResponse(true,"faculty updated successfully!..");
+            }
+            else {
+                return new ApiResponse(
+                        false,
+                        "error! nor saved faculty! Please, enter other faculty name!.."
+                );
+            }
+        }
+        else{
+            return new ApiResponse(
+                    false,
+                    "error... not fount faculty"
+            );
+        }
+    }
+
+    public ApiResponse save(FacultyDto dto){
+        if (!facultyRepository.existsFacultyByName(dto.getName())){
+            Faculty faculty = generateFaculty(dto);
+            facultyRepository.saveAndFlush(faculty);
+            return new ApiResponse(true,"new faculty saved successfully!...");
+        }
+        else {
+            return new ApiResponse(
+                    false,
+                    "error! not saved faculty! Please, enter other faculty name!"
+            );
+        }
+    }
+
+    public Faculty generateFaculty(FacultyDto dto) {
+        return new Faculty(dto.getName());
+    }
+    public FacultyDto generateFacultyDto(Faculty faculty) {
+        return new FacultyDto(faculty.getId(),faculty.getName());
+    }
+
+
+    @Override
+    public ApiResponse deleteById(Long id) {
+        if (facultyRepository.findById(id).isPresent()) {
+            facultyRepository.deleteById(id);
+            return new ApiResponse(true,"faculty deleted successfully!..");
+        }
+        else {
+            return new ApiResponse(false,"error... not fount faculty!");
+        }
+    }
+}
