a6e4b8e987fb52b4ed4614fe7989efab5551060d
Tohir <guvalakat1603@gmail.com>
2022-05-10 10:05:57 +0500

    created AbsLongEntity. It is necessary for us to work long of id type

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/temp/AbsLongEntity.java
@@ -0,0 +1,43 @@
+package uz.yeoju.yeoju_app.entity.temp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class AbsLongEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @CreatedBy
+    private UUID createdBy;
+
+    @LastModifiedBy
+    private UUID updatedBy;
+
+    public AbsLongEntity(Long id) {
+        this.id = id;
+    }
+
+}
