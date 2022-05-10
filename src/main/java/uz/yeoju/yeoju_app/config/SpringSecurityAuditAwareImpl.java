5b3c399be8f9dc122efb9d26395d985349d2572f
Tohir <guvalakat1603@gmail.com>
2022-05-10 10:27:11 +0500

    full done successfully!..

--- src/main/java/uz/yeoju/yeoju_app/config/SpringSecurityAuditAwareImpl.java
+++ src/main/java/uz/yeoju/yeoju_app/config/SpringSecurityAuditAwareImpl.java
@@ -1,6 +1,9 @@
 package uz.yeoju.yeoju_app.config;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import uz.yeoju.yeoju_app.entity.User;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -8,6 +11,16 @@ import java.util.UUID;
 public class SpringSecurityAuditAwareImpl implements AuditorAware<UUID> {
     @Override
     public Optional<UUID> getCurrentAuditor() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth!=null
+                &&
+            auth.isAuthenticated()
+            &&
+            "anonymousUser".equals(""+auth.getPrincipal())
+        ){
+            return Optional.of(((User)auth.getPrincipal()).getId());
+        }
+
         return Optional.empty();
     }
 }
