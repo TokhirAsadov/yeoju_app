f01cd1cf79129e36adf2deea5071d35fea9e5dbf
Tohir <guvalakat1603@gmail.com>
2022-05-10 10:03:49 +0500

    created PhoneNumber entity

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/PhoneNumber.java
@@ -0,0 +1,30 @@
+package uz.yeoju.yeoju_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class PhoneNumber extends AbsEntity {
+
+    private String phoneNumber;
+    @ManyToOne
+    private PhoneType phoneType;
+    private boolean hasTg;
+    private boolean hasInstagram;
+    private boolean hasFacebook;
+
+    public PhoneNumber(UUID id, String phoneNumber, PhoneType phoneType) {
+        super(id);
+        this.phoneNumber = phoneNumber;
+        this.phoneType = phoneType;
+    }
+}
