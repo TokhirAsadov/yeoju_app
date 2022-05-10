384fa6622b3541bca9df6dd89d7b12bf37c3cdaa
Tohir <guvalakat1603@gmail.com>
2022-05-11 00:15:26 +0500

    create  SectionUser entity.

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/SectionUser.java
@@ -0,0 +1,63 @@
+package uz.yeoju.yeoju_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.attachment.Attachment;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.sql.Date;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class SectionUser extends AbsEntity {
+
+    @OneToOne
+    private User user;
+
+    @OneToOne
+    private Section section;
+
+    @OneToOne
+    private EducationForm educationForm;
+
+    @OneToOne
+    private EducationType educationType;
+
+    @OneToOne
+    private EducationLanguage educationLanguage;
+
+    @OneToOne
+    private Address address;
+
+    @OneToMany
+    private Set<PhoneNumber> phoneNumbers;
+
+    @OneToOne
+    private Attachment photo;
+
+    @OneToOne
+    private Attachment passportCopy;
+
+    @OneToOne
+    private Attachment obyektivka;
+
+    @OneToMany
+    private Set<Attachment> diplomas;
+
+    private Date bornYear;
+    private String citizenship;
+
+
+    private String academicDegree;
+    private String academicUnvon; //TODO---------------
+
+    //TODO---------------
+    // private Status status;
+}
