cb0e724731624f9fb5e3a5cf63023b10efb10c7d
Tohir <guvalakat1603@gmail.com>
2022-05-10 10:10:42 +0500

    created MainLongService interface

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/MainLongService.java
@@ -0,0 +1,22 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces;
+
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
+import java.util.UUID;
+
+public interface MainLongService<T> {
+
+//    Page<T> findAll(Pageable pageable,String searchText);
+//
+//    Page<T> findAll(Pageable pageable);
+
+    ApiResponse findAll();
+
+    ApiResponse findById(Long id);
+
+    ApiResponse getById(Long id);
+
+    ApiResponse saveOrUpdate(T t);
+
+    ApiResponse deleteById(Long id);
+}
