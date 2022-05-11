2d1d4fb8a0b89ddc1c7e0f26b8aa974e60776ca5
Tohir <guvalakat1603@gmail.com>
2022-05-11 11:16:11 +0500

    profissor va pedagoglarning kunlik ish rejasi

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/workingTables/WorkingDayOfPP.java
@@ -0,0 +1,36 @@
+package uz.yeoju.yeoju_app.entity.workingTables;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.Group;
+import uz.yeoju.yeoju_app.entity.Lesson;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class WorkingDayOfPP extends AbsEntity {
+
+    @ManyToOne
+    private WorkingTimeOfPP workingTimeOfPP;
+
+    @ManyToOne
+    private Lesson lesson;
+
+    @ManyToMany
+    private Set<Group> groups;
+
+    private Timestamp startedDate;
+    private Timestamp finishedDate;
+
+    private String numberOfRoom;
+    private boolean active;
+}
