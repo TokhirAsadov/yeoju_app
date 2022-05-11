d21af350b7839bc551ddb810ca6d6f79ff4a3dc8
Tohir <guvalakat1603@gmail.com>
2022-05-11 11:15:49 +0500

    profissor va pedagoglarning haftalik ish rejasi

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/workingTables/WorkingTimeOfPP.java
@@ -0,0 +1,24 @@
+package uz.yeoju.yeoju_app.entity.workingTables;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.enums.DaysOfWeek;
+import uz.yeoju.yeoju_app.entity.enums.TimeTableStatus;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class WorkingTimeOfPP extends AbsEntity {
+    @Enumerated(EnumType.STRING)
+    private TimeTableStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private DaysOfWeek daysOfWeek;
+}
