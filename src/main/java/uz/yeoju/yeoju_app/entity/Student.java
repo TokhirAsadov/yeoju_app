7a6cac910a93cee4f88f7617fd26c7938bc25d98
Tohir <guvalakat1603@gmail.com>
2022-05-11 09:27:54 +0500

    remove import file

--- src/main/java/uz/yeoju/yeoju_app/entity/Student.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/Student.java
@@ -3,7 +3,6 @@ package uz.yeoju.yeoju_app.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uz.yeoju.yeoju_app.entity.attachment.Attachment;
 import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
 import javax.persistence.Entity;
