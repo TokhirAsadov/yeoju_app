41af90294846a56ba22c681548144c0e49252fb1
Tohir <guvalakat1603@gmail.com>
2022-05-11 16:51:24 +0500

    created PhoneTypeImplService

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/PhoneTypeImplService.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService;
+
+import uz.yeoju.yeoju_app.service.serviceInterfaces.MainLongService;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
+
+public interface PhoneTypeImplService<T> extends MainLongService<T> {
+}
