28c4fbf75c9a4964b19ad16abc018832abd1154d
Tohir <guvalakat1603@gmail.com>
2022-05-11 12:33:33 +0500

    created EduLanController controller and /api/v finished/eduLan/allLanguages API created successfully!...

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/EduLanController.java
@@ -0,0 +1,25 @@
+package uz.yeoju.yeoju_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.payload.EducationLanguageDto;
+import uz.yeoju.yeoju_app.payload.FacultyDto;
+import uz.yeoju.yeoju_app.service.useServices.EduLanService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/eduLan")
+@RequiredArgsConstructor
+public class EduLanController {
+
+    public final EduLanService eduLanService;
+
+    @GetMapping("/allLanguages")
+    public HttpEntity<?> allLanguages(){
+        return ResponseEntity.ok(eduLanService.findAll());
+    }
+
+}
