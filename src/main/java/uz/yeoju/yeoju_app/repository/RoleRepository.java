6c94df4328fa80e1378cd5aabf51e2b32ee23273
Tohir <guvalakat1603@gmail.com>
2022-05-12 11:18:36 +0500

    add new findRolesBySection() method

--- src/main/java/uz/yeoju/yeoju_app/repository/RoleRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/RoleRepository.java
@@ -2,10 +2,13 @@ package uz.yeoju.yeoju_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.Role;
+import uz.yeoju.yeoju_app.entity.Section;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
     Optional<Role> findRoleByRoleName(String roleName);
+    List<Role> findRolesBySection(Section section);
 }
