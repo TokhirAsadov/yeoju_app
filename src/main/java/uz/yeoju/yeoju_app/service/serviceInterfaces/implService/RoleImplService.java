3518a569e7d4a128cd2df430589dd13a6ad27c40
Tohir <guvalakat1603@gmail.com>
2022-05-12 12:16:53 +0500

    changed name of method

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/RoleImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/RoleImplService.java
@@ -4,9 +4,7 @@ import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.SectionDto;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
 
-import java.util.List;
-
 public interface RoleImplService<T> extends MainService<T> {
-    ApiResponse findRoleByRoleName(String roleName);
+    ApiResponse findRolesByRoleName(String roleName);
     ApiResponse findRolesBySection(SectionDto sectionDto);
 }
