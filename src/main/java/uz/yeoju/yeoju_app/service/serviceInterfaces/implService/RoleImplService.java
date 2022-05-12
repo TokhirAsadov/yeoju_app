e4ffe3e2936944971f8178e5e742055976110da2
Tohir <guvalakat1603@gmail.com>
2022-05-12 11:26:40 +0500

    add new methods

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/RoleImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/RoleImplService.java
@@ -1,9 +1,12 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService;
 
+import jdk.nashorn.internal.runtime.options.Option;
+import uz.yeoju.yeoju_app.payload.SectionDto;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
 
 import java.util.List;
 
 public interface RoleImplService<T> extends MainService<T> {
-
+    Option<T> findRoleByRoleName(String roleName);
+    List<T> findRolesBySection(SectionDto sectionDto);
 }
