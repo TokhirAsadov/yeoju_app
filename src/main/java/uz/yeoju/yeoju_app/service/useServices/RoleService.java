e6c1a70d04e96881fc920a05a69499722153423b
Tohir <guvalakat1603@gmail.com>
2022-05-12 11:31:35 +0500

    start creating methods

--- src/main/java/uz/yeoju/yeoju_app/service/useServices/RoleService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/useServices/RoleService.java
@@ -1,4 +1,58 @@
 package uz.yeoju.yeoju_app.service.useServices;
 
-public class RoleService {
+import jdk.nashorn.internal.runtime.options.Option;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.RoleDto;
+import uz.yeoju.yeoju_app.payload.SectionDto;
+import uz.yeoju.yeoju_app.repository.RoleRepository;
+import uz.yeoju.yeoju_app.repository.SectionRepository;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.RoleImplService;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService implements RoleImplService<RoleDto> {
+    public final RoleRepository roleRepository;
+    public final SectionService sectionService;
+    public final SectionRepository sectionRepository;
+
+
+    @Override
+    public ApiResponse findAll() {
+        return null;
+    }
+
+    @Override
+    public ApiResponse findById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public ApiResponse getById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public ApiResponse saveOrUpdate(RoleDto roleDto) {
+        return null;
+    }
+
+    @Override
+    public ApiResponse deleteById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Option<RoleDto> findRoleByRoleName(String roleName) {
+        return null;
+    }
+
+    @Override
+    public List<RoleDto> findRolesBySection(SectionDto sectionDto) {
+        return null;
+    }
 }
