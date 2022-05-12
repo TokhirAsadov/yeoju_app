01a95a4bb420819df090de5bf43b0423e6c392bd
Tohir <guvalakat1603@gmail.com>
2022-05-12 11:26:04 +0500

    created RoleImplService interface

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/RoleImplService.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService;
+
+import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
+
+import java.util.List;
+
+public interface RoleImplService<T> extends MainService<T> {
+
+}
