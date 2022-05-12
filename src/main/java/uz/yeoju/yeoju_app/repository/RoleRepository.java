8840d0072b0ef845a8e7345552ca915587cf9b58
Tohir <guvalakat1603@gmail.com>
2022-05-12 10:45:12 +0500

    added new method

--- src/main/java/uz/yeoju/yeoju_app/repository/RoleRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/RoleRepository.java
@@ -2,7 +2,10 @@ package uz.yeoju.yeoju_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.Role;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Optional<Role> findRoleByRoleName(String roleName);
 }
