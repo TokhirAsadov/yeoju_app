c66eb8ccc9b465ba18563645094d394b5e959501
Tohir <guvalakat1603@gmail.com>
2022-05-13 11:46:51 +0500

    changed type of field from UUID to String successfully!..

--- src/main/java/uz/yeoju/yeoju_app/entity/Group.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/Group.java
@@ -8,7 +8,6 @@ import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -23,7 +22,7 @@ public class Group extends AbsEntity {
     @ManyToOne
     private Faculty faculty;
 
-    public Group(UUID id, String name) {
+    public Group(String id, String name) {
         super(id);
         this.name = name;
     }
