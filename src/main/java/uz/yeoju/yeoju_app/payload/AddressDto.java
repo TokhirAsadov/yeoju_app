9da1008d8ef8b8f114095b25daa4b863e638a60b
Tohir <guvalakat1603@gmail.com>
2022-05-13 11:38:05 +0500

    changed type of field from UUID to String successfully!..

--- src/main/java/uz/yeoju/yeoju_app/payload/AddressDto.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/AddressDto.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddressDto {
-    private UUID id;
+    private String id;
     private String country;
     private String region;
     private String district;
