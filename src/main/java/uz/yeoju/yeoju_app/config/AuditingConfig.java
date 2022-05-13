f8605aa803ac29b4f7cf3a56469211caa4d3b63b
Tohir <guvalakat1603@gmail.com>
2022-05-13 11:41:03 +0500

    changed type of field from UUID to String successfully!..

--- src/main/java/uz/yeoju/yeoju_app/config/AuditingConfig.java
+++ src/main/java/uz/yeoju/yeoju_app/config/AuditingConfig.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @EnableJpaAuditing
 public class AuditingConfig {
     @Bean
-    public AuditorAware<UUID> auditorAware(){
+    public AuditorAware<String> auditorAware(){
         return new SpringSecurityAuditAwareImpl();
     }
 }
