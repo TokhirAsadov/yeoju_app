34c278004a9f35208e004d45260047954e14a58c
Tohir <guvalakat1603@gmail.com>
2022-05-13 11:42:24 +0500

    changed type of field from UUID to String successfully!..

--- src/main/java/uz/yeoju/yeoju_app/repository/EducationLanRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/EducationLanRepository.java
@@ -3,9 +3,7 @@ package uz.yeoju.yeoju_app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.EducationLanguage;
 
-import java.util.UUID;
-
-public interface EducationLanRepository extends JpaRepository<EducationLanguage, UUID> {
+public interface EducationLanRepository extends JpaRepository<EducationLanguage, String> {
     EducationLanguage getEducationLanguageByName(String name);
     boolean existsEducationLanguageByName(String name);
 }
