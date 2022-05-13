4fa650152eea7fd25aefb174d584f172bf90c1e4
Tohir <guvalakat1603@gmail.com>
2022-05-13 11:43:00 +0500

    changed type of field from UUID to String successfully!..

--- src/main/java/uz/yeoju/yeoju_app/entity/EducationType.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/EducationType.java
@@ -7,7 +7,6 @@ import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -18,7 +17,7 @@ public class EducationType extends AbsEntity {
     @Column(unique = true)
     private String name;
 
-    public EducationType(UUID id, String name) {
+    public EducationType(String id, String name) {
         super(id);
         this.name = name;
     }
