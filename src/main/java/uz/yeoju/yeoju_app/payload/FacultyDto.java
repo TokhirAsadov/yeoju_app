4b41fa3959a9798ea1f68502bb8938c9aa26f1f6
Tohir <guvalakat1603@gmail.com>
2022-05-13 11:45:19 +0500

    changed type of field from UUID to String successfully!..

--- src/main/java/uz/yeoju/yeoju_app/payload/FacultyDto.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/FacultyDto.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class FacultyDto {
-    private UUID id;
+    private String id;
     private String name;
 
     public FacultyDto(String name) {
