0ebb1d3320ed6df130dcdaad670f6d08f18ffd52
Tohir <guvalakat1603@gmail.com>
2022-05-14 10:09:03 +0500

    changed type of variables and fields.

--- src/main/java/uz/yeoju/yeoju_app/repository/SectionRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/SectionRepository.java
@@ -3,9 +3,7 @@ package uz.yeoju.yeoju_app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.Section;
 
-import java.util.UUID;
-
-public interface SectionRepository extends JpaRepository<Section, UUID> {
+public interface SectionRepository extends JpaRepository<Section, String> {
     Section getSectionByName(String name);
     boolean existsSectionByName(String name);
 }
