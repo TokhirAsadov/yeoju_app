eb2db831afe01f699a0e87dfb8042a6f8953af37
Tohir <guvalakat1603@gmail.com>
2022-05-14 08:33:36 +0500

    changed type of field and variable from UUID to String successfully!..

--- src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
@@ -3,9 +3,7 @@ package uz.yeoju.yeoju_app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.User;
 
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, String> {
     User getUserByLoginOrEmailOrRFID(String login, String email, String RFID);
     User getUserByLogin(String login);
     User getUserByEmail(String email);
