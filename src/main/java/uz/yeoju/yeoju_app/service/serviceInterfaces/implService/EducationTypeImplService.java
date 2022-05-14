f44cae6c79fb9acc171d7d50a1e9cf00f22335bf
Tohir <guvalakat1603@gmail.com>
2022-05-14 12:29:15 +0500

    created EducationTypeImplService interface

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/EducationTypeImplService.java
@@ -0,0 +1,6 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService;
+
+import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
+
+public interface EducationTypeImplService<T> extends MainService<T> {
+}
