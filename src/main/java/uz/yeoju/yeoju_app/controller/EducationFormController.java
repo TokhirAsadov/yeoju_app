b2d5dc5318aba5d78ce9427840b79440804c8f6b
Tohir <guvalakat1603@gmail.com>
2022-05-14 16:08:12 +0500

    created EducationFormController controller and full done CRUD APIs.

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/EducationFormController.java
@@ -0,0 +1,41 @@
+package uz.yeoju.yeoju_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.payload.EducationFormDto;
+import uz.yeoju.yeoju_app.service.useServices.EducationFormService;
+
+@RestController
+@RequestMapping("/v1/eduForm")
+@RequiredArgsConstructor
+public class EducationFormController {
+
+    public final EducationFormService educationFormService;
+
+    @GetMapping("/allEduForms")
+    public HttpEntity<?> allEduForms(){
+        return ResponseEntity.ok(educationFormService.findAll());
+    }
+
+    @GetMapping("/getEduFormById/{id}")
+    public HttpEntity<?> getEduFormById(@PathVariable String id){
+        return ResponseEntity.ok(educationFormService.findById(id));
+    }
+
+    @PostMapping("/createEduForm")
+    public HttpEntity<?> createNewEduForm(@RequestBody EducationFormDto dto){
+        return ResponseEntity.status(201).body(educationFormService.saveOrUpdate(dto));
+    }
+
+    @PostMapping("/updateEduForm")
+    public HttpEntity<?> updateEduForm(@RequestBody EducationFormDto dto){
+        return ResponseEntity.status(202).body(educationFormService.saveOrUpdate(dto));
+    }
+
+    @DeleteMapping("/deleteEduForm/{id}")
+    public HttpEntity<?> deleteEduForm(@PathVariable String id){
+        return ResponseEntity.status(204).body(educationFormService.deleteById(id));
+    }
+}
