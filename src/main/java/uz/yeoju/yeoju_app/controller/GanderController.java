4233691320f8bca1a1968bbf98fcdf3305161f4e
Tohir <guvalakat1603@gmail.com>
2022-05-14 12:14:20 +0500

    created "/v1/gander/allGanders" API

--- src/main/java/uz/yeoju/yeoju_app/controller/GanderController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/GanderController.java
@@ -1,7 +1,10 @@
 package uz.yeoju.yeoju_app.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.payload.FacultyDto;
 import uz.yeoju.yeoju_app.service.useServices.GanderService;
 
 @RestController
@@ -10,6 +13,11 @@ import uz.yeoju.yeoju_app.service.useServices.GanderService;
 public class GanderController {
 
     public final GanderService ganderService;
+
+    @GetMapping("/allGanders")
+    public HttpEntity<?> allGanders(){
+        return ResponseEntity.ok(ganderService.findAll());
+    }
 
 
 }
