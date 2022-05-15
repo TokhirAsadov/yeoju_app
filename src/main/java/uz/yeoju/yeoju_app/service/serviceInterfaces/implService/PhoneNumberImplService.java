c4498bca7dac52aa0e5b1fb817965cf133bd49cd
Tohir <guvalakat1603@gmail.com>
2022-05-15 22:47:08 +0500

    changed variable type

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/PhoneNumberImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/PhoneNumberImplService.java
@@ -1,9 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService;
 
 import uz.yeoju.yeoju_app.payload.ApiResponse;
-import uz.yeoju.yeoju_app.payload.UserDto;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
 
 public interface PhoneNumberImplService<T> extends MainService<T> {
-    ApiResponse findPhoneNumbersByUser(UserDto userDto);
+    ApiResponse findPhoneNumbersByUser(String user_id);
 }
