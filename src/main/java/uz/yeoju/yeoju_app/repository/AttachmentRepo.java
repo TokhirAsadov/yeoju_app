699aeb27959edfd4c5409707b4ece152ae7c936c
Tohir <guvalakat1603@gmail.com>
2022-05-15 21:03:24 +0500

    create AttachmentRepo repository

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/AttachmentRepo.java
@@ -0,0 +1,18 @@
+package uz.yeoju.yeoju_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.attachment.Attachment;
+
+import java.util.UUID;
+
+public interface AttachmentRepo extends JpaRepository<Attachment, String> {
+
+
+    /**
+     * huddi shu originalName lik file database da bormi yoki yo'q ligini tekshiruvchi boolean qaytaradigan Jpa query.
+     * @param originalName String
+     * @return boolean
+     */
+    boolean existsByOriginalName(String originalName);
+
+}
