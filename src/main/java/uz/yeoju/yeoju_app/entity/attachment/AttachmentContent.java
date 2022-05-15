7f9115550c504128ea4ce101b8f93322e333a124
Tohir <guvalakat1603@gmail.com>
2022-05-15 22:45:17 +0500

    main change

--- src/main/java/uz/yeoju/yeoju_app/entity/attachment/AttachmentContent.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/attachment/AttachmentContent.java
@@ -1,23 +1,20 @@
 package uz.yeoju.yeoju_app.entity.attachment;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class AttachmentContent extends AbsEntity {
 
-
+    @Lob
     @Column(nullable = false)
     private byte[] bytes;
 
