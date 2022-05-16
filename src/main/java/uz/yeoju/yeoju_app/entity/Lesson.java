d79e94f44f9b13371f332432e33f125dda4b3054
Tohir <guvalakat1603@gmail.com>
2022-05-16 08:17:25 +0500

    add new fields

--- src/main/java/uz/yeoju/yeoju_app/entity/Lesson.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/Lesson.java
@@ -7,6 +7,7 @@ import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Data
 @AllArgsConstructor
@@ -16,6 +17,17 @@ public class Lesson extends AbsEntity {
 
     @Column(unique = true)
     private String name;
-
+    @ManyToOne
+    private Kafedra kafedra;
+    @ManyToOne
+    private Faculty faculty;
     private boolean active = true;
+
+    public Lesson(String id, String name, Kafedra kafedra, Faculty faculty, boolean active) {
+        super(id);
+        this.name = name;
+        this.kafedra = kafedra;
+        this.faculty = faculty;
+        this.active = active;
+    }
 }
