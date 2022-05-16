036555d239b1f86909c1872d9d48aaca1ed2eb63
Tohir <guvalakat1603@gmail.com>
2022-05-16 10:36:04 +0500

    created UserDiplomaController controller and CRUD APIs full done successfully!...

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/UserDiplomasController.java
@@ -0,0 +1,48 @@
+package uz.yeoju.yeoju_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.payload.UserDiplomaDto;
+import uz.yeoju.yeoju_app.service.useServices.UserDiplomaService;
+
+@RestController
+@RequestMapping("/v1/userDiploma")
+@RequiredArgsConstructor
+public class UserDiplomasController {
+    public final UserDiplomaService userDiplomaService;
+
+    @GetMapping("/allUserDiplomas")
+    public HttpEntity<?> getAllUserDiplomas(){
+        return ResponseEntity.ok(userDiplomaService.findAll());
+    }
+
+    @GetMapping("/getUserDiplomaById/{id}")
+    public HttpEntity<?> getUserDiplomaByIdOrUserIdOrActive(
+            @PathVariable String id,
+            @RequestParam String user_id,
+            @RequestParam boolean active)
+    {
+        return id != null ? ResponseEntity.ok(userDiplomaService.findById(id))
+                :
+                user_id != null ? ResponseEntity.ok(userDiplomaService.findUserDiplomasByUserId(user_id))
+                :
+                ResponseEntity.ok(userDiplomaService.findUserDiplomasByActive(active));
+    }
+
+    @PostMapping("/createUserDiploma")
+    public HttpEntity<?> createNewUserDiploma(@RequestBody UserDiplomaDto dto){
+        return ResponseEntity.status(201).body(userDiplomaService.saveOrUpdate(dto));
+    }
+
+    @PostMapping("/updateUserDiploma")
+    public HttpEntity<?> updateUserDiploma(@RequestBody UserDiplomaDto dto){
+        return ResponseEntity.status(202).body(userDiplomaService.saveOrUpdate(dto));
+    }
+
+    @DeleteMapping("/deleteUserDiploma/{id}")
+    public HttpEntity<?> deleteUserDiploma(@PathVariable String id){
+        return ResponseEntity.status(204).body(userDiplomaService.deleteById(id));
+    }
+}
