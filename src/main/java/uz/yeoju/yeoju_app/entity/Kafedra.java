cd5b1a68aceabe93158b159fea1635dd8fb969b1
Tohir <guvalakat1603@gmail.com>
2022-05-16 07:52:36 +0500

    created Kafedra entity

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/Kafedra.java
@@ -0,0 +1,24 @@
+package uz.yeoju.yeoju_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Kafedra extends AbsEntity {
+
+    @Column(unique = true)
+    private String name;
+
+    public Kafedra(String id, String name) {
+        super(id);
+        this.name = name;
+    }
+}
