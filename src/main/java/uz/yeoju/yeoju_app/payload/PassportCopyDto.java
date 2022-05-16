c46d84a626c1b3cd04e4257288e6fc2aacd15954
Tohir <guvalakat1603@gmail.com>
2022-05-16 09:42:28 +0500

    created PassportCopyDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/PassportCopyDto.java
@@ -0,0 +1,16 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.attachment.Attachment;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PassportCopyDto {
+    private String id;
+    private UserDto userDto;
+    private Attachment attachment;
+    private boolean active;
+}
