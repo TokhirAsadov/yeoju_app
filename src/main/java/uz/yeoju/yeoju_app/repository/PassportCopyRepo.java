6501130f9fb659619c7d2bfa39e206c478d39039
Tohir <guvalakat1603@gmail.com>
2022-05-16 09:42:41 +0500

    created PassportCopyRepo

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/PassportCopyRepo.java
@@ -0,0 +1,14 @@
+package uz.yeoju.yeoju_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.attachment.PassportCopy;
+
+import java.util.List;
+
+public interface PassportCopyRepo extends JpaRepository<PassportCopy, String> {
+    List<PassportCopy> findPassportCopiesByUserId(String user_id);
+    List<PassportCopy> findPassportCopiesByActive(boolean active);
+    PassportCopy findPassportCopyByActive(boolean active);
+    boolean existsPassportCopyByUserId(String user_id);
+    boolean existsPassportCopyByActive(boolean active);
+}
