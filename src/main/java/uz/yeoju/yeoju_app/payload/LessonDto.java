7de695d495fa30183b57d7845e9fa2d4a9407596
Tohir <guvalakat1603@gmail.com>
2022-05-16 08:17:45 +0500

    created LessonDto class

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/LessonDto.java
@@ -0,0 +1,23 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LessonDto {
+    private String id;
+    private String name;
+    private KafedraDto kafedraDto;
+    private FacultyDto facultyDto;
+    private boolean active=true;
+
+    public LessonDto(String name, KafedraDto kafedraDto, FacultyDto facultyDto, boolean active) {
+        this.name = name;
+        this.kafedraDto = kafedraDto;
+        this.facultyDto = facultyDto;
+        this.active = active;
+    }
+}
