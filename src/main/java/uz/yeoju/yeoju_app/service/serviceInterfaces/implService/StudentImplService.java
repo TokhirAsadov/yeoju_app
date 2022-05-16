0e1d3ff3415dbd4032922d62bc84471e07dc9438
Tohir <guvalakat1603@gmail.com>
2022-05-16 11:14:47 +0500

    created StudentImplService

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/StudentImplService.java
@@ -0,0 +1,17 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService;
+
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
+
+import java.sql.Timestamp;
+public interface StudentImplService<T> extends MainService<T> {
+    ApiResponse findStudentByUserId(String user_id);
+    ApiResponse findStudentsByGroupId(String group_id);
+    ApiResponse findStudentsByEducationFormId(String educationForm_id);
+    ApiResponse findStudentsByEducationTypeId(String educationType_id);
+    ApiResponse findStudentsByEducationLanguageId(String educationLanguage_id);
+    ApiResponse findStudentByPassportSerial(String passportSerial);
+    ApiResponse findStudentsByBornYear(Timestamp bornYear);
+    ApiResponse findStudentsByEnrollmentYear(Timestamp enrollmentYear);
+    ApiResponse findStudentsByCitizenship(String citizenship);
+}
