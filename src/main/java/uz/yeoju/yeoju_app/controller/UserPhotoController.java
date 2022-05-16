81986816b9270ce7b97e27c36ccbd622c635a48a
Tohir <guvalakat1603@gmail.com>
2022-05-16 10:13:40 +0500

    created UserPhotoController controller and CRUD APIs full done successfully!...

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/UserPhotoController.java
@@ -0,0 +1,48 @@
+package uz.yeoju.yeoju_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.payload.UserPhotoDto;
+import uz.yeoju.yeoju_app.service.useServices.UserPhotoService;
+
+@RestController
+@RequestMapping("/v1/userPhoto")
+@RequiredArgsConstructor
+public class UserPhotoController {
+    public final UserPhotoService userPhotoService;
+
+    @GetMapping("/allUserPhotos")
+    public HttpEntity<?> getAllUserPhotos(){
+        return ResponseEntity.ok(userPhotoService.findAll());
+    }
+
+    @GetMapping("/getUserPhotoById/{id}")
+    public HttpEntity<?> getUserPhotoByIdOrUserIdOrActive(
+            @PathVariable String id,
+            @RequestParam String user_id,
+            @RequestParam boolean active)
+    {
+        return id != null ? ResponseEntity.ok(userPhotoService.findById(id))
+                :
+                user_id != null ? ResponseEntity.ok(userPhotoService.findUserPhotosByUserId(user_id))
+                :
+                ResponseEntity.ok(userPhotoService.findUserPhotosByActive(active));
+    }
+
+    @PostMapping("/createUserPhoto")
+    public HttpEntity<?> createNewUserPhoto(@RequestBody UserPhotoDto dto){
+        return ResponseEntity.status(201).body(userPhotoService.saveOrUpdate(dto));
+    }
+
+    @PostMapping("/updateUserPhoto")
+    public HttpEntity<?> updateUserPhoto(@RequestBody UserPhotoDto dto){
+        return ResponseEntity.status(202).body(userPhotoService.saveOrUpdate(dto));
+    }
+
+    @DeleteMapping("/deleteUserPhoto/{id}")
+    public HttpEntity<?> deleteUserPhoto(@PathVariable String id){
+        return ResponseEntity.status(204).body(userPhotoService.deleteById(id));
+    }
+}
