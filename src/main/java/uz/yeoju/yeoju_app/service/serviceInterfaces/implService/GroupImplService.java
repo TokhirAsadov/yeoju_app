f26e0b33f7718eaa8662223eaac0d95fc82da65b
Tohir <guvalakat1603@gmail.com>
2022-05-16 12:21:43 +0500

    created GroupImplService interface

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/GroupImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/GroupImplService.java
@@ -1,6 +1,10 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.MainService;
 
 public interface GroupImplService<T> extends MainService<T> {
+    ApiResponse findGroupByName(String name);
+    ApiResponse findGroupsByLevel(Integer level);
+    ApiResponse findGroupsByFacultyId(String faculty_id);
 }
