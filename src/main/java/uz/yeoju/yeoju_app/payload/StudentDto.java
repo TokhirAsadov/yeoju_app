39f22182f851f8892daeba72f660321263b72d79
Tohir <guvalakat1603@gmail.com>
2022-05-16 11:10:08 +0500

    created StudentDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/StudentDto.java
@@ -0,0 +1,29 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDto{
+
+    private String id;
+    private UserDto userDto;
+    private GroupDto groupDto;
+    private EducationFormDto eduFormDto;
+    private EducationTypeDto eduTypeDto;
+    private EducationLanguageDto eduLanDto;
+
+//    @ManyToOne
+//    private TeachStatus teachStatus;    // private Status status;
+
+    private String passportSerial;
+    private Timestamp bornYear;
+    private String description;
+    private Timestamp enrollmentYear;// uqishga kirgan yili
+    private String citizenship;//fuqaroligi
+
+}
