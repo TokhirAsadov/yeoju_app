e1a8a0118c33944618d9d814d7f0207b9f9a814f
Tohir <guvalakat1603@gmail.com>
2022-05-16 09:50:46 +0500

    created PersonalSheetController controller and CRUD APIs full done successfully!...

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/PersonalSheetController.java
@@ -0,0 +1,48 @@
+package uz.yeoju.yeoju_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.payload.ObyektivkaDto;
+import uz.yeoju.yeoju_app.service.useServices.ObyektivkaService;
+
+@RestController
+@RequestMapping("/v1/personalSheet")
+@RequiredArgsConstructor
+public class PersonalSheetController {
+    public final ObyektivkaService obyektivkaService;
+
+    @GetMapping("/allPersonalSheets")
+    public HttpEntity<?> getAllPersonalSheets(){
+        return ResponseEntity.ok(obyektivkaService.findAll());
+    }
+
+    @GetMapping("/getPersonalSheetById/{id}")
+    public HttpEntity<?> getPersonalSheetById(
+            @PathVariable String id,
+            @RequestParam String user_id,
+            @RequestParam boolean active)
+    {
+        return id!=null ? ResponseEntity.ok(obyektivkaService.findById(id))
+                :
+                user_id!=null ? ResponseEntity.ok(obyektivkaService.findObyektivkasByUserId(id))
+                :
+                ResponseEntity.ok(obyektivkaService.findObyektivkasByActive(active));
+    }
+
+    @PostMapping("/createPersonalSheet")
+    public HttpEntity<?> createNewPersonalSheet(@RequestBody ObyektivkaDto dto){
+        return ResponseEntity.status(201).body(obyektivkaService.saveOrUpdate(dto));
+    }
+
+    @PostMapping("/updatePersonalSheet")
+    public HttpEntity<?> updatePersonalSheet(@RequestBody ObyektivkaDto dto){
+        return ResponseEntity.status(202).body(obyektivkaService.saveOrUpdate(dto));
+    }
+
+    @DeleteMapping("/deletePersonalSheet/{id}")
+    public HttpEntity<?> deletePersonalSheet(@PathVariable String id){
+        return ResponseEntity.status(204).body(obyektivkaService.deleteById(id));
+    }
+}
