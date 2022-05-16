aaeabc41ca7eb7a2ec2506686b3a31b521c29c49
Tohir <guvalakat1603@gmail.com>
2022-05-16 10:35:25 +0500

    create UserDiplomaDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/UserDiplomaDto.java
@@ -0,0 +1,19 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.attachment.Attachment;
+
+import java.util.List;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDiplomaDto {
+    private String id;
+    private UserDto userDto;
+    private Set<Attachment> attachmentList;
+    private boolean active;
+}
