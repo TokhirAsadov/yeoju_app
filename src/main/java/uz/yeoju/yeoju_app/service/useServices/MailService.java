4772da099ba2c285d45cc9f238b0840f7b0f46bc
Tohir <guvalakat1603@gmail.com>
2022-05-18 10:11:15 +0500

    create MailService service

---
+++ src/main/java/uz/yeoju/yeoju_app/service/useServices/MailService.java
@@ -0,0 +1,52 @@
+package uz.yeoju.yeoju_app.service.useServices;
+
+
+import freemarker.template.Template;
+import lombok.RequiredArgsConstructor;
+import freemarker.template.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.repository.UserRepository;
+
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    public final JavaMailSender sender;
+
+    public final UserRepository userRepo;
+
+    public final Configuration configuration;
+
+
+    public ApiResponse sendHTML(User user) {
+        try {
+            Map<String,Object> model=new HashMap<>();
+            model.put("user",user);
+            MimeMessage mimeMessage=sender.createMimeMessage();
+            MimeMessageHelper helper=new MimeMessageHelper(mimeMessage,MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+            helper.addAttachment("logo.png", new ClassPathResource("logo.png"));
+            Template template=configuration.getTemplate("email-template.ftl");
+            String html= FreeMarkerTemplateUtils.processTemplateIntoString(template,model);
+            helper.setTo(user.getEmail());
+            helper.setFrom("asadovtohir2002@gmail.com","YTIT");
+            helper.setSubject("YTIT");
+            helper.setText(html,true);
+            sender.send(mimeMessage);
+            return new ApiResponse(true,"Sended");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ApiResponse(false,"Error");
+    }
+}
