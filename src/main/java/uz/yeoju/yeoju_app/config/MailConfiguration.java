37390f7061d08cd4f7d98e39ca90fbfbdc9d2fff
Tohir <guvalakat1603@gmail.com>
2022-05-18 10:10:33 +0500

    create MailConfiguration configuration

---
+++ src/main/java/uz/yeoju/yeoju_app/config/MailConfiguration.java
@@ -0,0 +1,28 @@
+package uz.yeoju.yeoju_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+    @Bean
+    public JavaMailSender sender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("asadovtohir2002@gmail.com");
+        mailSender.setPassword("asadov20tohir#02");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        return mailSender;
+    }
+}
