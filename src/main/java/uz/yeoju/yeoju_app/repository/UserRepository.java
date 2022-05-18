0a8f9a0ebf06a23a9fdb97d8269085bdaf7b7a00
Tohir <guvalakat1603@gmail.com>
2022-05-18 10:12:18 +0500

    changed name of method

--- src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
@@ -6,7 +6,7 @@ import uz.yeoju.yeoju_app.entity.User;
 public interface UserRepository extends JpaRepository<User, String> {
     User getUserByLoginOrEmailOrRFID(String login, String email, String RFID);
     User getUserByLogin(String login);
-    User getUserByEmail(String email);
+    User findUserByEmail(String email);
     User getUserByRFID(String RFID);
     boolean existsUserByLoginOrEmailOrRFID(String login, String email, String RFID);
 
