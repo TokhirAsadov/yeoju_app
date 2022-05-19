1d00ba4b0c2344e5cc32334220f29de1b9f8fb34
Tohir <guvalakat1603@gmail.com>
2022-05-19 11:06:39 +0500

    new API created

--- src/main/java/uz/yeoju/yeoju_app/controller/AuthController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/AuthController.java
@@ -3,12 +3,12 @@ package uz.yeoju.yeoju_app.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.ResToken;
 import uz.yeoju.yeoju_app.payload.SignInDto;
+import uz.yeoju.yeoju_app.payload.UserDto;
+import uz.yeoju.yeoju_app.service.useServices.MailService;
 import uz.yeoju.yeoju_app.service.useServices.UserService;
 
 @RestController
@@ -17,11 +17,20 @@ import uz.yeoju.yeoju_app.service.useServices.UserService;
 public class AuthController {
 
     public final UserService userService;
-
+    public final MailService mailService;
     @PostMapping("/login")
     public HttpEntity<?> login(@RequestBody SignInDto signInDto){
         ResToken resToken = userService.login(signInDto);
         return ResponseEntity.ok(resToken);
     }
 
+    @GetMapping("/email/{email}")
+    public HttpEntity<?> email(@PathVariable String email){
+        ApiResponse userByEmail = userService.getUserByEmail(email);
+        if (userByEmail.isSuccess()){
+            mailService.sendHTML(userService.generateUser((UserDto) userByEmail.getObj()));
+            return ResponseEntity.ok(new ApiResponse(true,"Sending login and password. Check your email!.."));
+        }
+        return ResponseEntity.ok(userByEmail);
+    }
 }
