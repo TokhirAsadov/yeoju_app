85bc63098ec9174ce51572d5b836540f5a3d9bf2
Tohir <guvalakat1603@gmail.com>
2022-05-19 11:02:10 +0500

    DistrictName enum

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/enums/DistrictName.java
@@ -0,0 +1,180 @@
+package uz.yeoju.yeoju_app.entity.enums;
+
+public enum DistrictName {
+
+    //ANDIJON VILOYATIGA TEGISHLI TUMANLAR
+    ANDIJON_SHAHRI("Andijon shahri",RegionName.ANDIJON),
+    XONOBOD_SHAHRI("Xonobod shahri",RegionName.ANDIJON),
+    ANDIJON_TUMANI("Andijon tumani",RegionName.ANDIJON),
+    ASAKA_TUMANI("Asaka tumani",RegionName.ANDIJON),
+    BOSTON_TUMANI("Bo'ston tumani",RegionName.ANDIJON),
+    BULOQBOSHI_TUMANI("Buloqboshi tumani",RegionName.ANDIJON),
+    IZBOSKAN_TUMANI("Izboskan tumani",RegionName.ANDIJON),
+    JALAQUDUQ_TUMANI("Jalaquduq tumani",RegionName.ANDIJON),
+    MARHAMAT_TUMANI("Marhamat tumani",RegionName.ANDIJON),
+    OLTINKOL_TUMANI("Oltinko'l tumani",RegionName.ANDIJON),
+    PAXTAOBOD_TUMANI("Paxtaobod tumani",RegionName.ANDIJON),
+    SHAXRIHON_TUMANI("Shaxrihon tumani",RegionName.ANDIJON),
+    ULUGNOR_TUMANI("Ulug'nor tumani",RegionName.ANDIJON),
+    XOJAOBOD_TUMANI("Xo'jaobod tumani",RegionName.ANDIJON),
+    QORGONTEPA_TUMANI("Qo'rg'ontepa tumani",RegionName.ANDIJON),
+    BALICHI_TUMANI("Baliqchi tumani",RegionName.ANDIJON),
+
+    //FARG'ONA VILOYATIGA TEGISHLI TUMANLAR
+    BESHARIQ_SHAHRI("Beshariq shahri",RegionName.FARGONA),
+    MARGILON_SHAHRI("Marg'ilon shahri",RegionName.FARGONA),
+    RISHTON_SHAHRI("Rishton shahri",RegionName.FARGONA),
+    FARGONA_SHAHRI("Farg'ona shahri",RegionName.FARGONA),
+    YAYPAN_SHAHRI("Yaypan shahri",RegionName.FARGONA),
+    QUVA_SHAHRI("Quva shahri",RegionName.FARGONA),
+    QUVASOY_SHAHRI("Quvasoy shahri",RegionName.FARGONA),
+    QOQON_SHAHRI("Qo'qon shahri",RegionName.FARGONA),
+    HAMZA_SHAHRI("Hamza shahri",RegionName.FARGONA),
+    BOGDOD_TUMANI("Bog'dod tumani",RegionName.FARGONA),
+    BESHARIQ_TUMANI("Beshariq tumani",RegionName.FARGONA),
+    BUVAYDA_TUMANI("Buvayda tumani",RegionName.FARGONA),
+    DANGARA_TUMANI("Dang'ara tumani",RegionName.FARGONA),
+    FARGONA_TUMANI("Farg'ona tumani",RegionName.FARGONA),
+    FURQAT_TUMANI("Furqat tumani",RegionName.FARGONA),
+    OLTIARIQ_TUMANI("Oltiariq tumani",RegionName.FARGONA),
+    OZBEKISTON_TUMANI("O'zbekiston tumani",RegionName.FARGONA),
+    QOSHTEPA_TUMANI("Qo'shtepa tumani",RegionName.FARGONA),
+    QUVA_TUMANI("Quva tumani",RegionName.FARGONA),
+    RISHTON_TUMANI("Rishton tumani",RegionName.FARGONA),
+    SOX_TUMANI("So'x tumani",RegionName.FARGONA),
+    TOSHLOQ_TUMANI("Toshloq tumani",RegionName.FARGONA),
+    UCHKOPRIK_TUMANI("Uchko'prik tumani",RegionName.FARGONA),
+    YOZYOVON_TUMANI("Yozyovon tumani",RegionName.FARGONA),
+
+    //NAMANGAN VILOYATIGA TEGISHLI TUMANLAR
+    NAMANGAN_SHAHRI("Namangan shahri",RegionName.NAMANGAN),
+    MINGBULOQ_TUMANI("Mingbuloq tumani",RegionName.NAMANGAN),
+    KOSONSOY_TUMANI("Kosonsoy tumani",RegionName.NAMANGAN),
+    NAMANGAN_TUMANI("Namangan tumani",RegionName.NAMANGAN),
+    NORIN_TUMANI("Norin tumani",RegionName.NAMANGAN),
+    POP_TUMANI("Pop tumani",RegionName.NAMANGAN),
+    TORAQORGON_TUMANI("To'raqo'rg'on tumani",RegionName.NAMANGAN),
+    UYCHI_TUMANI("Uychi tumani",RegionName.NAMANGAN),
+    UCHQORGON_TUMANI("Uchqo'rg'on tumani",RegionName.NAMANGAN),
+    CHORTOQ_TUMANI("Chortoq tumani",RegionName.NAMANGAN),
+    CHUST_TUMANI("Chust tumani",RegionName.NAMANGAN),
+    YANGIQORGON_TUMANI("Yangiqo'rg'on tumani",RegionName.NAMANGAN),
+
+    //TOSHKENT VILOYATIGA TEGISHLI TUMANLAR
+    BEKOBOD_TUMANI("Bekobod tumani",RegionName.TOSHKENT_VILOYATI),
+    BOSTONLIQ_TUMANI("Bo'stonliq tumani",RegionName.TOSHKENT_VILOYATI),
+    BOKA_TUMANI("Bo'ka tumani",RegionName.TOSHKENT_VILOYATI),
+    CHINOZ_TUMANI("Chinoz tumani",RegionName.TOSHKENT_VILOYATI),
+    QIBRAY_TUMANI("Qibray tumani",RegionName.TOSHKENT_VILOYATI),
+    OHANGARON_TUMANI("Ohangaron tumani",RegionName.TOSHKENT_VILOYATI),
+    OQQORGON_TUMANI("Oqqo'rg'on tumani",RegionName.TOSHKENT_VILOYATI),
+    PARKENT_TUMANI("Parkent tumani",RegionName.TOSHKENT_VILOYATI),
+    PISKENT_TUMANI("Piskent tumani",RegionName.TOSHKENT_VILOYATI),
+    QUYI_CHIRCHIQ_TUMANI("Quyi chirchiq tumani",RegionName.TOSHKENT_VILOYATI),
+    ORTA_CHIRCHIQ_TUMANI("O'rta chirchiq tumani",RegionName.TOSHKENT_VILOYATI),
+    YANGIYOL_TUMANI("Yangiyo'l tumani",RegionName.TOSHKENT_VILOYATI),
+    YUQORI_CHIRCHIQ_TUMANI("Yuqori chirchiq tumani",RegionName.TOSHKENT_VILOYATI),
+    ZANGIOTA_TUMANI("zangiota tumani",RegionName.TOSHKENT_VILOYATI),
+
+    //TOSHKENT SHAHRIGA TEGISHLI TUMANLAR
+    BEKTEMIR_TUMANI("Bektemir tumani",RegionName.TOSHKENT_SHAHRI),
+    CHILONZOR_TUMANI("Chilonzor tumani",RegionName.TOSHKENT_SHAHRI),
+    HAMZA_TUMANI("Hamza tumani",RegionName.TOSHKENT_SHAHRI),
+    MIROBOD_TUMANI("Mirobod tumani",RegionName.TOSHKENT_SHAHRI),
+    MIRZO_ULUGBEK_TUMANI("Mirzo ulug'bek tumani",RegionName.TOSHKENT_SHAHRI),
+    SERGELI_TUMANI("Sergeli tumani",RegionName.TOSHKENT_SHAHRI),
+    SHAYXONTOHUR_TUMANI("Shayxontohur tumani",RegionName.TOSHKENT_SHAHRI),
+    OLMAZOR_TUMANI("Olmazor tumani",RegionName.TOSHKENT_SHAHRI),
+    UCHTEPA_TUMANI("Uchtepa tumani",RegionName.TOSHKENT_SHAHRI),
+    YAKKASAROY_TUMANI("Yakkasaroy tumani",RegionName.TOSHKENT_SHAHRI),
+    YUNUSOBOD_TUMANI("Yunusobod tumani",RegionName.TOSHKENT_SHAHRI),
+    YANGIHAYOT_TUMANI("Yangihayot tumani",RegionName.TOSHKENT_SHAHRI),
+
+    //SIRDARYO VILOYATIGA TEGISHLI TUMANLAR
+    OQOLTIN_TUMANI("Oqoltin tumani",RegionName.SIRDARYO),
+    BOYOVUT_TUMANI("Boyovut tumani",RegionName.SIRDARYO),
+    GULISTON_TUMANI("Guliston tumani",RegionName.SIRDARYO),
+    XOVOS_TUMANI("Xovos tumani",RegionName.SIRDARYO),
+    MIRZAOBOD_TUMANI("Mirzaobod tumani",RegionName.SIRDARYO),
+    SAYXUNOBOD_TUMANI("Sayxunobod tumani",RegionName.SIRDARYO),
+    SARDOBA_TUMANI("Sardoba tumani",RegionName.SIRDARYO),
+    SIRDARYO_TUMANI("Sirdaryo tumani",RegionName.SIRDARYO),
+    YANGIYER_SHAHRI("Yangiyer shahri",RegionName.SIRDARYO),
+    GULISTON_SHAHRI("Guliston shahri",RegionName.SIRDARYO),
+    SHIRIN_SHAHRI("Shirin shahri",RegionName.SIRDARYO),
+
+    //JIZZAX VILOYATIGA TEGISHLI TUMANLAR
+    ARNASOY_TUMANI("Arnasoy tumani",RegionName.JIZZAX),
+    BAXMAL_TUMANI("Baxmal tumani",RegionName.JIZZAX),
+    DOSTLIK_TUMANI("Do'stlik tumani",RegionName.JIZZAX),
+    FORISH_TUMANI("Forish tumani",RegionName.JIZZAX),
+    GALLAOROL_TUMANI("G'allaorol tumani",RegionName.JIZZAX),
+    SHAROF_RASHIDOV_TUMANI("Sharof Rashidov tumani",RegionName.JIZZAX),
+    MIRZACHOL_TUMANI("Mirzacho'l tumani",RegionName.JIZZAX),
+    PAXTAKOR_TUMANI("Paxtakor tumani",RegionName.JIZZAX),
+    YANGIOBOD_TUMANI("Yangiobod tumani",RegionName.JIZZAX),
+    ZOMIN_TUMANI("Zomin tumani",RegionName.JIZZAX),
+    ZAFAROBOD_TUMANI("Zafarobod tumani",RegionName.JIZZAX),
+    ZARBDOR_TUMANI("Zarbdor tumani",RegionName.JIZZAX),
+    JIZZAX_SHAHRI("Jizzax shahri",RegionName.JIZZAX),
+
+    //SAMARQAND VILOYATIGA TEGISHLI TUMANLAR
+    BULUNGUR_TUMANI("Bulung'ur tumani",RegionName.SAMARQAND),
+    ISHTIXON_TUMANI("Ishtixon tumani",RegionName.SAMARQAND),
+    JOMBOY_TUMANI("Jomboy tumani",RegionName.SAMARQAND),
+    KATTAQORGON_TUMANI("Kattaqo'rg'on tumani",RegionName.SAMARQAND),
+    QOSHRABOT_TUMANI("Qo'shrabot tumani",RegionName.SAMARQAND),
+    NARPAY_TUMANI("Narpay tumani",RegionName.SAMARQAND),
+    NUROBOD_TUMANI("Nurobod tumani",RegionName.SAMARQAND),
+    OQDARYO_TUMANI("Oqdaryo tumani",RegionName.SAMARQAND),
+    PAXTACHI_TUMANI("Paxtachi tumani",RegionName.SAMARQAND),
+    PAYARIQ_TUMANI("Payariq tumani",RegionName.SAMARQAND),
+    PASTARGOM_TUMANI("Pastarg'om tumani",RegionName.SAMARQAND),
+    SAMARQAND_TUMANI("Samarqand tumani",RegionName.SAMARQAND),
+    TOYLOQ_TUMANI("Toyloq tumani",RegionName.SAMARQAND),
+    URGUT_TUMANI("Urgut tumani",RegionName.SAMARQAND),
+    SAMARQAND_SHAHRI("Samarqand shahri",RegionName.SAMARQAND),
+    KATTAQORGON_SHAHRI("Kattaqo'rg'on shahri",RegionName.SAMARQAND),
+
+    //NAVOIY VILOYTIGA TEGISHLI TUMANLAR
+    NUROTA_TUMANI("Nurota tumani",RegionName.NAVOIY),
+    NAVBAHOR_TUMANI("Navbahor tumani",RegionName.NAVOIY),
+    XATIRCHI_TUMANI("Xatirchi tumani",RegionName.NAVOIY),
+    KARMANA_TUMANI("Karmana tumani",RegionName.NAVOIY),
+    QIZILTEPA_TUMANI("Qiziltepa tumani",RegionName.NAVOIY),
+    KONIMEX_TUMANI("Konimex tumani",RegionName.NAVOIY),
+    UCHQUDUQ_TUMANI("Uchquduq tumani",RegionName.NAVOIY),
+    TOMDI_TUMANI("Tomdi tumani",RegionName.NAVOIY),
+    ZARAFSHON_SHAHRI("Zarafshon shahri",RegionName.NAVOIY),
+    NAVOIY_SHAHRI("Navoiy shahri",RegionName.NAVOIY),
+    GAZGON_SHAHRI("G'azg'on shahri",RegionName.NAVOIY),
+
+    //QASHQADARYO VILOYATIGA TEGISHLI TUMANLAR
+    CHIROQCHI_TUMANI("Chiroqchi tumani",RegionName.QASHQADARYO),
+    DEHQONOBOD_TUMANI("Dehqonobod tumani",RegionName.QASHQADARYO),
+    GUZOR_TUMANI("G'uzor tumani",RegionName.QASHQADARYO),
+    QAMASHI_TUMANI("Qamashi tumani",RegionName.QASHQADARYO),
+    QARSHI_TUMANI("Qarshi tumani",RegionName.QASHQADARYO),
+    KOSON_TUMANI("Koson tumani",RegionName.QASHQADARYO),
+    KASBI_TUMANI("Kasbi tumani",RegionName.QASHQADARYO),
+    KITOB_TUMANI("Kitob tumani",RegionName.QASHQADARYO),
+    MIRISHKOR_TUMANI("Mirishkor tumani",RegionName.QASHQADARYO),
+    MUBORAK_TUMANI("Muborak tumani",RegionName.QASHQADARYO),
+    NISHON_TUMANI("Nishon tumani",RegionName.QASHQADARYO),
+    SHAHRISABZ_TUMANI("Shahrisabz tumani",RegionName.QASHQADARYO),
+    YAKKABOG_TUMANI("Yakkabog' tumani",RegionName.QASHQADARYO),
+    KOKDALA_TUMANI("Ko'kdala tumani",RegionName.QASHQADARYO),
+    QARSHI_SHAHRI("Qarshi shahri",RegionName.QASHQADARYO),
+    SHAHRISABZ_SHAHRI("Shahrisabz shahri",RegionName.QASHQADARYO);
+
+
+
+    private String originalName;
+    private RegionName regionName;
+
+    DistrictName(String originalName, RegionName regionName) {
+        this.originalName = originalName;
+        this.regionName = regionName;
+    }
+}
+
