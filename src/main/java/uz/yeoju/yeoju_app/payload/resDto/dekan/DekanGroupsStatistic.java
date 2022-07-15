d29f6c1d8abfb404b2efd94e86b0646c53db723e
Tohir <guvalakat1603@gmail.com>
2022-07-15 11:49:50 +0500

    add restDto for get data from database

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/dekan/DekanGroupsStatistic.java
@@ -0,0 +1,8 @@
+package uz.yeoju.yeoju_app.payload.resDto.dekan;
+
+public interface DekanGroupsStatistic {
+    String getLevel();
+    String getName();
+    String getComeCount();
+    String getAllCount();
+}
