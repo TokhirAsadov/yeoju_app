2822600c5e6655e1f7d898859340d9286afb8eb0
Tohir <guvalakat1603@gmail.com>
2022-07-25 16:41:34 +0500

    add function

--- src/main/java/uz/yeoju/yeoju_app/controller/studentBall/FileReaderController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/studentBall/FileReaderController.java
@@ -2,6 +2,7 @@ package uz.yeoju.yeoju_app.controller.studentBall;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -14,6 +15,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.Group;
+import uz.yeoju.yeoju_app.entity.Lesson;
+import uz.yeoju.yeoju_app.entity.Student;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.studentBall.StudentResult;
+import uz.yeoju.yeoju_app.entity.studentBall.SubjectCredit;
+import uz.yeoju.yeoju_app.repository.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +30,13 @@ import java.util.List;
 @RequestMapping(BaseUrl.BASE_URL+"/fileRead")
 @RequiredArgsConstructor
 public class FileReaderController {
+
+    private final SubjectCreditRepository subjectCreditRepository;
+    private final LessonRepository lessonRepository;
+    private final StudentResultRepository studentResultRepository;
+    private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
+    private final StudentRepository studentRepository;
 
     @SneakyThrows
     @PostMapping("/import")
@@ -38,17 +53,61 @@ public class FileReaderController {
         //        <input type="file" th:name="file">
         //    <input th:type="submit" value="Import" />
 
-        for(int i=1;i<worksheet.getPhysicalNumberOfRows() ;i++) {
-//            Test tempStudent = new Test();
+        for(int i=100;i<worksheet.getPhysicalNumberOfRows() ;i++) {
 
             XSSFRow row = worksheet.getRow(i);
-            System.out.println(row);
-            strings.add(row.getCell(1).toString());
+
+            String student_id = row.getCell(0).toString();
+            String year = row.getCell(1).toString();
+            String semester = row.getCell(2).toString();
+            String subject = row.getCell(3).toString();
+            String credit =row.getCell(4).toString();
+            String score =row.getCell(5).toString();
+            String grade = row.getCell(6).toString();
+
+            SubjectCredit subjectCredit = new SubjectCredit();
+            Student studentByUserLogin = studentRepository.findStudentByUserLogin(student_id);
+
+            if (studentByUserLogin!=null) {// student borligi tekshirilyapdi
+                Group group = studentByUserLogin.getGroup();
+                if (!subject.contains("RETAKE")) { // retake emasligi tekshirilyapdi
+                    if (!subjectCreditRepository.existsSubjectCreditByLessonNameAndGroupName(subject, group.getName())) { // bir xil lesson hamda group nameli subject credit borligi tekshirilyapdi
+
+                        Lesson lessonByName = lessonRepository.getLessonByName(subject);
+                        subjectCredit.setLesson(lessonByName);
+                        subjectCredit.setCredit(credit);
+                        subjectCredit.setYear(year);
+                        subjectCredit.setSemester(semester);
+                        SubjectCredit save = subjectCreditRepository.save(subjectCredit);
+
+                        StudentResult studentResult = new StudentResult();
+                        User userByLogin = userRepository.getUserByLogin(student_id);
+                        studentResult.setScore(score);
+                        studentResult.setUser(userByLogin);
+                        studentResult.setSubjectCredit(save);
+                        studentResultRepository.saveAndFlush(studentResult);
+                    }
+                    else {
+                        StudentResult studentResult = new StudentResult();
+                        SubjectCredit subjectCreditByLessonNameAndGroupName = subjectCreditRepository.getSubjectCreditByLessonNameAndGroupName(subject, group.getName());
+                        studentResult.setSubjectCredit(subjectCreditByLessonNameAndGroupName);
+                        User userByLogin = userRepository.getUserByLogin(student_id);
+                        studentResult.setUser(userByLogin);
+                        studentResult.setScore(score);
+                        studentResultRepository.save(studentResult);
+                    }
+                }
+                else {
+                    //todo====================== RETAKE LARNI YOZIW KK LOGIKASINI ==============================
+                }
+            }
+
+            strings.add(grade);
 
 //            tempStudent.setId((int) row.getCell(0).getNumericCellValue());
 //            tempStudent.setContent(row.getCell(1).getStringCellValue());
 //            tempStudentList.add(tempStudent);
         }
-        return ResponseEntity.ok(strings);
+        return ResponseEntity.ok(strings.size()+" "+strings);
     }
 }
