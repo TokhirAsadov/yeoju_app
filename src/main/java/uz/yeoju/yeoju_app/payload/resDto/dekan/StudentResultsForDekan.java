634a4345e8ff9a89105c634e5308b316a64e5089
Tohir <guvalakat1603@gmail.com>
2022-07-29 06:14:00 +0500

    created new restDto for Dekan

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/dekan/StudentResultsForDekan.java
@@ -0,0 +1,11 @@
+package uz.yeoju.yeoju_app.payload.resDto.dekan;
+
+public interface StudentResultsForDekan {
+    String getId();
+    Integer getFail();
+    String getFullName();
+    String getEmail();
+    String getPassport();
+    String getLogin();
+    String getCardNo();
+}
