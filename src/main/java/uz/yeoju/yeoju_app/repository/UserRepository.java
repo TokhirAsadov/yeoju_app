b571774bc95f1b7692c442d12569e36863e94c0d
Tohir <guvalakat1603@gmail.com>
2022-08-01 07:00:18 +0500

    add new function

--- src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/UserRepository.java
@@ -45,4 +45,6 @@ public interface UserRepository extends JpaRepository<User, String> {
             "or S.passportSerial like :keyword",nativeQuery = true)
     List<SearchDto> getUserForSearch(@Param("keyword") String keyword);
 
+    User getUserByPassportNum(String passportNum);
+
 }
