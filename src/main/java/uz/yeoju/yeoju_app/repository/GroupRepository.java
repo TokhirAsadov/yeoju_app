dd2594ada8dab75b86dede344a6638162a46ba48
Tohir <guvalakat1603@gmail.com>
2022-08-02 09:59:31 +0500

    add new native query

--- src/main/java/uz/yeoju/yeoju_app/repository/GroupRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/GroupRepository.java
@@ -1,7 +1,10 @@
 package uz.yeoju.yeoju_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uz.yeoju.yeoju_app.entity.Group;
+import uz.yeoju.yeoju_app.payload.resDto.dekan.StudentGroupField;
 
 import java.util.List;
 
@@ -10,4 +13,11 @@ public interface GroupRepository extends JpaRepository<Group,String> {
    boolean existsGroupByName(String name);
    List<Group> findGroupsByLevel(Integer level);
    List<Group> findGroupsByFacultyId(String faculty_id);
+
+   @Query(value = "select g.id,g.level,g.name,F.name as facultyName,et.name as educationTypeName from Student s\n" +
+           "join groups g on g.id = s.group_id\n" +
+           "join Faculty F on F.id = g.faculty_id\n" +
+           "join EducationType et on g.educationType_id = et.id\n" +
+           "where s.user_id =:userId",nativeQuery = true)
+   StudentGroupField getGroupFieldByUserId(@Param("userId") String userId);
 }
