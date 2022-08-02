459e2c59f5c569f9e07f2158f85b2fecd316d0a7
Tohir <guvalakat1603@gmail.com>
2022-08-02 09:59:10 +0500

    add new native query

--- src/main/java/uz/yeoju/yeoju_app/repository/StudentResultRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/StudentResultRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import uz.yeoju.yeoju_app.entity.studentBall.StudentResult;
+import uz.yeoju.yeoju_app.payload.resDto.dekan.StudentResults;
 import uz.yeoju.yeoju_app.payload.resDto.dekan.StudentResultsForDekan;
 
 import java.util.List;
@@ -21,4 +22,12 @@ public interface StudentResultRepository extends JpaRepository<StudentResult, Lo
             "where g.name=:groupName and cast(score as double precision) < 65\n" +
             "group by u.fullName, u.passportNum, u.email, u.login, u.RFID, sr.user_id",nativeQuery = true)
     List<StudentResultsForDekan> getStudentResultsForDekan(@Param("groupName") String groupName);
+
+    @Query(value = "select sr.id,SC.year,cast(SC.semester as double precision) as semester ,\n" +
+            "       l.name as subject,cast(SC.credit as double precision) as credit, \n" +
+            "       cast(sr.score as double precision) as score from StudentResult sr\n" +
+            "join SubjectCredit SC on SC.id = sr.subjectCredit_id\n" +
+            "join Lesson l on SC.lesson_id = l.id\n" +
+            "where sr.user_id=:userId",nativeQuery = true)
+    List<StudentResults> getStudentResultsByUserId(@Param("userId") String userId);
 }
