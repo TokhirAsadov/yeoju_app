b26dc8734e0807240399017ac97a869cf045c7da
Tohir <guvalakat1603@gmail.com>
2022-08-02 09:59:40 +0500

    add new native query

--- src/main/java/uz/yeoju/yeoju_app/repository/AddressUserRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/AddressUserRepository.java
@@ -1,7 +1,13 @@
 package uz.yeoju.yeoju_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uz.yeoju.yeoju_app.entity.AddressUser;
+import uz.yeoju.yeoju_app.payload.resDto.dekan.StudentAddress;
 
 public interface AddressUserRepository extends JpaRepository<AddressUser,String> {
+
+    @Query(value = "select au.id,au.country,au.region,au.area,au.address from AddressUser au where user_id=:userId",nativeQuery = true)
+    StudentAddress getAddressByUserId(@Param("userId") String userId);
 }
