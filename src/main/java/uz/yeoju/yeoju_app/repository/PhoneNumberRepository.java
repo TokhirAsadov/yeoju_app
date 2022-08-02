ea3ea710084e399eee1acaf912b39fa8f1daa64c
Tohir <guvalakat1603@gmail.com>
2022-08-02 09:59:20 +0500

    add new native query

--- src/main/java/uz/yeoju/yeoju_app/repository/PhoneNumberRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/PhoneNumberRepository.java
@@ -1,7 +1,10 @@
 package uz.yeoju.yeoju_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uz.yeoju.yeoju_app.entity.PhoneNumber;
+import uz.yeoju.yeoju_app.payload.resDto.dekan.StudentPhones;
 
 import java.util.List;
 
@@ -10,5 +13,9 @@ public interface PhoneNumberRepository extends JpaRepository<PhoneNumber, String
     boolean existsPhoneNumberByPhoneNumber(String phoneNumber);
 
     List<PhoneNumber> findPhoneNumbersByUserId(String user_id);
+
+
+    @Query(value = "select id,phoneNumber,phoneType from PhoneNumber p_n where p_n.user_id=:userId",nativeQuery = true)
+    List<StudentPhones> getPhoneNumberUserId(@Param("userId") String userId);
 
 }
