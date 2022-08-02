150fcba5ccb71ee33df6e0a0e433cade5329295f
Tohir <guvalakat1603@gmail.com>
2022-08-02 09:58:42 +0500

    create new restDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/dekan/StudentResults.java
@@ -0,0 +1,10 @@
+package uz.yeoju.yeoju_app.payload.resDto.dekan;
+
+public interface StudentResults {
+    Long getId();
+    String getYear();
+    Integer getSemester();
+    String getSubject();
+    Integer getCredit();
+    Integer getScore();
+}
