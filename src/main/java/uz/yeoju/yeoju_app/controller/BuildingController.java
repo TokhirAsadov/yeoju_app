297960e87431c3535902f22f41f990ecd7db004b
Bismillah <guvalakat1603@gmail.con>
2022-09-07 18:00:23 +0500

    add new controllers

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/BuildingController.java
@@ -0,0 +1,43 @@
+package uz.yeoju.yeoju_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.payload.admin.BuildingDto;
+import uz.yeoju.yeoju_app.payload.admin.RoomDto;
+import uz.yeoju.yeoju_app.service.useServices.BuildingService;
+
+@RestController
+@RequestMapping(BaseUrl.BASE_URL+"/building")
+@RequiredArgsConstructor
+public class BuildingController {
+
+    public final BuildingService buildingService;
+
+    @GetMapping("/buildings")
+    public HttpEntity<?> allBuildings(){
+        return ResponseEntity.ok(buildingService.findAll());
+    }
+
+    @GetMapping("/getBuildingById/{id}")
+    public HttpEntity<?> getBuildingById(@PathVariable String id){
+        return ResponseEntity.ok(buildingService.findById(id));
+    }
+
+    @PostMapping("/createBuilding")
+    public HttpEntity<?> createBuilding(@RequestBody BuildingDto dto){
+        return ResponseEntity.status(201).body(buildingService.saveOrUpdate(dto));
+    }
+
+    @PutMapping("/updateBuilding")
+    public HttpEntity<?> updateBuilding(@RequestBody BuildingDto dto){
+        return ResponseEntity.status(202).body(buildingService.saveOrUpdate(dto));
+    }
+
+    @DeleteMapping("/deleteBuilding/{id}")
+    public HttpEntity<?> deleteBuilding(@PathVariable String id){
+        return ResponseEntity.status(204).body(buildingService.deleteById(id));
+    }
+
+}
---
+++ src/main/java/uz/yeoju/yeoju_app/controller/RoomController.java
@@ -0,0 +1,43 @@
+package uz.yeoju.yeoju_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.payload.EducationLanguageDto;
+import uz.yeoju.yeoju_app.payload.admin.RoomDto;
+import uz.yeoju.yeoju_app.service.useServices.RoomService;
+
+@RestController
+@RequestMapping(BaseUrl.BASE_URL+"/room")
+@RequiredArgsConstructor
+public class RoomController {
+
+    public final RoomService roomService;
+
+    @GetMapping("/rooms")
+    public HttpEntity<?> allRooms(){
+        return ResponseEntity.ok(roomService.findAll());
+    }
+
+    @GetMapping("/getRoomById/{id}")
+    public HttpEntity<?> getRoomById(@PathVariable String id){
+        return ResponseEntity.ok(roomService.findById(id));
+    }
+
+    @PostMapping("/createRoom")
+    public HttpEntity<?> createRoom(@RequestBody RoomDto dto){
+        return ResponseEntity.status(201).body(roomService.saveOrUpdate(dto));
+    }
+
+    @PutMapping("/updateRoom")
+    public HttpEntity<?> updateRoom(@RequestBody RoomDto dto){
+        return ResponseEntity.status(202).body(roomService.saveOrUpdate(dto));
+    }
+
+    @DeleteMapping("/deleteRoom/{id}")
+    public HttpEntity<?> deleteRoom(@PathVariable String id){
+        return ResponseEntity.status(204).body(roomService.deleteById(id));
+    }
+
+}
