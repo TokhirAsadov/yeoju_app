17dd6a3ef193405f519aefdacaa09068feb4fcdc
Bismillah <guvalakat1603@gmail.con>
2022-09-08 17:57:17 +0500

    add new rest dto

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/admin/DeviceMenu.java
@@ -0,0 +1,14 @@
+package uz.yeoju.yeoju_app.entity.admin;
+
+import java.util.Date;
+
+public interface DeviceMenu {
+    String getId();
+    String getFullName();
+    String getDevice();
+    String getRoom();
+    Date getTime();
+    String getPassword();
+    String getCard();
+    String getLogin();
+}
