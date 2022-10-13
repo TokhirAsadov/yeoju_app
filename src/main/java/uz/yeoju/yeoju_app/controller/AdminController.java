b56ee3d604da8405f09f56b75686586a9e12dfd7
Bismillah <guvalakat1603@gmail.con>
2022-10-13 17:03:00 +0500

    changed repository

--- src/main/java/uz/yeoju/yeoju_app/controller/AdminController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/AdminController.java
@@ -7,23 +7,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.yeoju.yeoju_app.entity.User;
-import uz.yeoju.yeoju_app.repository.DevicePortRepository;
+import uz.yeoju.yeoju_app.repository.AccDoorRepository;
 import uz.yeoju.yeoju_app.secret.CurrentUser;
 
 @RestController
 @RequestMapping(BaseUrl.BASE_URL+"/admin")
 @RequiredArgsConstructor
 public class AdminController {
-    private final DevicePortRepository devicePortRepository;
+    private final AccDoorRepository accDoorRepository;
 
     @GetMapping("/menu")
     public HttpEntity<?> getMonitoring(){
-        return ResponseEntity.ok(devicePortRepository.getAccMonitoring());
+        return ResponseEntity.ok(accDoorRepository.getAccMonitoring());
     }
 
     @GetMapping("/device")
     public HttpEntity<?> device(@CurrentUser User user){
-        return ResponseEntity.ok(devicePortRepository.deviceDates(user.getId()));
+        return ResponseEntity.ok(accDoorRepository.deviceDates(user.getId()));
     }
+
+
 
 }
