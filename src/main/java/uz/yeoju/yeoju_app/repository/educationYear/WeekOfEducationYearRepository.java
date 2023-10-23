2fd2d1751538a66020422faf2c696ee8e8260bc3
TokhirAsadov <guvalakat1603@gmail.com>
2023-10-23 09:40:38 +0500

    new query was added

--- src/main/java/uz/yeoju/yeoju_app/repository/educationYear/WeekOfEducationYearRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/educationYear/WeekOfEducationYearRepository.java
@@ -1,6 +1,7 @@
 package uz.yeoju.yeoju_app.repository.educationYear;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.yeoju.yeoju_app.entity.educationYear.WeekOfEducationYear;
 
 import java.util.Date;
@@ -9,6 +10,10 @@ import java.util.Optional;
 
 public interface WeekOfEducationYearRepository extends JpaRepository<WeekOfEducationYear,String> {
     boolean existsByStart(Date start);
+
+    @Query(value = "select * from WeekOfEducationYear order by createdAt desc ",nativeQuery = true)
+    List<WeekOfEducationYear> findAllByCreatedAt();
+
     Optional<WeekOfEducationYear> findWeekOfEducationYearByStart(Date start);
 //    Optional<WeekOfEducationYear> findWeekOfEducationYearBySortNumberAndWeekNumber();
     List<WeekOfEducationYear> findWeekOfEducationsBySortNumberOrWeekNumber(Integer sortNumber, Integer weekNumber);
