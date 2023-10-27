d06f48a7c6602772a2253278f2f54e2706af1f71
TokhirAsadov <guvalakat1603@gmail.com>
2023-10-27 10:00:59 +0500

    New CreateGradeChangedHistoryDto object dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/uquvbulimi/CreateGradeChangedHistoryDto.java
@@ -0,0 +1,14 @@
+package uz.yeoju.yeoju_app.payload.uquvbulimi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateGradeChangedHistoryDto {
+    private String gradeId;
+    private Integer oldGrade;
+    private Integer newGrade;
+}
