525bec761e99dbc1989ca7b1692aa4e8eb111747
TokhirAsadov <guvalakat1603@gmail.com>
2023-10-27 10:03:09 +0500

    New GetDataForStudentReference res dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/GetDataForStudentReference.java
@@ -0,0 +1,17 @@
+package uz.yeoju.yeoju_app.payload.resDto.student;
+
+import java.sql.Timestamp;
+
+public interface GetDataForStudentReference {
+    String getId();
+    Timestamp getTime();
+    String getFullName();
+    String getPassport();
+    String getDirection();
+    Integer getGrade();
+    String getEduLang();
+    String getEduType();
+    String getLengthOfStudying();
+    String getRektororder();
+    String getDean();
+}
