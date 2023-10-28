629f4c7ffa260e3304fda283eca4ae2092599b99
TokhirAsadov <guvalakat1603@gmail.com>
2023-10-28 12:02:10 +0500

    GetStudentStatisticsForDeanOneWeekSection res dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/GetStudentStatisticsForDeanOneWeekSection.java
@@ -0,0 +1,21 @@
+package uz.yeoju.yeoju_app.payload.resDto.student;
+
+import org.springframework.beans.factory.annotation.Value;
+import uz.yeoju.yeoju_app.payload.resDto.kafedra.TeacherStatisticsOfWeekday;
+
+import java.util.List;
+
+public interface GetStudentStatisticsForDeanOneWeekSection {
+    String getStudentId();
+    String getBetweenDuringDate();
+    String getRoom();
+    Integer getWeekday();
+    Integer getSection();
+    String getLesson();
+    Integer getWeekNumber();
+    Integer getWeek();
+    Integer getYear();
+
+    @Value("#{@userRepository.getTimesForRoomStatisticsByUserIdAndWeek(target.studentId,target.room,target.weekday,target.week,target.year,target.section)}")
+    List<TeacherStatisticsOfWeekday> getStatistics();
+}
