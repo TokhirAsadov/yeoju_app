09e9c643769c621f590377867c3730014561f732
TokhirAsadov <guvalakat1603@gmail.com>
2023-10-28 12:03:08 +0500

    GradeChangedHistoryRepository repository was created for connect database

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/uquvbulimi/GradeChangedHistoryRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository.uquvbulimi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.uquvbulim.GradeChangedHistory;
+
+public interface GradeChangedHistoryRepository extends JpaRepository<GradeChangedHistory,String> {
+}
