40581a30346de8d61d44a851bf37ba2b1c966e60
TokhirAsadov <guvalakat1603@gmail.com>
2023-10-29 18:06:20 +0500

    some functions were removed

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTableDB/TimeTableDBService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTableDB/TimeTableDBService.java
@@ -12,16 +12,5 @@ public interface TimeTableDBService {
     ApiResponseTwoObj generateNewTimeTableToDB(String educationYearId,Integer year,Integer week);
 
 
-    ApiResponseTwoObj getStatisticsForDeanDashboard(String educationYearId,String eduType,String eduTypeId,String facultyId,String facultyShortName);
 
-    ApiResponseTwoObj getStudentTimeTableAPIByWeekOfYear(User user, String groupName,Integer year, Integer week, Integer day, Boolean s);
-    ApiResponseTwoObj getStudentTimeTableAPIByWeekOfYear(String userId, String groupName,Integer year, Integer week, Integer day, Boolean s);
-    ApiResponseTwoObj getTimesForRoomStatisticsByUserIdAndWeek(User user, String groupName, Integer year,Integer week, Integer day, Boolean s);
-    ApiResponse getTeacherTimeTable(User user,Integer week,Integer year);
-    ApiResponseTwoObj getTeacherTimeTableAndStatisticsForKafedra(User user,String kafedraId,Integer year,Integer month, Integer day,Integer week, Integer weekday);
-
-    ApiResponseTwoObj getTimeTableByRoomAndWeek(User user, String room, Integer weekday, Integer week, Integer year);
-
-    ApiResponseTwoObj getTimeTableByAllRoomAndWeek(User user,String building, Integer weekday, Integer week, Integer year,Boolean t);
-    ApiResponseTwoObj getTimeTableByAllRoomAndWeek2(User user, Integer weekday, Integer week, Integer year,Boolean t);
 }
