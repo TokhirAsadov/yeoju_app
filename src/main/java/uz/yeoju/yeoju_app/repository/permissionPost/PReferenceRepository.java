e840e2f600cef9d146061989272f080a5e8de742
TokhirAsadov <guvalakat1603@gmail.com>
2023-10-30 09:56:31 +0500

    existsPReferenceByStudentIdAndEducationYearIdAndStatus function was added

--- src/main/java/uz/yeoju/yeoju_app/repository/permissionPost/PReferenceRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/permissionPost/PReferenceRepository.java
@@ -1,11 +1,15 @@
 package uz.yeoju.yeoju_app.repository.permissionPost;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import uz.yeoju.yeoju_app.entity.enums.PPostStatus;
 import uz.yeoju.yeoju_app.entity.permissionPost.PReference;
 
 import java.util.List;
 
 public interface PReferenceRepository extends JpaRepository<PReference,String> {
+
+    Boolean existsPReferenceByStudentIdAndEducationYearIdAndStatus(String student_id, String educationYear_id, PPostStatus status);
     List<PReference> findAllByOrderByCreatedAtDesc();
     List<PReference> findAllByCreatedByOrderByCreatedAtDesc(String createdBy);
     List<PReference> findAllByDeanIdOrderByCreatedAtDesc(String dean_id);
