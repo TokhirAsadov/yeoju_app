cc4a280b7099d4d671b491841b1ece546bce647d
TokhirAsadov <guvalakat1603@gmail.com>
2023-10-31 20:51:49 +0500

    time of entering was given

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/GetStudentStatisticsForDeanOneWeek.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/GetStudentStatisticsForDeanOneWeek.java
@@ -2,6 +2,7 @@ package uz.yeoju.yeoju_app.payload.resDto.student;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Date;
 import java.util.Set;
 
 public interface GetStudentStatisticsForDeanOneWeek {
@@ -12,7 +13,11 @@ public interface GetStudentStatisticsForDeanOneWeek {
     Integer getWeekday();
     Integer getWeek();
     Integer getYear();
+    String getRfid();
 
     @Value("#{@groupRepository.getStudentStatisticsForDeanOneWeekSection(target.educationYearId, target.groupId, target.weekday, target.week, target.year,target.studentId)}")
     Set<GetStudentStatisticsForDeanOneWeekSection> getSubjects();
+
+    @Value("#{@groupRepository.getFirstOfEntering(target.rfid)}")
+    Date getEntering();
 }
