d2f106927d7856d1e14becdd2c5ca0018ec53c5d
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-01 10:33:12 +0500

    WebClient and REQUEST_TIMEOUT were added

--- src/main/java/uz/yeoju/yeoju_app/service/useServices/RoomService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/useServices/RoomService.java
@@ -1,13 +1,28 @@
 package uz.yeoju.yeoju_app.service.useServices;
 
 import lombok.RequiredArgsConstructor;
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+import uz.yeoju.yeoju_app.entity.RoleWebClient;
 import uz.yeoju.yeoju_app.entity.admin.Room;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.admin.RoomDto;
 import uz.yeoju.yeoju_app.repository.RoomRepository;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.RoomImplService;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Iterator;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -15,6 +30,16 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class RoomService implements RoomImplService<RoomDto> {
     private final RoomRepository roomRepository;
+
+
+    private static final Duration REQUEST_TIMEOUT = Duration.ofSeconds(3);// request time that how time is during
+
+    private final WebClient webClient;// get web client
+
+//    @Autowired
+//    public UserService(WebClient webClient) {
+//        this.webClient = webClient;
+//    }
 
 
     public ApiResponse getRoomsForSelect(){
