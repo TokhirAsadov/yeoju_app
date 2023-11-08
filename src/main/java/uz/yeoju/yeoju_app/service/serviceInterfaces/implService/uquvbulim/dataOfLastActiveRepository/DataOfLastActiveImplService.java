9da1e95cafe3cbc601e382d53a395c4aa427d8e5
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-08 10:13:56 +0500

    DataOfLastActiveImplService service was created

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/dataOfLastActiveRepository/DataOfLastActiveImplService.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.dataOfLastActiveRepository;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataOfLastActiveImplService implements DataOfLastActiveService {
+}
