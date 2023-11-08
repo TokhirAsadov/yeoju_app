a8d250447df961795c3a1a119e25e37431601c19
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-08 10:11:41 +0500

    DataOfLastActiveRepository repository was created

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/uquvbulimi/DataOfLastActiveRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository.uquvbulimi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.uquvbulim.DataOfLastActive;
+
+public interface DataOfLastActiveRepository extends JpaRepository<DataOfLastActive,String> {
+}
