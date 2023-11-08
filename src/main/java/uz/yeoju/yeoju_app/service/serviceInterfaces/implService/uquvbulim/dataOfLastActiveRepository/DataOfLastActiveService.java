debf3a2ddca7089b4c6a479687ea4968aa0e4094
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-08 10:17:06 +0500

    some functions were created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/dataOfLastActiveRepository/DataOfLastActiveService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/dataOfLastActiveRepository/DataOfLastActiveService.java
@@ -1,4 +1,9 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.dataOfLastActiveRepository;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
 public interface DataOfLastActiveService {
+    ApiResponse findAll();
+    ApiResponse findByCreatorId(String id);
+    ApiResponse create(String passage);
 }
