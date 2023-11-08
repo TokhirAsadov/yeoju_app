789ba0fa8a7b3758ab619bed6fca53b49563b489
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-08 10:09:49 +0500

    passage field was added

--- src/main/java/uz/yeoju/yeoju_app/entity/uquvbulim/DataOfLastActive.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/uquvbulim/DataOfLastActive.java
@@ -3,22 +3,14 @@ package uz.yeoju.yeoju_app.entity.uquvbulim;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DataOfLastActive extends AbsEntity {
-    @ManyToOne
-    private User user;
-
-    public DataOfLastActive(String id, User user) {
-        super(id);
-        this.user = user;
-    }
+    private String passage;
 }
