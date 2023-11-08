3eb0f35d6cc15c5dd74bbcef46fdd66dd0f0e53c
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-08 10:37:39 +0500

    /dataOfLastActive/findAll api was created

--- src/main/java/uz/yeoju/yeoju_app/controller/uquvbulim/DataOfLastActiveController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/uquvbulim/DataOfLastActiveController.java
@@ -1,13 +1,27 @@
 package uz.yeoju.yeoju_app.controller.uquvbulim;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.dataOfLastActiveRepository.DataOfLastActiveService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(BaseUrl.BASE_URL+"/dataOfLastActive")
 public class DataOfLastActiveController {
+    private final DataOfLastActiveService service;
+
+    @PreAuthorize("hasRole('ROLE_DEKAN')")
+    @GetMapping("/findAll")
+    public HttpEntity<?> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
 
 }
