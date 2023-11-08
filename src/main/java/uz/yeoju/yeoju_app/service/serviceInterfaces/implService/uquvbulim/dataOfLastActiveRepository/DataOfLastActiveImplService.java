c7a6b477052d77785f9d5550d3e40781210537af
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-08 10:17:32 +0500

    some functions were prepared to override

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/dataOfLastActiveRepository/DataOfLastActiveImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/dataOfLastActiveRepository/DataOfLastActiveImplService.java
@@ -1,7 +1,22 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.dataOfLastActiveRepository;
 
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 
 @Service
 public class DataOfLastActiveImplService implements DataOfLastActiveService {
+    @Override
+    public ApiResponse findAll() {
+        return null;
+    }
+
+    @Override
+    public ApiResponse findByCreatorId(String id) {
+        return null;
+    }
+
+    @Override
+    public ApiResponse create(String passage) {
+        return null;
+    }
 }
