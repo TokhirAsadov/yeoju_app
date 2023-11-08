74ff1b6aeef8d6842ff6878cda440df3da763084
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-08 10:28:07 +0500

    DataOfLastActiveController controller was created

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/uquvbulim/DataOfLastActiveController.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app.controller.uquvbulim;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.yeoju.yeoju_app.controller.BaseUrl;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(BaseUrl.BASE_URL+"/dataOfLastActive")
+public class DataOfLastActiveController {
+
+}
