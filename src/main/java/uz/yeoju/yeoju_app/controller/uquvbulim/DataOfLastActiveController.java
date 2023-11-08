5910d9aea0b7362004526a131a7c95ae65cdfd37
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-08 10:40:33 +0500

    /dataOfLastActive/create/{passage} api was created

--- src/main/java/uz/yeoju/yeoju_app/controller/uquvbulim/DataOfLastActiveController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/uquvbulim/DataOfLastActiveController.java
@@ -4,11 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.dataOfLastActiveRepository.DataOfLastActiveService;
 
 @RestController
@@ -27,5 +25,12 @@ public class DataOfLastActiveController {
     @GetMapping("/findByCreatorId/{findByCreatorId}")
     public HttpEntity<?> findByCreatorId(@PathVariable("findByCreatorId") String findByCreatorId) {
         return ResponseEntity.ok(service.findByCreatorId(findByCreatorId));
+    }
+
+    @PreAuthorize("hasRole('ROLE_DEKAN')")
+    @PostMapping("/create/{passage}")
+    public HttpEntity<?> create(@PathVariable("passage") String passage) {
+        ApiResponse response = service.create(passage);
+        return ResponseEntity.status(response.isSuccess() ? 201 : 401).body(response);
     }
 }
