4cf7376dc53dcba4aa44be371c4f456e3b43abfa
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-08 10:21:00 +0500

    some functions were done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/dataOfLastActiveRepository/DataOfLastActiveImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/dataOfLastActiveRepository/DataOfLastActiveImplService.java
@@ -1,22 +1,30 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.dataOfLastActiveRepository;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.uquvbulim.DataOfLastActive;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.repository.uquvbulimi.DataOfLastActiveRepository;
 
 @Service
+@RequiredArgsConstructor
 public class DataOfLastActiveImplService implements DataOfLastActiveService {
+
+    private final DataOfLastActiveRepository repository;
+
     @Override
     public ApiResponse findAll() {
-        return null;
+        return new ApiResponse(true,"all last active data",repository.findAll());
     }
 
     @Override
     public ApiResponse findByCreatorId(String id) {
-        return null;
+        return new ApiResponse(true,"all last active data",repository.findById(id));
     }
 
     @Override
     public ApiResponse create(String passage) {
-        return null;
+        repository.save(new DataOfLastActive(passage));
+        return new ApiResponse(true,"saved successfully.");
     }
 }
