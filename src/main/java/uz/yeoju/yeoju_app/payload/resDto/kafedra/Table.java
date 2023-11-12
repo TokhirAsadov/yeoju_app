048b4ad52840349684fb42b7881ade18675cd25f
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-12 15:19:00 +0500

    imports were removed

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/kafedra/Table.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/kafedra/Table.java
@@ -3,11 +3,9 @@ package uz.yeoju.yeoju_app.payload.resDto.kafedra;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uz.yeoju.yeoju_app.payload.forTimeTableFromXmlFile.LessonXml;
 import uz.yeoju.yeoju_app.payload.forTimeTableFromXmlFile.Show;
 
 import java.util.List;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
