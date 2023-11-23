efa61d39d6ff94b5632e480cb3fc688a29c529d0
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-23 09:04:46 +0500

    GetDataOtherImplService was created

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/getDataOtherService/GetDataOtherImplService.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.getDataOtherService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetDataOtherImplService{
+}
