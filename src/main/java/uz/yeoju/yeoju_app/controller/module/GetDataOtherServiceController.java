375c9d59acff92092d317c4a46607ce6ee84fa88
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-23 09:18:11 +0500

    /otherService/studentsResults/{studentId} api was created

--- src/main/java/uz/yeoju/yeoju_app/controller/module/GetDataOtherServiceController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/module/GetDataOtherServiceController.java
@@ -1,12 +1,25 @@
 package uz.yeoju.yeoju_app.controller.module;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.getDataOtherService.GetDataOtherService;
 
 @RestController
 @RequestMapping(BaseUrl.BASE_URL+"/otherService")
 @RequiredArgsConstructor
 public class GetDataOtherServiceController {
+    private final GetDataOtherService otherService;
+
+    @GetMapping("/studentsResults/{studentId}")
+    public HttpEntity<?> getStudentResults(@PathVariable("studentId") String studentId){
+        ApiResponse response = otherService.getStudentsResults(studentId);
+        return ResponseEntity.status(response.isSuccess() ? 200:409).body(response);
+    }
 }
