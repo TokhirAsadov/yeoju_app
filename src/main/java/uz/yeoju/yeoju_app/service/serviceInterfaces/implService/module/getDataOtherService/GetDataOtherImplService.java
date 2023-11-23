fa6da4fe6b3a9eff46cb77853992db5ead5e7269
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-23 09:05:16 +0500

    getStudentsResults was override

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/getDataOtherService/GetDataOtherImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/getDataOtherService/GetDataOtherImplService.java
@@ -1,7 +1,12 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.getDataOtherService;
 
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 
 @Service
-public class GetDataOtherImplService{
+public class GetDataOtherImplService implements GetDataOtherService{
+    @Override
+    public ApiResponse getStudentsResults(String studentId) {
+        return null;
+    }
 }
