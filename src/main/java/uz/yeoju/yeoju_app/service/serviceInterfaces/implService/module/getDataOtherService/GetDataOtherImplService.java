bcb23a2db5c7ba8bed8b1177e2bcd10dd1033c2b
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-23 09:08:40 +0500

    getResToken was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/getDataOtherService/GetDataOtherImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/getDataOtherService/GetDataOtherImplService.java
@@ -1,8 +1,13 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.getDataOtherService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.ResToken;
 import uz.yeoju.yeoju_app.payload.SignInDto;
@@ -23,6 +28,26 @@ public class GetDataOtherImplService implements GetDataOtherService{
 
     @Override
     public ResToken getResToken(SignInDto sign) {
-        return null;
+        ResponseEntity<ResToken> resTokenResponse = webClient.post()
+                .uri("/auth/login")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(sign), SignInDto.class)
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, response -> {
+                    //logError("Client error occurred");
+                    return Mono.error(new WebClientResponseException
+                            (response.statusCode().value(), "Bad Request", null, null, null));
+                })
+                .onStatus(HttpStatus::is5xxServerError, response -> {
+                    //logError("Server error occurred");
+                    return Mono.error(new WebClientResponseException
+                            (response.statusCode().value(), "Server Error", null, null, null));
+                })
+                .toEntity(ResToken.class)
+                .block(REQUEST_TIMEOUT);
+        System.out.println(resTokenResponse.getBody()+"----------------------------------------- RES TOKEN");
+
+        return resTokenResponse.getBody();
     }
 }
