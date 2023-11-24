7930dbeaa9fa57eba8366698d68989157b68cd9e
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-24 09:11:51 +0500

    getResToken was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/workOtherService/WorkOtherImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/workOtherService/WorkOtherImplService.java
@@ -1,16 +1,49 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.workOtherService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 import uz.yeoju.yeoju_app.payload.ResToken;
 import uz.yeoju.yeoju_app.payload.SignInDto;
+import uz.yeoju.yeoju_app.repository.UserRepository;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class WorkOtherImplService implements WorkOtherService{
+    private final UserRepository userRepository;
+    private static final Duration REQUEST_TIMEOUT = Duration.ofSeconds(10);// request time that how time is during
+
+    private final WebClient webClient;// get web client
 
     @Override
     public ResToken getResToken(SignInDto sign) {
-        return null;
+        ResponseEntity<ResToken> resTokenResponse = webClient.post()
+                .uri("/auth/login")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(sign), SignInDto.class)
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, response -> {
+                    //logError("Client error occurred");
+                    return Mono.error(new WebClientResponseException
+                            (response.statusCode().value(), "Bad Request", null, null, null));
+                })
+                .onStatus(HttpStatus::is5xxServerError, response -> {
+                    //logError("Server error occurred");
+                    return Mono.error(new WebClientResponseException
+                            (response.statusCode().value(), "Server Error", null, null, null));
+                })
+                .toEntity(ResToken.class)
+                .block(REQUEST_TIMEOUT);
+        System.out.println(resTokenResponse.getBody()+"----------------------------------------- RES TOKEN");
+
+        return resTokenResponse.getBody();
     }
 }
