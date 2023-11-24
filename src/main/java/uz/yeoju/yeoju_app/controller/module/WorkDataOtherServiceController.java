0dc3225b55349b6302062d7c278e244f8dd0c81a
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-24 09:03:32 +0500

    WorkDataOtherServiceController was created

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/module/WorkDataOtherServiceController.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app.controller.module;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.yeoju.yeoju_app.controller.BaseUrl;
+
+@RestController
+@RequestMapping(BaseUrl.BASE_URL+"/workOtherService")
+@RequiredArgsConstructor
+public class WorkDataOtherServiceController {
+
+}
