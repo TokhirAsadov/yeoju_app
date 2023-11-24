16aaa59e647c975dae9e2463be19d1b159f530fe
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-24 09:09:52 +0500

    getResToken was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/workOtherService/WorkOtherService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/workOtherService/WorkOtherService.java
@@ -1,4 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.workOtherService;
 
+import uz.yeoju.yeoju_app.payload.ResToken;
+import uz.yeoju.yeoju_app.payload.SignInDto;
+
 public interface WorkOtherService {
+    ResToken getResToken(SignInDto sign);
 }
