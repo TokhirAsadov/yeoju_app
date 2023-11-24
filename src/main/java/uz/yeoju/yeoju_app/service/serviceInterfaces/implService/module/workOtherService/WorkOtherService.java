2e0fc8cb5b7f05d32c55f69c55d4af13014a6169
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-24 09:26:32 +0500

    sendMultipartDataOtherServer was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/workOtherService/WorkOtherService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/workOtherService/WorkOtherService.java
@@ -1,8 +1,11 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.workOtherService;
 
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.ResToken;
 import uz.yeoju.yeoju_app.payload.SignInDto;
 
 public interface WorkOtherService {
+    Object sendMultipartDataOtherServer(MultipartHttpServletRequest request);
     ResToken getResToken(SignInDto sign);
 }
