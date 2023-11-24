4226d824382b29bc664f2f82d589ede343f3f826
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-24 09:10:08 +0500

    getResToken was override

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/workOtherService/WorkOtherImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/workOtherService/WorkOtherImplService.java
@@ -1,4 +1,16 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.workOtherService;
 
-public class WorkOtherImplService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.payload.ResToken;
+import uz.yeoju.yeoju_app.payload.SignInDto;
+
+@Service
+@RequiredArgsConstructor
+public class WorkOtherImplService implements WorkOtherService{
+
+    @Override
+    public ResToken getResToken(SignInDto sign) {
+        return null;
+    }
 }
