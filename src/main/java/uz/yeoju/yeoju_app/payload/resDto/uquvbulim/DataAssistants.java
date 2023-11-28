2514ae88c48c06ab84073abf61e0661b15caf7f9
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-28 10:41:54 +0500

    res dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/uquvbulim/DataAssistants.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.payload.resDto.uquvbulim;
+
+public interface DataAssistants {
+    String getId();
+    String getFullName();
+    String getFirstName();
+    String getLastName();
+    String getMiddleName();
+}
