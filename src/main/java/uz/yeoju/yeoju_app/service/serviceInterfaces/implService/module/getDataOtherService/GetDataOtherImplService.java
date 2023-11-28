3edc27a1f3412805f27ebaba944da816b714f409
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-28 10:08:40 +0500

    getStudentFails2 was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/getDataOtherService/GetDataOtherImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/getDataOtherService/GetDataOtherImplService.java
@@ -148,6 +148,20 @@ public class GetDataOtherImplService implements GetDataOtherService{
 
     @Override
     public ApiResponse getStudentFails2(String login) {
-        return null;
+        Optional<User> optionalUser = userRepository.findUserByLogin(login);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            return new ApiResponse(true, "fails student id: "+user.getLogin(),
+                    webClient
+                            .get()
+                            .uri("/failtable/getFailsByStudentId/"+user.getLogin())
+                            .retrieve()
+                            .bodyToMono(Object.class)
+                            .block(REQUEST_TIMEOUT)
+            );
+        }
+        else {
+            return new ApiResponse(false,"student not found by id: "+login);
+        }
     }
 }
