3993cb5cb2b646de1fa0ab3df79a56ba48c9216c
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-28 10:42:08 +0500

    getAssistants was created

--- src/main/java/uz/yeoju/yeoju_app/repository/uquvbulimi/DataOfLastActiveRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/uquvbulimi/DataOfLastActiveRepository.java
@@ -1,7 +1,20 @@
 package uz.yeoju.yeoju_app.repository.uquvbulimi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.yeoju.yeoju_app.entity.uquvbulim.DataOfLastActive;
+import uz.yeoju.yeoju_app.payload.resDto.uquvbulim.DataAssistants;
+
+import java.util.List;
+import java.util.Set;
 
 public interface DataOfLastActiveRepository extends JpaRepository<DataOfLastActive,String> {
+
+    List<DataOfLastActive> findAllByCreatedByOrderByCreatedAtDesc(String createdBy);
+
+
+    @Query(value = "select u.id,u.fullName,u.firstName,u.lastName,u.middleName from users u join users_Role ur on u.id = ur.users_id\n" +
+            "join Role r on ur.roles_id = r.id\n" +
+            "where r.roleName='ROLE_MONITORING_ASSISTANT'",nativeQuery = true)
+    Set<DataAssistants> getAssistants();
 }
