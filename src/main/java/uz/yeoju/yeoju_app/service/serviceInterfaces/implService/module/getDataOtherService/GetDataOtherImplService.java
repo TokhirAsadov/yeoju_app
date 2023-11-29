033cdd903cc12d497c5b7b0d1a148fcc3e86b80a
TokhirAsadov <guvalakat1603@gmail.com>
2023-11-29 10:09:03 +0500

    getStudentsFinals2 was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/getDataOtherService/GetDataOtherImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/getDataOtherService/GetDataOtherImplService.java
@@ -224,6 +224,20 @@ public class GetDataOtherImplService implements GetDataOtherService{
 
     @Override
     public ApiResponse getStudentsFinals2(String login) {
-        return null;
+        Optional<User> optionalUser = userRepository.findUserByLogin(login);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            return new ApiResponse(true, "Finals by student id: "+user.getLogin(),
+                    webClient
+                            .get()
+                            .uri("/final/getFinalsByStudentId/"+user.getLogin())
+                            .retrieve()
+                            .bodyToMono(Object.class)
+                            .block(REQUEST_TIMEOUT)
+            );
+        }
+        else {
+            return new ApiResponse(false,"student not found by id: "+login);
+        }
     }
 }
