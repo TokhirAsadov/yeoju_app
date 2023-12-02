ce2279d7e68d61ac8d57082394f881595141fa45
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-02 10:32:27 +0500

    createAndUpdate was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/notificationOuter/NotificationOuterService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/notificationOuter/NotificationOuterService.java
@@ -1,7 +1,9 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.notificationOuter;
 
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.dekanat.NotificationOuterCreateDto;
 
 public interface NotificationOuterService {
     ApiResponse findAllNotifications();
+    ApiResponse createAndUpdate(NotificationOuterCreateDto dto);
 }
