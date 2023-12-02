28d44e4e594ce1e9ad4b8249220589b9f9437c11
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-02 10:31:22 +0500

    findAllNotifications was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/notificationOuter/NotificationOuterImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/notificationOuter/NotificationOuterImplService.java
@@ -3,12 +3,15 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.notificationOut
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.repository.NotificationOuterRepository;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationOuterImplService implements NotificationOuterService{
+    private final NotificationOuterRepository repository;
+
     @Override
     public ApiResponse findAllNotifications() {
-        return null;
+        return new ApiResponse(true,"all notifications",repository.findAll());
     }
 }
