753e483ccceaad35fe4d907ae0e632a92147d5a2
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-02 10:52:09 +0500

    GetStudentNotificationOuters was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/dekan/dekanat/GetStudentNotificationOuters.java
@@ -0,0 +1,10 @@
+package uz.yeoju.yeoju_app.payload.resDto.dekan.dekanat;
+
+import java.util.Date;
+
+public interface GetStudentNotificationOuters {
+    String getId();
+    String getEducationYear();
+    Date getFromDate();
+    Date getToDate();
+}
