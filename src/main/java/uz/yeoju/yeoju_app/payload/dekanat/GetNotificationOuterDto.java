8095c7a8c1c09716443daf451ac4da67d67bd1e9
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-02 11:44:42 +0500

    GetNotificationOuterDto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/dekanat/GetNotificationOuterDto.java
@@ -0,0 +1,22 @@
+package uz.yeoju.yeoju_app.payload.dekanat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetNotificationOuterDto {
+    public String id;
+    public String dekanat;
+    public String educationYear;
+    public Integer course;
+    public Date fromDate;
+    public Date toDate;
+    public Set<String> facultyNames;
+    public Set<String> groupNames;
+}
