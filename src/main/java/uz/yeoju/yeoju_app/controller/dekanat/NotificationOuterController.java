e8c672c009498c4e31af5a7d332893907a6c3994
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-02 10:59:55 +0500

    /notificationOuter/save api was created

--- src/main/java/uz/yeoju/yeoju_app/controller/dekanat/NotificationOuterController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/dekanat/NotificationOuterController.java
@@ -1,9 +1,12 @@
 package uz.yeoju.yeoju_app.controller.dekanat;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.dekanat.NotificationOuterCreateDto;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.notificationOuter.NotificationOuterService;
 
 @RestController
@@ -12,5 +15,10 @@ import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.notificationOute
 public class NotificationOuterController {
     private final NotificationOuterService service;
 
+    @PostMapping("/save")
+    public HttpEntity<?> save(@RequestBody NotificationOuterCreateDto dto){
+        ApiResponse response = service.createAndUpdate(dto);
+        return ResponseEntity.status(response.isSuccess() ? 201 : 409).body(response);
+    }
 
 }
