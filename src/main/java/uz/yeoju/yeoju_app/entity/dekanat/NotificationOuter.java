074f96be56375e468737453d5957b3e85d3f1168
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-02 10:27:47 +0500

    NotificationOuter was created

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/dekanat/NotificationOuter.java
@@ -0,0 +1,48 @@
+package uz.yeoju.yeoju_app.entity.dekanat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.Faculty;
+import uz.yeoju.yeoju_app.entity.Group;
+import uz.yeoju.yeoju_app.entity.educationYear.EducationYear;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class NotificationOuter extends AbsEntity {// sirtqi talimda uqiyatgan talabalar uchun xabarnoma talabaning ish joyi uchun
+    @ManyToOne
+    private Dekanat dekanat;
+
+    @ManyToOne
+    private EducationYear educationYear;
+
+    private Integer course;
+    private Date fromDate;
+    private Date toDate;
+
+    @ManyToMany
+    private Set<Faculty> faculties;
+
+    @ManyToMany
+    private Set<Group> groups;
+
+    public NotificationOuter(String id, Dekanat dekanat, EducationYear educationYear, Integer course, Date fromDate, Date toDate, Set<Faculty> faculties, Set<Group> groups) {
+        super(id);
+        this.dekanat = dekanat;
+        this.educationYear = educationYear;
+        this.course = course;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.faculties = faculties;
+        this.groups = groups;
+    }
+}
