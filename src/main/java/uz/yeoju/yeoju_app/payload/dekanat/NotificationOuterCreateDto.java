f1b53d22720ee7f88746a6a99bd61846fe92d17d
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-02 10:27:56 +0500

    NotificationOuterCreateDto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/dekanat/NotificationOuterCreateDto.java
@@ -0,0 +1,33 @@
+package uz.yeoju.yeoju_app.payload.dekanat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationOuterCreateDto {
+    public String id;
+    public String dekanatId;
+    public String educationId;
+    public Integer course;
+    public Date fromDate;
+    public Date toDate;
+    public Set<String> facultiesId;
+
+    public Set<String> groupsId;
+
+    public NotificationOuterCreateDto(String dekanatId, String educationId, Integer course, Date fromDate, Date toDate, Set<String> facultiesId, Set<String> groupsId) {
+        this.dekanatId = dekanatId;
+        this.educationId = educationId;
+        this.course = course;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.facultiesId = facultiesId;
+        this.groupsId = groupsId;
+    }
+}
