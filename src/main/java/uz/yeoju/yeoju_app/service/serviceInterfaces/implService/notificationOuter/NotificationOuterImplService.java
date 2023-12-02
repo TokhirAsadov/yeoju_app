3e410ddd57edbaffa1b01100f094c31b39665354
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-02 10:29:50 +0500

    NotificationOuterImplService was created

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/notificationOuter/NotificationOuterImplService.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.notificationOuter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationOuterImplService implements NotificationOuterService{
+}
