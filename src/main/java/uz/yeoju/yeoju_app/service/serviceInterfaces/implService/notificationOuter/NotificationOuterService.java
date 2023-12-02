809ef2acdbb4ab5740b6145bf564f841e43c46f2
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-02 10:30:20 +0500

    findAllNotifications was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/notificationOuter/NotificationOuterService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/notificationOuter/NotificationOuterService.java
@@ -1,4 +1,7 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.notificationOuter;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
 public interface NotificationOuterService {
+    ApiResponse findAllNotifications();
 }
