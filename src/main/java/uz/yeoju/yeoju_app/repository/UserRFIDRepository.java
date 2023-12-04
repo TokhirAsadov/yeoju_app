d66005a247afc39c8c5d3e9052964072eed2740d
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-04 10:48:38 +0500

    some imports were removed

--- src/main/java/uz/yeoju/yeoju_app/repository/UserRFIDRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/UserRFIDRepository.java
@@ -4,24 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import uz.yeoju.yeoju_app.entity.User;
-import uz.yeoju.yeoju_app.payload.resDto.dekan.StudentData;
 import uz.yeoju.yeoju_app.payload.resDto.fayzulla.UserShareRFID;
-import uz.yeoju.yeoju_app.payload.resDto.kafedra.TeacherData;
-import uz.yeoju.yeoju_app.payload.resDto.rektor.dashboard.KafedraStatistics;
-import uz.yeoju.yeoju_app.payload.resDto.rektor.dashboard.RektorDashboard;
-import uz.yeoju.yeoju_app.payload.resDto.rektor.dashboard.StaffDatasForRektorDashboard;
-import uz.yeoju.yeoju_app.payload.resDto.rektor.dashboard.StudentDatasForRektorDashboard;
-import uz.yeoju.yeoju_app.payload.resDto.rektor.kafedraTeachers.KafedraTeachers28;
-import uz.yeoju.yeoju_app.payload.resDto.rektor.kafedraTeachers.KafedraTeachers29;
-import uz.yeoju.yeoju_app.payload.resDto.rektor.kafedraTeachers.KafedraTeachers30;
-import uz.yeoju.yeoju_app.payload.resDto.rektor.kafedraTeachers.KafedraTeachers31;
-import uz.yeoju.yeoju_app.payload.resDto.search.SearchDto;
-import uz.yeoju.yeoju_app.payload.resDto.user.UserField;
-import uz.yeoju.yeoju_app.payload.resDto.user.UserForTeacherSave;
-import uz.yeoju.yeoju_app.payload.resDto.user.UserForTeacherSaveItem;
-import uz.yeoju.yeoju_app.payload.resDto.user.UserResDto;
-
-import java.util.List;
 
 public interface UserRFIDRepository extends JpaRepository<User, String> {
 
