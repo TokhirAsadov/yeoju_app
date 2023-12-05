e7682127230181b9beb5035cf0927f1b44428e57
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-05 10:12:05 +0500

    /permissionForTeacherGrading/findAllPermissions api was created

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/uquvbulim/PermissionForTeacherGradingController.java
@@ -0,0 +1,29 @@
+package uz.yeoju.yeoju_app.controller.uquvbulim;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.enums.PPostStatus;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.uquvbulimi.CreatePermissionForTeacherGradingDto;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.permissionForTeacherGrading.PermissionForTeacherGradingService;
+
+import java.util.Set;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(BaseUrl.BASE_URL+"/permissionForTeacherGrading")
+public class PermissionForTeacherGradingController {
+    private final PermissionForTeacherGradingService service;
+
+    @GetMapping("/findAllPermissions")
+    public HttpEntity<?> findAllPermissions(){
+        return ResponseEntity.ok(service.findAllPermissionsForTeacherGrading());
+    }
+
+   
+}
