7c5d488dceb339bd602b05609926c092ac44ea21
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-27 14:24:02 +0500

    findAllQueue function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/queue/QueueService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/queue/QueueService.java
@@ -1,4 +1,7 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dekanat.queue;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
 public interface QueueService {
+    ApiResponse findAllQueue();
 }
