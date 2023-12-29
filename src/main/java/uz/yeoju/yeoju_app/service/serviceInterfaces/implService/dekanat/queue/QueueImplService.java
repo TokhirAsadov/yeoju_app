1ba0c95326126b3fbd169706dc128f744f23963b
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-29 16:58:40 +0500

    queueStatusChanger function was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/queue/QueueImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/queue/QueueImplService.java
@@ -51,6 +51,28 @@ public class QueueImplService implements QueueService{
             return new ApiResponse(false, "You already have a queue!. Please, wait...");
         }
     }
+    @Override
+    public ApiResponse queueStatusChanger(QueueStatusChangerDto dto) {
+        Optional<Queue> optionalQueue = queueRepository.findById(dto.id);
+        if (optionalQueue.isPresent()) {
+            Queue queue = optionalQueue.get();
+            queue.setStatus(dto.statusEnum);
+            if (dto.statusEnum== QueueStatusEnum.CALLED){
+                queue.setCalledAt(new Timestamp(System.currentTimeMillis()));
+            }else if (dto.statusEnum== QueueStatusEnum.RUNNING){
+                queue.setStartedAt(new Timestamp(System.currentTimeMillis()));
+            }else if (dto.statusEnum== QueueStatusEnum.WAITING){
+                queue.setWaitingAt(new Timestamp(System.currentTimeMillis()));
+            }else {
+                queue.setFinishedAt(new Timestamp(System.currentTimeMillis()));
+            }
+            queueRepository.save(queue);
+            return new ApiResponse(true,"Status of queue was changed to "+dto.statusEnum.name());
+        }
+        else {
+            return new ApiResponse(false,"Queue was not found by id: "+dto.id);
+        }
+    }
 
 
 }
