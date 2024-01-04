3102c9500d01332b91cf3540f811316be74f8620
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-04 09:21:07 +0500

    findAllQueueOfToday function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/queue/QueueService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/queue/QueueService.java
@@ -1,7 +1,10 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dekanat.queue;
 
+import uz.yeoju.yeoju_app.entity.dekanat.queue.QueueStatusEnum;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.dekanat.queue.QueueStatusChangerDto;
 
 public interface QueueService {
-    ApiResponse findAllQueue();
+    ApiResponse findAllQueueOfToday();
+
 }
