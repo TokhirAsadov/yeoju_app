776855dd497a78b0e5b179fc02f007f002d578ec
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-06 09:35:20 +0500

    getAllCounters native query was created

--- src/main/java/uz/yeoju/yeoju_app/repository/NotificationOuterCounterRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/NotificationOuterCounterRepository.java
@@ -14,6 +14,16 @@ public interface NotificationOuterCounterRepository extends JpaRepository<Notifi
     Long maxQueue();
 
     Boolean existsByUserIdAndNotificationOuterId(String user_id, String notificationOuter_id);
-
+    @Query(value = "select u.fullName,g.name as groupName,g.level,f.name as direction, NOC.id,ey.name as educationYear, n.fromDate,n.toDate,n.createdAt,n.queue,NOC.queue as studentQueue from NotificationOuter n\n" +
+            "   join EducationYear ey on ey.id = n.educationYear_id\n" +
+            "   join NotificationOuter_groups NOg on n.id = NOg.NotificationOuter_id\n" +
+            "   join groups g on NOg.groups_id = g.id\n" +
+            "    join Faculty f on g.faculty_id = f.id\n" +
+            "   join Student S on g.id = S.group_id\n" +
+            "   join users u on u.id=S.user_id\n" +
+            "   join NotificationOuterCounter NOC on n.id = NOC.notificationOuter_id\n" +
+            "   and NOC.user_id=u.id\n" +
+            " order by n.createdAt desc",nativeQuery = true)
+    Set<GetStudentNotificationOuters> getAllCounters();
 
 }
