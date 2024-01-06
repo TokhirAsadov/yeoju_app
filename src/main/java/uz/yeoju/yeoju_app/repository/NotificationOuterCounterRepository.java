c60954413f2f47399417ea23aae974ce95306d85
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-06 09:34:14 +0500

    maxQueue query was created

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/NotificationOuterCounterRepository.java
@@ -0,0 +1,17 @@
+package uz.yeoju.yeoju_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import uz.yeoju.yeoju_app.entity.dekanat.NotificationOuterCounter;
+import uz.yeoju.yeoju_app.payload.resDto.dekan.dekanat.GetStudentNotificationOuters;
+
+import java.util.Set;
+
+public interface NotificationOuterCounterRepository extends JpaRepository<NotificationOuterCounter,String> {
+
+    @Query(value = "select max(queue) from NotificationOuterCounter",nativeQuery = true)
+    Long maxQueue();
+
+
+}
