b3a35fb2fc33bcfcf514150a9d6e49d2e53c6c2e
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-09 11:04:24 +0500

    getStudentNotificationOuters2 function was created

--- src/main/java/uz/yeoju/yeoju_app/repository/NotificationOuterRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/NotificationOuterRepository.java
@@ -9,11 +9,23 @@ import uz.yeoju.yeoju_app.payload.resDto.dekan.dekanat.GetStudentNotificationOut
 import java.util.Set;
 
 public interface NotificationOuterRepository extends JpaRepository<NotificationOuter,String> {
-    @Query(value = "select n.id,ey.name as educationYear, n.fromDate,n.toDate,n.updatedAt from NotificationOuter n \n" +
+    @Query(value = "select n.id,ey.name as educationYear, n.fromDate,n.toDate,n.createdAt,n.queue from NotificationOuter n \n" +
             "join EducationYear ey on ey.id = n.educationYear_id\n" +
             "join NotificationOuter_groups NOg on n.id = NOg.NotificationOuter_id\n" +
             "join groups g on NOg.groups_id = g.id\n" +
             "join Student S on g.id = S.group_id\n" +
             "where s.user_id=:studentId order by n.createdAt desc ",nativeQuery = true)
     Set<GetStudentNotificationOuters> getStudentNotificationOuters(@Param("studentId") String studentId);
+
+
+    @Query(value = "select n.id,ey.name as educationYear, n.fromDate,n.toDate,n.createdAt,n.queue,NOC.queue as studentQueue from NotificationOuter n\n" +
+            "   join EducationYear ey on ey.id = n.educationYear_id\n" +
+            "   join NotificationOuter_groups NOg on n.id = NOg.NotificationOuter_id\n" +
+            "   join groups g on NOg.groups_id = g.id\n" +
+            "   join Student S on g.id = S.group_id\n" +
+            "   join NotificationOuterCounter NOC on n.id = NOC.notificationOuter_id                                                                              \n" +
+            "where s.user_id=:studentId and NOC.user_id=:studentId order by n.createdAt desc",nativeQuery = true)
+    Set<GetStudentNotificationOuters> getStudentNotificationOuters2(@Param("studentId") String studentId);
+
+
 }
