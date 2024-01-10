741314bd57b0e2d2f2b0109023678186ba54e0ec
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-10 16:42:43 +0500

    findAllByCreatedByOrderByCreatedAtDesc function was created

--- src/main/java/uz/yeoju/yeoju_app/repository/QueueRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/QueueRepository.java
@@ -1,7 +1,17 @@
 package uz.yeoju.yeoju_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.yeoju.yeoju_app.entity.dekanat.queue.Queue;
+import uz.yeoju.yeoju_app.entity.dekanat.queue.QueueStatusEnum;
+import uz.yeoju.yeoju_app.payload.resDto.dekan.queue.GetQueueRestDto;
+
+import java.util.Set;
 
 public interface QueueRepository extends JpaRepository<Queue,String> {
+
+//    Set<Queue> findAllByCreatedByOrderByCreatedAtDesc(String createdBy);
+    Set<Queue> findAllByCreatedByOrderByCreatedAtDesc(String createdBy);
+
+
 }
