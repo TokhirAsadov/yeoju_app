51b185bc0d6b10a3cee89860d563205e39e5907c
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-21 19:05:45 +0500

    DataOfLeaders was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/uquvbulim/DataOfLeaders.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.payload.resDto.uquvbulim;
+
+public interface DataOfLeaders {
+    String getHeadOfDepartment();
+    String getCourseLeader();
+    String getHeadOfAcademicAffair();
+}
