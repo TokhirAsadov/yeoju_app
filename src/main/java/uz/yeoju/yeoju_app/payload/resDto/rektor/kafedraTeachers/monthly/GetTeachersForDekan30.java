b6cc61641d00552fb21231f883263509b1f87b9f
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-25 13:22:03 +0500

    some fields were added

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/rektor/kafedraTeachers/monthly/GetTeachersForDekan30.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/rektor/kafedraTeachers/monthly/GetTeachersForDekan30.java
@@ -3,7 +3,7 @@ package uz.yeoju.yeoju_app.payload.resDto.rektor.kafedraTeachers.monthly;
 import org.springframework.beans.factory.annotation.Value;
 import uz.yeoju.yeoju_app.payload.resDto.attachment.UserPhotoRes;
 import uz.yeoju.yeoju_app.payload.resDto.kafedra.month.GetDate30;
-import uz.yeoju.yeoju_app.payload.resDto.kafedra.month.GetDate31;
+import uz.yeoju.yeoju_app.payload.resDto.user.UserForTeacherSaveItem;
 
 import java.util.Date;
 
@@ -16,6 +16,11 @@ public interface GetTeachersForDekan30 {
     String getEmail();
 
     Date getDate();
+
+    Float getRate();
+
+    @Value("#{@positionRepository.getUserPositionForEditUserPosition(target.id)}")
+    UserForTeacherSaveItem getUserPosition();
 
     @Value("#{@userPhotoRepo.getUserPhotoRes(target.id)}")
     UserPhotoRes getPhoto();
