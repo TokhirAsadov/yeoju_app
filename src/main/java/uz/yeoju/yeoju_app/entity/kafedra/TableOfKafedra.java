71f4fdba45a6cacb41eb29873c25d5781b0a7f07
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-30 16:39:14 +0500

    table object was created

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/kafedra/TableOfKafedra.java
@@ -0,0 +1,16 @@
+//package uz.yeoju.yeoju_app.entity.kafedra;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+//
+//import javax.persistence.Entity;
+//
+//@Entity
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class TableOfKafedra extends AbsEntity {
+//
+//}
