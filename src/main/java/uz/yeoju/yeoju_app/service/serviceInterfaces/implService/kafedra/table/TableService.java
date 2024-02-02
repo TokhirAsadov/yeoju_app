b180733bbdba3ae84f26201c08dee0d9a5213b8b
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-02 15:56:44 +0500

    TableService was created

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/table/TableService.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.kafedra.table;
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
+public interface TableService {
+
+}
