2583a8d2f892ecce330f3ac480b1d6e7bca51f46
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-02 18:11:02 +0500

    type of return was changed

--- src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfKafedraRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfKafedraRepository.java
@@ -3,9 +3,11 @@ package uz.yeoju.yeoju_app.repository.kafedra;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.kafedra.TableOfKafedra;
 
+import java.util.List;
+
 public interface TableOfKafedraRepository extends JpaRepository<TableOfKafedra,String> {
 
-    TableOfKafedra findAllByKafedraIdOrderByCreatedAtDesc(String kafedra_id);
+    List<TableOfKafedra> findAllByKafedraIdOrderByCreatedAtDesc(String kafedra_id);
     TableOfKafedra findByFileName(String fileName);
     Boolean existsByYearAndMonthAndKafedraId(Integer year, String month, String kafedra_id);
 }
