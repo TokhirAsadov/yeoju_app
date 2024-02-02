572f27c4e3abf60087e1941d41a7e07ce2b76a04
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-02 14:49:09 +0500

    Fields of TableOfKafedra were added

--- src/main/java/uz/yeoju/yeoju_app/entity/kafedra/TableOfKafedra.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/kafedra/TableOfKafedra.java
@@ -1,16 +1,23 @@
-//package uz.yeoju.yeoju_app.entity.kafedra;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
-//
-//import javax.persistence.Entity;
-//
-//@Entity
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class TableOfKafedra extends AbsEntity {
-//
-//}
+package uz.yeoju.yeoju_app.entity.kafedra;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TableOfKafedra extends AbsEntity {
+    private Integer year;
+    private String month;
+    @ManyToOne
+    private Kafedra kafedra;
+    private String fileName;
+
+
+}
