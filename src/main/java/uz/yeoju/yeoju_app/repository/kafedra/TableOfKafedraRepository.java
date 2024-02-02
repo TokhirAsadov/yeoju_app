01b29dd718bef674c40364f8fb18a705255420c0
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-02 14:44:25 +0500

    TableOfKafedraRepository was created

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfKafedraRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository.kafedra;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.kafedra.TableOfKafedra;
+
+public interface TableOfKafedraRepository extends JpaRepository<TableOfKafedra,String> {
+}
