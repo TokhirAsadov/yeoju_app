9e39681ab27f8dbe7712059f8d47d9de9c1f6d28
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-02 17:23:44 +0500

    findByName function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/table/TableService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/table/TableService.java
@@ -4,7 +4,12 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 
+import java.io.IOException;
+
 public interface TableService {
     ApiResponse saveFileToSystem(User user, MultipartHttpServletRequest request, Integer year, String month,String kafedraId, String id);
     ApiResponse findByKafedraId(String kafedraId);
+
+    ApiResponse findByName(String name);
+
 }
