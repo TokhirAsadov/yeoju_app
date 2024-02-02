af63e4f0e2ed554b94ce20e2d8f7b7e2a532937b
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-02 16:49:34 +0500

    existsByYearAndMonthAndKafedraId function was added

--- src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfKafedraRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfKafedraRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.kafedra.TableOfKafedra;
 
 public interface TableOfKafedraRepository extends JpaRepository<TableOfKafedra,String> {
+    Boolean existsByYearAndMonthAndKafedraId(Integer year, String month, String kafedra_id);
 }
