5ca04d7b84d6ebcaaa87f692ec1752d1a713da08
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-02 17:24:12 +0500

    findByFileName function was created

--- src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfKafedraRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfKafedraRepository.java
@@ -6,5 +6,6 @@ import uz.yeoju.yeoju_app.entity.kafedra.TableOfKafedra;
 public interface TableOfKafedraRepository extends JpaRepository<TableOfKafedra,String> {
 
     TableOfKafedra findAllByKafedraIdOrderByCreatedAtDesc(String kafedra_id);
+    TableOfKafedra findByFileName(String fileName);
     Boolean existsByYearAndMonthAndKafedraId(Integer year, String month, String kafedra_id);
 }
