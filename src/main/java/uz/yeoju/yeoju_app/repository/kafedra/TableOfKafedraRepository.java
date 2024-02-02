755c0a73253de4bc277363aaa68d46e7459f4ae5
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-02 17:05:35 +0500

    findAllByKafedraIdOrderByCreatedAtDesc function was created

--- src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfKafedraRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfKafedraRepository.java
@@ -5,6 +5,6 @@ import uz.yeoju.yeoju_app.entity.kafedra.TableOfKafedra;
 
 public interface TableOfKafedraRepository extends JpaRepository<TableOfKafedra,String> {
 
-    TableOfKafedra findAllByKafedraId(String kafedra_id);
-    Boolean existsByYearAndMonthAndKafedraIdOrderByCreatedAtDesc(Integer year, String month, String kafedra_id);
+    TableOfKafedra findAllByKafedraIdOrderByCreatedAtDesc(String kafedra_id);
+    Boolean existsByYearAndMonthAndKafedraId(Integer year, String month, String kafedra_id);
 }
