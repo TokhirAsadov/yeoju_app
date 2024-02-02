0dfc040a8f9808c0ca02f1bc5c7356786dd90574
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-02 15:58:19 +0500

    saveFileToSystem function was override and done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/table/TableImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/table/TableImplService.java
@@ -28,6 +28,95 @@ public class TableImplService implements TableService{
     private final TableOfKafedraRepository repository;
     private final KafedraRepository kafedraRepository;
 
+    @Override
+    public ApiResponse saveFileToSystem(User user, MultipartHttpServletRequest request, Integer year, String month,String kafedraId, String id) {
+        Path folder = Paths.get("tables");
 
+        if (Files.exists(folder)) {
+            // file exist
+            System.out.println("File exists");
+        }
+        if (Files.notExists(folder)) {
+            // file is not exist
+            System.out.println("file is not exist");
+            File f = new File("tables");
+            f.mkdir();
+        }
+        if (id==null || id.equals("")) {
+            Path yearPath = Paths.get(String.valueOf(year));
+            if (Files.exists(yearPath)) {
+                // file exist
+                System.out.println("File exists");
+            }
+            if (Files.notExists(yearPath)) {
+                // file is not exist
+                System.out.println("file is not exist");
+                File f = new File(String.valueOf(year));
+                f.mkdir();
+            }
+
+            Path path = Paths.get("tables\\" + year + "\\" + month);
+
+            if(Files.notExists(path)) {
+                File f = new File("tables\\" + year + "\\" + month);
+                f.mkdirs();
+            }
+
+
+            Iterator<String> fileNames = request.getFileNames();
+            while (fileNames.hasNext()) {
+                MultipartFile file = request.getFile(fileNames.next());
+                if (file != null) {
+                    System.out.println(file.getOriginalFilename());
+                    String substring = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.'));
+                    String name = String.valueOf(year)+"-"+month + "_" + UUID.randomUUID();
+                    Path filePath = Paths.get("tables\\" + year + "\\" + month + "\\" + name + substring);
+                    try {
+                        Files.copy(file.getInputStream(), filePath);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    TableOfKafedra table = new TableOfKafedra();
+                    table.setYear(year);
+                    table.setMonth(month);
+                    table.setFileName(name);
+                    Optional<Kafedra> optionalKafedra = kafedraRepository.findById(kafedraId);
+                    if (optionalKafedra.isPresent()) {
+                        Kafedra kafedra = optionalKafedra.get();
+                        if (kafedra.getOwner().getId().equals(user.getId())) {
+                            table.setKafedra(optionalKafedra.get());
+                            repository.save(table);
+                            return new ApiResponse(true,"Table was saved successfully.");
+                        }
+                        else {
+                            try {
+                                FileUtils.forceDelete(FileUtils.getFile("tables\\" + year + "\\" + month + "\\" + name + substring));
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            return new ApiResponse(false,"🚷❌Siz qoida buzarlik qilyapsiz❌🚷. Siz "+kafedra.getNameEn()+" kafedrasining kafedra mudiri emassiz!..");
+                        }
+                    }
+                    else {
+                        try {
+                            FileUtils.forceDelete(FileUtils.getFile("tables\\" + year + "\\" + month + "\\" + name + substring));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return new ApiResponse(false,"not found kafedra by id!..");
+                    }
+
+                }
+                else {
+                    return new ApiResponse(false,"error. file is empty");
+                }
+            }
+            return new ApiResponse(false,"error. file is empty");
+        }
+        else {
+            return new ApiResponse(false,"update uchun yoziw kk");
+        }
+    }
 
 }
