ca10476abc28e93899f9f1dc9243e3eaba4a0241
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 20:00:39 +0500

    checkerThatExistsTeachersFreeHours function was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursImplService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.entity.educationYear.EducationYear;
-import uz.yeoju.yeoju_app.entity.module.GradeOfStudentByTeacher;
 import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.teacher.TeachersFreeHoursDto;
@@ -25,6 +24,19 @@ public class TeachersFreeHoursImplService implements TeachersFreeHoursService{
     public ApiResponse getAllHoursByTeacherId(String teacherId) {
         List<TeachersFreeHours> freeHours = repository.findAllByCreatedByOrderByCreatedAt(teacherId);
         return new ApiResponse(true,"Teacher's free hours",freeHours);
+    }
+
+    @Override
+    public ApiResponse checkerThatExistsTeachersFreeHours(String educationYearId,String teacherId) {
+        Boolean bool = repository.existsByEducationYearIdAndCreatedBy(educationYearId, teacherId);
+        if (bool){
+            return new ApiResponse(true,"All things have been excellent!.");
+        }
+        else {
+            List<TeachersFreeHours> hoursList = repository.findAllByCreatedByOrderByCreatedAt(teacherId);
+            repository.deleteAll(hoursList);
+            return new ApiResponse(false,"You must to create your free hours for retakes..");
+        }
     }
 
     @Override
