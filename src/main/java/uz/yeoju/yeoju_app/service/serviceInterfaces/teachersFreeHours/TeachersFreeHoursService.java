dab9d2f96f29f0f171f99472c20142faa5321b14
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 18:26:48 +0500

    deleteFreeHours was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursService.java
@@ -3,9 +3,11 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.teachersFreeHours;
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.teacher.TeachersFreeHoursDto;
 
 public interface TeachersFreeHoursService {
 
     ApiResponse getAllHoursByTeacherId(String teacherId);
-    ApiResponse createNewHour(User user, TeachersFreeHours hour);
+    ApiResponse createNewHour(User user, TeachersFreeHoursDto dto);
+    ApiResponse deleteFreeHours(User user, String id);
 }
