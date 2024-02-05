c75d7a8311b211f5aefd555f795e7189d3cf753a
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 17:49:16 +0500

    getAllHoursByTeacherId was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursService.java
@@ -1,4 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.teachersFreeHours;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
 public interface TeachersFreeHoursService {
+
+    ApiResponse getAllHoursByTeacherId(String teacherId);
 }
