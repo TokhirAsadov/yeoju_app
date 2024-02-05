38cb2c613aaa5687f798d5fab365b91dded103ad
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 17:42:59 +0500

    New Object TeachersFreeDay was created

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/teacher/TeachersFreeDay.java
@@ -0,0 +1,29 @@
+package uz.yeoju.yeoju_app.entity.teacher;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.educationYear.EducationYear;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TeachersFreeDay extends AbsEntity {
+
+    @ManyToOne
+    private EducationYear educationYear;
+    private String day;
+    private String schedule;
+
+    public TeachersFreeDay(String id, EducationYear educationYear, String day, String schedule) {
+        super(id);
+        this.educationYear = educationYear;
+        this.day = day;
+        this.schedule = schedule;
+    }
+}
