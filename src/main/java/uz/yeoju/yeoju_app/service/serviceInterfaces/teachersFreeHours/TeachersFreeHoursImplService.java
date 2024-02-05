2f552022be1996f7bbff06207282bf1c4fda0c5f
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 17:50:43 +0500

    createNewHour was override

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursImplService.java
@@ -2,6 +2,8 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.teachersFreeHours;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.repository.TeachersFreeHoursRepository;
 
@@ -13,6 +15,11 @@ public class TeachersFreeHoursImplService implements TeachersFreeHoursService{
 
     @Override
     public ApiResponse getAllHoursByTeacherId(String teacherId) {
+        return null;
+    }
+
+    @Override
+    public ApiResponse createNewHour(User user, TeachersFreeHours hour) {
         return null;
     }
 }
