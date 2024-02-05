41d52657212f230c59e086fbcf704d7c3fd30156
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 17:50:31 +0500

    createNewHour was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursService.java
@@ -1,8 +1,11 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.teachersFreeHours;
 
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 
 public interface TeachersFreeHoursService {
 
     ApiResponse getAllHoursByTeacherId(String teacherId);
+    ApiResponse createNewHour(User user, TeachersFreeHours hour);
 }
