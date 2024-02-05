1a258a75e4b033ddcc86ec315679a0587f91207b
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 18:25:35 +0500

    createNewHour was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursImplService.java
@@ -3,15 +3,21 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.teachersFreeHours;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.educationYear.EducationYear;
 import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.teacher.TeachersFreeHoursDto;
 import uz.yeoju.yeoju_app.repository.TeachersFreeHoursRepository;
+import uz.yeoju.yeoju_app.repository.educationYear.EducationYearRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class TeachersFreeHoursImplService implements TeachersFreeHoursService{
 
     private final TeachersFreeHoursRepository repository;
+    private final EducationYearRepository educationYearRepository;
 
     @Override
     public ApiResponse getAllHoursByTeacherId(String teacherId) {
@@ -19,7 +25,18 @@ public class TeachersFreeHoursImplService implements TeachersFreeHoursService{
     }
 
     @Override
-    public ApiResponse createNewHour(User user, TeachersFreeHours hour) {
-        return null;
+    public ApiResponse createNewHour(User user, TeachersFreeHoursDto dto) {
+        Optional<EducationYear> optionalEducationYear = educationYearRepository.findById(dto.getEducationYearId());
+        if (optionalEducationYear.isPresent()) {
+            repository.save(new TeachersFreeHours(
+                    optionalEducationYear.get(),
+                    dto.day,
+                    dto.schedule
+            ));
+            return new ApiResponse(true,"free-hour was created successfully");
+        }
+        else {
+            return new ApiResponse(false,"education year not found by id: "+dto.getEducationYearId());
+        }
     }
 }
