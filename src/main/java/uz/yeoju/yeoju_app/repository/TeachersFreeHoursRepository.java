ebc69f971ba38ec1f672776a808dbf4d27462042
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 18:32:56 +0500

    findAllByCreatedByOrderByCreatedAt was creaated

--- src/main/java/uz/yeoju/yeoju_app/repository/TeachersFreeHoursRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/TeachersFreeHoursRepository.java
@@ -3,8 +3,11 @@ package uz.yeoju.yeoju_app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeachersFreeHoursRepository extends JpaRepository<TeachersFreeHours,String> {
     Optional<TeachersFreeHours> findByIdAndCreatedBy(String id, String createdBy);
+
+    List<TeachersFreeHours> findAllByCreatedByOrderByCreatedAt(String createdBy);
 }
