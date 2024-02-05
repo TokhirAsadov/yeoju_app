9adaac5a5786be178a2ec0e82eb0f9cd0112402f
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 15:35:53 +0500

    failGrade field and constructor were added

--- src/main/java/uz/yeoju/yeoju_app/payload/module/CreateGradeOfStudentByTeacher.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/module/CreateGradeOfStudentByTeacher.java
@@ -10,10 +10,21 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 public class CreateGradeOfStudentByTeacher {
     private String id;
+    private String failGradeId;
     private Float grade;
     private Timestamp time;
     private String description;
     private String studentId;
     private String subjectId;
     private String educationYearId;
+
+    public CreateGradeOfStudentByTeacher(String id, Float grade, Timestamp time, String description, String studentId, String subjectId, String educationYearId) {
+        this.id = id;
+        this.grade = grade;
+        this.time = time;
+        this.description = description;
+        this.studentId = studentId;
+        this.subjectId = subjectId;
+        this.educationYearId = educationYearId;
+    }
 }
