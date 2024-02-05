edbedb1ededf3f5b621371033db16f13512d7251
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 18:29:56 +0500

    findByIdAndCreatedBy was created

--- src/main/java/uz/yeoju/yeoju_app/repository/TeachersFreeHoursRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/TeachersFreeHoursRepository.java
@@ -3,5 +3,8 @@ package uz.yeoju.yeoju_app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
 
+import java.util.Optional;
+
 public interface TeachersFreeHoursRepository extends JpaRepository<TeachersFreeHours,String> {
+    Optional<TeachersFreeHours> findByIdAndCreatedBy(String id, String createdBy);
 }
