fa4a6b19dc1408cc1ea12f50c5823ab65558853d
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 17:44:50 +0500

    TeachersFreeHoursRepository was created

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/TeachersFreeHoursRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
+
+public interface TeachersFreeHoursRepository extends JpaRepository<TeachersFreeHours,String> {
+}
