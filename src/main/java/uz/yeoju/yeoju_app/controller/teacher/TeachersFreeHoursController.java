ad2143177d1aedd41a4629ec0e460919974c4fca
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-05 20:01:26 +0500

    /teachersFreeHours/checkerThatExistsTeachersFreeHours/{educationYearId}/{teacherId} api was created

--- src/main/java/uz/yeoju/yeoju_app/controller/teacher/TeachersFreeHoursController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/teacher/TeachersFreeHoursController.java
@@ -1,16 +1,25 @@
 package uz.yeoju.yeoju_app.controller.teacher;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
-import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.teacher.TeacherService;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.teacher.TeachersFreeHoursDto;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.teachersFreeHours.TeachersFreeHoursService;
 
 @RestController
 @RequestMapping(BaseUrl.BASE_URL+"/teachersFreeHours")
 public class TeachersFreeHoursController {
     @Autowired
-    private TeacherService service;
+    private TeachersFreeHoursService service;
 
+    @GetMapping("/checkerThatExistsTeachersFreeHours/{educationYearId}/{teacherId}")
+    public HttpEntity<?> checkerThatExistsTeachersFreeHours(@PathVariable("educationYearId") String educationYearId,@PathVariable("teacherId") String teacherId){
+        return ResponseEntity.ok(service.checkerThatExistsTeachersFreeHours(educationYearId,teacherId));
+    }
 
 }
