15a752bb5375180ade66598676828a95f4851647
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 18:49:29 +0500

    StudentsDynamicAttendance res dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/kafedra/StudentsDynamicAttendance.java
@@ -0,0 +1,16 @@
+package uz.yeoju.yeoju_app.payload.resDto.kafedra;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Date;
+
+public interface StudentsDynamicAttendance {
+    String getType();
+//    @JsonIgnore
+    String getCreatedBy();
+    Boolean getIsCome();
+    Date getTime();
+    Integer getWeek();
+    Integer getWeekday();
+    Integer getSection();
+}
