50aea78a432ab21973df9254c24976b86efa1ded
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 16:38:59 +0500

    hours were added

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/module/student/GetStudentGradesByLessons.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/module/student/GetStudentGradesByLessons.java
@@ -1,6 +1,8 @@
 package uz.yeoju.yeoju_app.payload.resDto.module.student;
 
 import org.springframework.beans.factory.annotation.Value;
+import uz.yeoju.yeoju_app.payload.resDto.kafedra.GetFreeHours;
+import uz.yeoju.yeoju_app.payload.resDto.kafedra.GetFreeHoursWithSubject;
 import uz.yeoju.yeoju_app.payload.resDto.module.GetGradesOfStudent;
 
 import java.util.Set;
@@ -17,6 +19,9 @@ public interface GetStudentGradesByLessons {
     String getLastName();
     String getMiddleName();
     String getFullName();
+
+    @Value("#{@teachersFreeHoursRepository.getFreeHours(target.groupId,target.educationYearId,target.lessonId)}")
+    Set<GetFreeHours> getHours();
 
     @Value("#{@gradeOfStudentByTeacherRepository.getMaxStep(target.educationYearId,target.lessonId,target.groupId)}")
     Long getMaxStep();
