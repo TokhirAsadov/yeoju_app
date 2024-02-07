a82e20fab1ce41e4b5cd58994e172837f1d1ff02
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 17:47:54 +0500

    import uz.yeoju.yeoju_app.entity.User;  was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceService.java
@@ -1,4 +1,9 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dynamicAttendance;
 
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.DynamicAttendanceDto;
+
 public interface DynamicAttendanceService {
+    ApiResponse createDynamicAttendance(User user, DynamicAttendanceDto dto);
 }
