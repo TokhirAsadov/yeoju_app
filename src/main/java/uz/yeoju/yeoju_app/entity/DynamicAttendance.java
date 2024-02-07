0e5230bdb9021f32333aab3c5cdac8a13ffe536e
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 17:47:05 +0500

    new fields were  added

--- src/main/java/uz/yeoju/yeoju_app/entity/DynamicAttendance.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/DynamicAttendance.java
@@ -6,14 +6,18 @@ import lombok.NoArgsConstructor;
 import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DynamicAttendance extends AbsEntity {
+    private Integer year;
     private Integer week;
     private Integer weekday;
     private Integer section;
     private Boolean isCome;
+    @ManyToOne
+    private User student;
 }
