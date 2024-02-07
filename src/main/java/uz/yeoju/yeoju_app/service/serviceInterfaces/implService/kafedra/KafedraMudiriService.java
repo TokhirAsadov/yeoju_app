f346ac7c6b07e919d1bd6fb8d1447561a3d2f39d
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 18:47:09 +0500

    changeRoomOfKafedra was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/KafedraMudiriService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/KafedraMudiriService.java
@@ -39,4 +39,5 @@ public interface KafedraMudiriService {
     ApiResponse getTeachersStatisticsForKafedraDashboard(String kafedraId);
 
     ApiResponse changeNameOfKafedra(ChangeKafedraNameDto dto);
+    ApiResponse changeRoomOfKafedra(ChangeKafedraNameDto dto);
 }
