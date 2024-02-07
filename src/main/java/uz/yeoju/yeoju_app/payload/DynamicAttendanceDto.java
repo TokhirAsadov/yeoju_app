d669f4d1f8897320aa99e4240fdaf66c52329ed1
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 17:46:41 +0500

    DynamicAttendanceDto dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/DynamicAttendanceDto.java
@@ -0,0 +1,27 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DynamicAttendanceDto {
+    public String id;
+    public Integer year;
+    public Integer week;
+    public Integer weekday;
+    public Integer section;
+    public Boolean isCome;
+    public String studentId;
+
+    public DynamicAttendanceDto(Integer year, Integer week, Integer weekday, Integer section, Boolean isCome, String studentId) {
+        this.year = year;
+        this.week = week;
+        this.weekday = weekday;
+        this.section = section;
+        this.isCome = isCome;
+        this.studentId = studentId;
+    }
+}
