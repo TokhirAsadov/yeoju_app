9b6ddad17be57050bbd4d1b23cea5af64c1f3f80
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 17:30:03 +0500

    DynamicAttendanceImplService was created

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceImplService.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dynamicAttendance;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DynamicAttendanceImplService implements DynamicAttendanceService {
+}
