eec5fe1237dec6842983372e9d0a57ac4094cc9d
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 15:26:15 +0500

    getFreeHoursWithSubject was created

--- src/main/java/uz/yeoju/yeoju_app/repository/TeachersFreeHoursRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/TeachersFreeHoursRepository.java
@@ -1,10 +1,14 @@
 package uz.yeoju.yeoju_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
+import uz.yeoju.yeoju_app.payload.resDto.kafedra.GetFreeHours;
+import uz.yeoju.yeoju_app.payload.resDto.kafedra.GetFreeHoursWithSubject;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface TeachersFreeHoursRepository extends JpaRepository<TeachersFreeHours,String> {
 
@@ -12,4 +16,12 @@ public interface TeachersFreeHoursRepository extends JpaRepository<TeachersFreeH
     Optional<TeachersFreeHours> findByIdAndCreatedBy(String id, String createdBy);
 
     List<TeachersFreeHours> findAllByCreatedByOrderByCreatedAt(String createdBy);
+
+    @Query(value = "select g.id as groupId, g.name as groupName, L.id as subjectId,L.name as subject, gcs.educationYear_id as educationYearId from GroupConnectSubject gcs\n" +
+            "    join GroupConnectSubject_Lesson GCSL on gcs.id = GCSL.GroupConnectSubject_id\n" +
+            "    join Lesson L on GCSL.lessons_id = L.id\n" +
+            "    join groups g on gcs.group_id = g.id\n" +
+            "where gcs.group_id=?1 and gcs.educationYear_id=?2",nativeQuery = true)
+    Set<GetFreeHoursWithSubject> getFreeHoursWithSubject(String groupId,String educationYearId);
+
 }
