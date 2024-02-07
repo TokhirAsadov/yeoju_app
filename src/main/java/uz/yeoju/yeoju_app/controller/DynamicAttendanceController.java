67b7bd006a2a5b2b1458432754e7fe29073a108c
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 17:50:44 +0500

    DynamicAttendanceController was created

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/DynamicAttendanceController.java
@@ -0,0 +1,23 @@
+package uz.yeoju.yeoju_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.*;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dynamicAttendance.DynamicAttendanceService;
+
+
+@RestController
+@RequestMapping(BaseUrl.BASE_URL+"/dynamicAttendance")
+@RequiredArgsConstructor
+public class DynamicAttendanceController {
+
+    public final DynamicAttendanceService service;
+
+
+
+
+}
