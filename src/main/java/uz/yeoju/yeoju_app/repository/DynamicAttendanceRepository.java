cc9ce2bf927c3ab2ce119b5cc4747261b8c57c09
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 17:24:33 +0500

    DynamicAttendanceRepository was created

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/DynamicAttendanceRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.DynamicAttendance;
+
+public interface DynamicAttendanceRepository extends JpaRepository<DynamicAttendance,String> {
+}
