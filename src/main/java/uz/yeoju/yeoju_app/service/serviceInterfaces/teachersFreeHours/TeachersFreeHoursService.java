a9e3bd2917a063c5b3afb261bbf2b3405b1473ab
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 15:25:40 +0500

    import code was removed

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/teachersFreeHours/TeachersFreeHoursService.java
@@ -1,7 +1,6 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.teachersFreeHours;
 
 import uz.yeoju.yeoju_app.entity.User;
-import uz.yeoju.yeoju_app.entity.teacher.TeachersFreeHours;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.teacher.TeachersFreeHoursDto;
 
