19a0fb0de9184535f7a9d9c260ef870f6fac4749
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-07 16:38:41 +0500

    @JsonIgnore was used

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/kafedra/GetFreeHoursWithSubject.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/kafedra/GetFreeHoursWithSubject.java
@@ -1,14 +1,18 @@
 package uz.yeoju.yeoju_app.payload.resDto.kafedra;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Set;
 
 public interface GetFreeHoursWithSubject {
+    @JsonIgnore
     String getGroupId();
     String getGroupName();
+    @JsonIgnore
     String getSubjectId();
     String getSubject();
+    @JsonIgnore
     String getEducationYearId();
 
     @Value("#{@teachersFreeHoursRepository.getFreeHours(target.groupId,target.educationYearId,target.subjectId)}")
