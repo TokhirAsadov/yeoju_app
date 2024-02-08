9444806df36cabcd9be06fee19f361913b63f4be
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-08 15:40:17 +0500

    handleMethodArgumentNotValid exception handler was created

--- src/main/java/uz/yeoju/yeoju_app/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/uz/yeoju/yeoju_app/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,9 @@
 package uz.yeoju.yeoju_app.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -30,7 +32,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                "Total errors:"+ex.getErrorCount()+", First error:"+ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 
 
 }
