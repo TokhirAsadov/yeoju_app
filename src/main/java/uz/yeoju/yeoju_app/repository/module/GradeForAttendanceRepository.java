dd1c4114d4749f8069150a8702e52a665a0fe096
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-09 15:29:04 +0500

    existTeachersLesson function was override

--- src/main/java/uz/yeoju/yeoju_app/repository/module/GradeForAttendanceRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/GradeForAttendanceRepository.java
@@ -1,7 +1,11 @@
 package uz.yeoju.yeoju_app.repository.module;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.yeoju.yeoju_app.entity.module.GradeForAttendance;
 
 public interface GradeForAttendanceRepository extends JpaRepository<GradeForAttendance,String> {
+
+    @Query(value = "select dbo.existTeachersLesson(?1,?2,?3,?4)",nativeQuery = true)
+    Boolean existTeachersLesson(String teacherId, String groupId,String educationYearId,String lessonId);
 }
