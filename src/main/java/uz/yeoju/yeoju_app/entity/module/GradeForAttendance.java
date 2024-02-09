fab466d02e888310c3b27e7573c74056d5b89ab3
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-09 14:59:51 +0500

    New object GradeForAttendance was created

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/module/GradeForAttendance.java
@@ -0,0 +1,28 @@
+package uz.yeoju.yeoju_app.entity.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.Group;
+import uz.yeoju.yeoju_app.entity.Lesson;
+import uz.yeoju.yeoju_app.entity.educationYear.EducationYear;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GradeForAttendance extends AbsEntity {
+    private Float grade;
+    @ManyToOne
+    private EducationYear educationYear;
+    @ManyToOne
+    private Lesson lesson;
+    @ManyToMany
+    private Set<Group> groups;
+}
