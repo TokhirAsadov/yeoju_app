8cb091117663937236f774da63c3b270da337d13
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-09 15:27:37 +0500

    createGradeForAttendance function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/gradeForAttendance/GradeForAttendanceService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/gradeForAttendance/GradeForAttendanceService.java
@@ -1,4 +1,9 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.gradeForAttendance;
 
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.module.GradeForAttendanceDto;
+
 public interface GradeForAttendanceService {
+    ApiResponse createGradeForAttendance(User user,GradeForAttendanceDto dto);
 }
