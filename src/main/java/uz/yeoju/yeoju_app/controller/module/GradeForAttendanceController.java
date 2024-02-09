93a74f374beeb74fdafab90608277f6fadd77e1f
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-09 16:05:58 +0500

    /gradeForAttendance/create api was created

--- src/main/java/uz/yeoju/yeoju_app/controller/module/GradeForAttendanceController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/module/GradeForAttendanceController.java
@@ -1,8 +1,14 @@
 package uz.yeoju.yeoju_app.controller.module;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.module.GradeForAttendanceDto;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.gradeForAttendance.GradeForAttendanceService;
 
 
@@ -13,4 +19,9 @@ public class GradeForAttendanceController {
     private final GradeForAttendanceService service;
 
 
+    @PostMapping("/create")
+    public HttpEntity<?> create(@CurrentUser User user, @RequestBody GradeForAttendanceDto dto){
+        ApiResponse apiResponse = service.createGradeForAttendance(user, dto);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 201 : 401).body(apiResponse);
+    }
 }
