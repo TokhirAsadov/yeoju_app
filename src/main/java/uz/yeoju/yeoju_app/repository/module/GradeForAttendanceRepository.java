c20be630a29b2e7e3f22d15bef6f378dd28f948e
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-09 15:00:50 +0500

    GradeForAttendanceRepository was created

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/GradeForAttendanceRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository.module;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.module.GradeForAttendance;
+
+public interface GradeForAttendanceRepository extends JpaRepository<GradeForAttendance,String> {
+}
