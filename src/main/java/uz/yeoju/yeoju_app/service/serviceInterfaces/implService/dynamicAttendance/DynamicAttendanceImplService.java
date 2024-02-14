0f744ed911a6c95b1673dd7e0a4393e288b28550
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-14 16:18:27 +0500

    createMultiDynamicAttendance function was override and done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceImplService.java
@@ -2,10 +2,13 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dynamicAttendan
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uz.yeoju.yeoju_app.entity.DynamicAttendance;
 import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.exceptions.UserNotFoundException;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.DynamicAttendanceDto;
+import uz.yeoju.yeoju_app.payload.MultiDynamicAttendanceDto;
 import uz.yeoju.yeoju_app.repository.DynamicAttendanceRepository;
 import uz.yeoju.yeoju_app.repository.UserRepository;
 
@@ -35,5 +38,29 @@ public class DynamicAttendanceImplService implements DynamicAttendanceService {
             return new ApiResponse(false,"Attendance already exists");
         }
 
+    }
+
+    @Override
+    @Transactional
+    public ApiResponse createMultiDynamicAttendance(User user, MultiDynamicAttendanceDto dto) {
+        dto.getStudentsIds().forEach(id ->{
+            Boolean exists = repository.existsByYearAndWeekAndWeekdayAndSectionAndCreatedByAndStudentIdAndRoom(dto.getYear(), dto.getWeek(), dto.getWeekday(), dto.getSection(), user.getId(),id, dto.room);
+            if (!exists) {
+                Optional<User> optionalUser = userRepository.findById(id);
+                if (optionalUser.isPresent()){
+                    repository.save(new DynamicAttendance(
+                            dto.getYear(), dto.getWeek(), dto.getWeekday(), dto.getSection(), dto.getIsCome(), dto.room, optionalUser.get()
+                    ));
+                }
+                else {
+                    throw new UserNotFoundException("Not found student by id: " + id);
+                }
+            }
+            else {
+                throw new UserNotFoundException("Attendance already exists");
+            }
+        });
+
+        return new ApiResponse(true,"Attendances were created successful!.");
     }
 }
