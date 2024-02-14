7dc3f3cda572dd54b53cf449d4052c2c9e4b7f86
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-14 16:18:02 +0500

    createMultiDynamicAttendance function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceService.java
@@ -3,7 +3,9 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dynamicAttendan
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.DynamicAttendanceDto;
+import uz.yeoju.yeoju_app.payload.MultiDynamicAttendanceDto;
 
 public interface DynamicAttendanceService {
     ApiResponse createDynamicAttendance(User user, DynamicAttendanceDto dto);
+    ApiResponse createMultiDynamicAttendance(User user, MultiDynamicAttendanceDto dto);
 }
