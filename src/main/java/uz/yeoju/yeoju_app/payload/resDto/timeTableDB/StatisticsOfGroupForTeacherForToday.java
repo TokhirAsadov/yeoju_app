2a027d7de798cf4b52b2fe21a798012369b74854
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-15 17:10:13 +0500

    field was added

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/StatisticsOfGroupForTeacherForToday.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/StatisticsOfGroupForTeacherForToday.java
@@ -2,8 +2,11 @@ package uz.yeoju.yeoju_app.payload.resDto.timeTableDB;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.beans.factory.annotation.Value;
+import uz.yeoju.yeoju_app.payload.resDto.kafedra.StudentsDynamicAttendance;
 
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public interface StatisticsOfGroupForTeacherForToday {
 //    @JsonIgnore
@@ -42,6 +45,22 @@ public interface StatisticsOfGroupForTeacherForToday {
 
     @Value("#{@groupConnectSubjectRepository.getSubjectsByEduYearIdAndGroupAndStudentId(target.groupId,target.educationId,target.subjectId,target.studentId,target.year,target.week,target.day)}")
     Set<StudentSubjectsByEduYearIdAndGroupAndStudentId> getSubjects();
+
+    default Float getAllGradesForAttendance(){
+        if (this.getGradeForAttendance() == null ||this.getGradeForAttendance()==0){
+            return (float) 0;
+        }
+        AtomicInteger counter = new AtomicInteger();
+        this.getSubjects().forEach(subject ->{
+            if (!subject.getStatistics().isEmpty()) {
+                Set<StudentsDynamicAttendance> dynamic = subject.getStatistics().stream().filter(stats -> stats.getType().equalsIgnoreCase("DYNAMIC") && !stats.getIsCome()).collect(Collectors.toSet());
+                if (dynamic.isEmpty()) {
+                    counter.getAndIncrement();
+                }
+            }
+        });
+        return counter.get()*this.getGradeForAttendance();
+    };
 
     @Value("#{@gradeOfStudentByTeacherRepository.getSumGrade(target.teacherId,target.studentId,target.educationId,target.subjectId)}")
     Double getSumGrade();
