4c92a13e49a8da7749e5d7cd250d6109ffb758dd
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-15 15:47:27 +0500

    getAllSumGrade was added

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/GetLessonsOfGroupForBall.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/GetLessonsOfGroupForBall.java
@@ -1,6 +1,11 @@
 package uz.yeoju.yeoju_app.payload.resDto.timeTableDB;
 
 import org.springframework.beans.factory.annotation.Value;
+import uz.yeoju.yeoju_app.payload.resDto.kafedra.StudentsDynamicAttendance;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public interface GetLessonsOfGroupForBall {
     String getEducationYearId();
@@ -11,4 +16,8 @@ public interface GetLessonsOfGroupForBall {
 
     @Value("#{@gradeOfStudentByTeacherRepository.getMiddleGrade(target.educationYearId,target.studentId,target.subjectId)}")
     Double getMiddleGrade();
+    @Value("#{@gradeOfStudentByTeacherRepository.getAllSumGrade(target.studentId,target.educationYearId,target.subjectId)}")
+    Double getAllSumGrade();
+
+
 }
