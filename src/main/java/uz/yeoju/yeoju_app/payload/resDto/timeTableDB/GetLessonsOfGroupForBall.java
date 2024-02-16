1caed37893f36a657fcc284611b57d7b0e960f30
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-16 17:43:23 +0500

    imports were removed

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/GetLessonsOfGroupForBall.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/GetLessonsOfGroupForBall.java
@@ -1,11 +1,8 @@
 package uz.yeoju.yeoju_app.payload.resDto.timeTableDB;
 
 import org.springframework.beans.factory.annotation.Value;
-import uz.yeoju.yeoju_app.payload.resDto.kafedra.StudentsDynamicAttendance;
 
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public interface GetLessonsOfGroupForBall {
     String getEducationYearId();
@@ -21,9 +18,6 @@ public interface GetLessonsOfGroupForBall {
 
     @Value("#{@gradeForAttendanceRepository.getValueOfGradeForAttendance(target.groupId,target.educationYearId,target.subjectId)}")
     Float getGradeForAttendance();
-
-//    @Value("#{@gradeForAttendanceRepository.getGradeForAttendance(target.groupId,target.educationYearId,target.subjectId)}")
-//    Float getGradeForAttendance();
 
     @Value("#{@groupConnectSubjectRepository.getSubjectsByEduYearIdAndGroupAndStudentId(target.groupId,target.educationYearId,target.subjectId,target.studentId)}")
     Set<StudentSubjectsByEduYearIdAndGroupAndStudentId> getSubjects();
