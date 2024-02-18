a450afa5c13ede7eeb2d2fa27bee5689ec7d26f5
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-18 23:38:38 +0500

    imports were removed

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/StudentWithAscAndDescDate.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/StudentWithAscAndDescDate.java
@@ -1,7 +1,5 @@
 package uz.yeoju.yeoju_app.payload.resDto.student;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 public interface StudentWithAscAndDescDate {
