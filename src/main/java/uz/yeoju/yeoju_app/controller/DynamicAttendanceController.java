b045edf0ecf57ff895602523ef425f33953a7176
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-21 18:54:20 +0500

    /dynamicAttendance/updateDynamicAttendance api was created

--- src/main/java/uz/yeoju/yeoju_app/controller/DynamicAttendanceController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/DynamicAttendanceController.java
@@ -9,6 +9,8 @@ import uz.yeoju.yeoju_app.payload.*;
 import uz.yeoju.yeoju_app.secret.CurrentUser;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dynamicAttendance.DynamicAttendanceService;
 
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping(BaseUrl.BASE_URL+"/dynamicAttendance")
@@ -19,6 +21,12 @@ public class DynamicAttendanceController {
 
     @PostMapping("/createDynamicAttendance")
     public HttpEntity<?> create(@CurrentUser User user, @RequestBody DynamicAttendanceDto dto){
+        ApiResponse response = service.createDynamicAttendance(user, dto);
+        return ResponseEntity.status(response.isSuccess() ? 201:402).body(response);
+    }
+
+    @PutMapping("/updateDynamicAttendance")
+    public HttpEntity<?> update(@CurrentUser User user,@Valid @RequestBody DynamicAttendanceDto dto){
         ApiResponse response = service.createDynamicAttendance(user, dto);
         return ResponseEntity.status(response.isSuccess() ? 201:402).body(response);
     }
