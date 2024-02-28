4840474771167ff19b7e3bc7550e9616e55a25b8
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-28 14:51:09 +0500

    MultiDynamicAttendance2Dto dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/MultiDynamicAttendance2Dto.java
@@ -0,0 +1,17 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MultiDynamicAttendance2Dto {
+    public Integer year;
+    public Integer week;
+    public Integer weekday;
+    public Set<ChildOfMultiDynamicAttendance2Dto> attendance;
+}
