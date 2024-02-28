0c06e4935b0d89a2f35e657a41a32e2a7817d26f
TokhirAsadov <guvalakat1603@gmail.com>
2024-02-28 14:51:46 +0500

    field was changed

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceService.java
@@ -3,11 +3,12 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dynamicAttendan
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.DynamicAttendanceDto;
+import uz.yeoju.yeoju_app.payload.MultiDynamicAttendance2Dto;
 import uz.yeoju.yeoju_app.payload.MultiDynamicAttendanceDto;
 
 public interface DynamicAttendanceService {
     ApiResponse createDynamicAttendance(User user, DynamicAttendanceDto dto);
     ApiResponse createMultiDynamicAttendance(User user, MultiDynamicAttendanceDto dto);
 
-    ApiResponse createMultiDynamicAttendance2(User user, MultiDynamicAttendanceDto dto);
+    ApiResponse createMultiDynamicAttendance2(User user, MultiDynamicAttendance2Dto dto);
 }
