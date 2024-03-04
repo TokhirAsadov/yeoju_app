52dcd65217b2466a2d2dcb6e404266f057c77016
TokhirAsadov <guvalakat1603@gmail.com>
2024-03-04 15:55:07 +0500

    updateTheme function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/themeOfSubjectForGradeByTeacher/ThemeOfSubjectForGradeByTeacherService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/themeOfSubjectForGradeByTeacher/ThemeOfSubjectForGradeByTeacherService.java
@@ -2,10 +2,12 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.themeOfS
 
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.module.CreateThemeOfSubjectForGradeDto;
+import uz.yeoju.yeoju_app.payload.module.UpdateThemeOfSubjectForGradeDto;
 
 public interface ThemeOfSubjectForGradeByTeacherService {
 
     ApiResponse createTheme(CreateThemeOfSubjectForGradeDto dto);
+    ApiResponse updateTheme(UpdateThemeOfSubjectForGradeDto dto);
     ApiResponse getThemeByLessonIdAndEducationYearIdAndCreatorId(String lessonId, String educationYearId, String creatorId);
 
 }
