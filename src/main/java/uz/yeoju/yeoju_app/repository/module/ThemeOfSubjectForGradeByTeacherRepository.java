76441d92ca173db24b34a81d5ddbfd0c057e582c
TokhirAsadov <guvalakat1603@gmail.com>
2024-03-04 15:46:47 +0500

    findAllByLessonIdAndEducationYearIdAndCreatedBy function was created

--- src/main/java/uz/yeoju/yeoju_app/repository/module/ThemeOfSubjectForGradeByTeacherRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/ThemeOfSubjectForGradeByTeacherRepository.java
@@ -3,6 +3,9 @@ package uz.yeoju.yeoju_app.repository.module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.module.ThemeOfSubjectForGradeByTeacher;
 
+import java.util.Set;
+
 public interface ThemeOfSubjectForGradeByTeacherRepository extends JpaRepository<ThemeOfSubjectForGradeByTeacher, String> {
+    Set<ThemeOfSubjectForGradeByTeacher> findAllByLessonIdAndEducationYearIdAndCreatedBy(String lesson_id, String educationYear_id, String createdBy);
 
 }
