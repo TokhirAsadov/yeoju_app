ee7a40c8bb3493359b835473e5604f6ba1dbba52
TokhirAsadov <guvalakat1603@gmail.com>
2024-03-04 15:41:54 +0500

    createTheme function was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/themeOfSubjectForGradeByTeacher/ThemeOfSubjectForGradeByTeacherImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/themeOfSubjectForGradeByTeacher/ThemeOfSubjectForGradeByTeacherImplService.java
@@ -2,6 +2,14 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.themeOfS
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.Lesson;
+import uz.yeoju.yeoju_app.entity.educationYear.EducationYear;
+import uz.yeoju.yeoju_app.entity.module.ThemeOfSubjectForGradeByTeacher;
+import uz.yeoju.yeoju_app.exceptions.UserNotFoundException;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.module.CreateThemeOfSubjectForGradeDto;
+import uz.yeoju.yeoju_app.repository.LessonRepository;
+import uz.yeoju.yeoju_app.repository.educationYear.EducationYearRepository;
 import uz.yeoju.yeoju_app.repository.module.ThemeOfSubjectForGradeByTeacherRepository;
 
 @Service
@@ -9,5 +17,27 @@ import uz.yeoju.yeoju_app.repository.module.ThemeOfSubjectForGradeByTeacherRepos
 public class ThemeOfSubjectForGradeByTeacherImplService implements ThemeOfSubjectForGradeByTeacherService{
 
     private final ThemeOfSubjectForGradeByTeacherRepository repository;
+    private final EducationYearRepository educationYearRepository;
+    private final LessonRepository lessonRepository;
 
+    @Override
+    public ApiResponse createTheme(CreateThemeOfSubjectForGradeDto dto) {
+        boolean existsEducationYear = educationYearRepository.existsById(dto.educationYearId);
+        if (existsEducationYear){
+            boolean existsLesson = lessonRepository.existsById(dto.subjectId);
+            if (existsLesson){
+                EducationYear educationYear = educationYearRepository.getById(dto.educationYearId);
+                Lesson lesson = lessonRepository.getById(dto.subjectId);
+                repository.save(new ThemeOfSubjectForGradeByTeacher(dto.name, lesson,educationYear));
+                return new ApiResponse(true,"Theme was saved successfully!.");
+            }
+            else {
+                throw new UserNotFoundException("Lesson was not found by id: "+dto.subjectId);
+            }
+        }
+        else {
+            throw new UserNotFoundException("Education year was not found by id: "+dto.educationYearId);
+        }
+
+    }
 }
