0cc0178de29f9fc1e19a2cd6272a29b3bbcb91b9
TokhirAsadov <guvalakat1603@gmail.com>
2024-03-04 15:24:11 +0500

    ThemeOfSubjectForGradeByTeacherRepository repository was created

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/ThemeOfSubjectForGradeByTeacherRepository.java
@@ -0,0 +1,8 @@
+package uz.yeoju.yeoju_app.repository.module;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.module.ThemeOfSubjectForGradeByTeacher;
+
+public interface ThemeOfSubjectForGradeByTeacherRepository extends JpaRepository<ThemeOfSubjectForGradeByTeacher, String> {
+
+}
