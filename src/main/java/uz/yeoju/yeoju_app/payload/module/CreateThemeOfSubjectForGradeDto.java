76803d9ef04ff86effcf03d9783955398cd77be3
TokhirAsadov <guvalakat1603@gmail.com>
2024-03-04 15:41:29 +0500

    CreateThemeOfSubjectForGradeDto dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/module/CreateThemeOfSubjectForGradeDto.java
@@ -0,0 +1,17 @@
+package uz.yeoju.yeoju_app.payload.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateThemeOfSubjectForGradeDto {
+    @Size(min = 3,message = "Mazvu nomining uzunligi kamida 4 bo`lishi kerak.")
+    public String name;
+    public String subjectId;
+    public String educationYearId;
+}
