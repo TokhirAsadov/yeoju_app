a5512233113994ebf17c9daf30f60b168746ea8f
TokhirAsadov <guvalakat1603@gmail.com>
2024-03-04 15:41:45 +0500

    createTheme function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/themeOfSubjectForGradeByTeacher/ThemeOfSubjectForGradeByTeacherService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/themeOfSubjectForGradeByTeacher/ThemeOfSubjectForGradeByTeacherService.java
@@ -1,4 +1,10 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.themeOfSubjectForGradeByTeacher;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.module.CreateThemeOfSubjectForGradeDto;
+
 public interface ThemeOfSubjectForGradeByTeacherService {
+
+    ApiResponse createTheme(CreateThemeOfSubjectForGradeDto dto);
+
 }
