b42395dada27a1dfd378d2e9faa89c546315015a
whitecode <guvalakat1603@gmail.com>
2024-03-12 13:33:15 +0500

    /themeOfSubjectForGrading/createThemeWithGrade api was created

--- src/main/java/uz/yeoju/yeoju_app/controller/module/ThemeOfSubjectForGradeByTeacherController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/module/ThemeOfSubjectForGradeByTeacherController.java
@@ -1,12 +1,28 @@
 package uz.yeoju.yeoju_app.controller.module;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.module.CreateGradesWithThemeDto;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.themeOfSubjectForGradeByTeacher.ThemeOfSubjectForGradeByTeacherService;
 
 @RestController
 @RequestMapping(BaseUrl.BASE_URL+"/themeOfSubjectForGrading")
 @RequiredArgsConstructor
 public class ThemeOfSubjectForGradeByTeacherController {
+    private final ThemeOfSubjectForGradeByTeacherService service;
+
+    @PostMapping("/createThemeWithGrade")
+    HttpEntity<?> createThemeWithGrade(@CurrentUser User user, @RequestBody CreateGradesWithThemeDto dto){
+        ApiResponse themeWithGrade = service.createThemeWithGrade(user, dto);
+        return ResponseEntity.status(201).body(themeWithGrade);
+    }
 }
