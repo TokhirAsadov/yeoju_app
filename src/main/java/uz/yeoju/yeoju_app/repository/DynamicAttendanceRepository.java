67cdfb794a912ea17867ae28a1cf774536a1e340
whitecode <guvalakat1603@gmail.com>
2024-03-13 17:29:08 +0500

    existsByIdAndCreatedBy function was created

--- src/main/java/uz/yeoju/yeoju_app/repository/DynamicAttendanceRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/DynamicAttendanceRepository.java
@@ -5,4 +5,5 @@ import uz.yeoju.yeoju_app.entity.DynamicAttendance;
 
 public interface DynamicAttendanceRepository extends JpaRepository<DynamicAttendance,String> {
     Boolean existsByYearAndWeekAndWeekdayAndSectionAndCreatedByAndStudentIdAndRoom(Integer year, Integer week, Integer weekday, Integer section, String createdBy, String student_id, String room);
+    Boolean existsByIdAndCreatedBy(String id, String createdBy);
 }
