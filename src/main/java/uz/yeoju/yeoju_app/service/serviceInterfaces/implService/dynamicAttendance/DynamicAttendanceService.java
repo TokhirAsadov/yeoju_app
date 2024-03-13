7151188923bdff5e86dfe29d8e07e27184e186fb
whitecode <guvalakat1603@gmail.com>
2024-03-13 17:30:03 +0500

    type of variable was changed

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dynamicAttendance/DynamicAttendanceService.java
@@ -3,10 +3,12 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dynamicAttendan
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.payload.*;
 
+import java.util.Set;
+
 public interface DynamicAttendanceService {
     ApiResponse createDynamicAttendance(User user, DynamicAttendanceDto dto);
     ApiResponse createMultiDynamicAttendance(User user, MultiDynamicAttendanceDto dto);
 
     ApiResponse createMultiDynamicAttendance2(User user, MultiDynamicAttendance2Dto dto);
-    ApiResponse updateMultiDynamicAttendance2(User user, UpdateMultiDynamicAttendanceDto dto);
+    ApiResponse updateMultiDynamicAttendance2(User user, Set<UpdateMultiDynamicAttendanceDto> dto);
 }
