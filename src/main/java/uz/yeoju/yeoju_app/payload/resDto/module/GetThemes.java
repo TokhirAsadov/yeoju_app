df1b7b26f26a05e051d0939c44c3d57f99e97ee9
whitecode <guvalakat1603@gmail.com>
2024-03-15 16:12:34 +0500

    GetThemes resdto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/module/GetThemes.java
@@ -0,0 +1,19 @@
+package uz.yeoju.yeoju_app.payload.resDto.module;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+public interface GetThemes {
+    String getId();
+    String getName();
+    String getGroupId();
+    String getLessonId();
+    String getEducationYearId();
+    String getCreatedBy();
+    Timestamp getCreatedAt();
+
+    @Value("#{@themeOfSubjectForGradeByTeacherRepository.getThemesItems(target.id,target.groupId,target.lessonId,target.educationYearId,target.createdBy)}")
+    Set<GetThemesItems> getStudents();
+}
