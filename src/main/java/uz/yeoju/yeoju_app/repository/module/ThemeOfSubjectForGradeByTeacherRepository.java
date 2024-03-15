d2d141678c1f9e2ba1df6a4859ee7c2fff6d0f0f
whitecode <guvalakat1603@gmail.com>
2024-03-15 15:08:38 +0500

    functions were changed

--- src/main/java/uz/yeoju/yeoju_app/repository/module/ThemeOfSubjectForGradeByTeacherRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/ThemeOfSubjectForGradeByTeacherRepository.java
@@ -3,14 +3,13 @@ package uz.yeoju.yeoju_app.repository.module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.module.ThemeOfSubjectForGradeByTeacher;
 
-import javax.validation.constraints.Size;
 import java.util.Set;
 
 public interface ThemeOfSubjectForGradeByTeacherRepository extends JpaRepository<ThemeOfSubjectForGradeByTeacher, String> {
 
     Boolean existsByNameAndLessonIdAndEducationYearIdAndCreatedBy(String name, String lesson_id, String educationYear_id, String createdBy);
-    Set<ThemeOfSubjectForGradeByTeacher> findAllByLessonIdAndEducationYearIdAndCreatedByOrderByCreatedAtDesc(String lesson_id, String educationYear_id, String createdBy);
+    Set<ThemeOfSubjectForGradeByTeacher> findAllByGroupIdAndLessonIdAndEducationYearIdAndCreatedByOrderByCreatedAtDesc(String group_id, String lesson_id, String educationYear_id, String createdBy);
 
-    ThemeOfSubjectForGradeByTeacher findFirstByLessonIdAndEducationYearIdAndCreatedByOrderByCreatedAtDesc(String lesson_id, String educationYear_id, String createdBy);
+    ThemeOfSubjectForGradeByTeacher findFirstByGroupIdAndLessonIdAndEducationYearIdAndCreatedByOrderByCreatedAtDesc(String group_id, String lesson_id, String educationYear_id, String createdBy);
 
 }
