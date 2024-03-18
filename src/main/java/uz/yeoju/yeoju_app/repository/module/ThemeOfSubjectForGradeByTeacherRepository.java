f60edffade68b8bf968acf817fa2d9765ecc094c
whitecode <guvalakat1603@gmail.com>
2024-03-18 11:24:28 +0500

    getThemesItems query was created

--- src/main/java/uz/yeoju/yeoju_app/repository/module/ThemeOfSubjectForGradeByTeacherRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/ThemeOfSubjectForGradeByTeacherRepository.java
@@ -1,7 +1,10 @@
 package uz.yeoju.yeoju_app.repository.module;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.yeoju.yeoju_app.entity.module.ThemeOfSubjectForGradeByTeacher;
+import uz.yeoju.yeoju_app.payload.resDto.module.GetThemes;
+import uz.yeoju.yeoju_app.payload.resDto.module.GetThemesItems;
 
 import java.util.Set;
 
@@ -11,5 +14,12 @@ public interface ThemeOfSubjectForGradeByTeacherRepository extends JpaRepository
     Set<ThemeOfSubjectForGradeByTeacher> findAllByGroupIdAndLessonIdAndEducationYearIdAndCreatedByOrderByCreatedAtDesc(String group_id, String lesson_id, String educationYear_id, String createdBy);
 
     ThemeOfSubjectForGradeByTeacher findFirstByGroupIdAndLessonIdAndEducationYearIdAndCreatedByOrderByCreatedAtDesc(String group_id, String lesson_id, String educationYear_id, String createdBy);
+
+
+    //@Query(value = "select * from dbo.GetListStudentsForGetGradesByThemeIdAndGroupIdAndSubjectIdAndTeacherIdAndEducationId(?5,?1,?2,?4,?3) order by createdAt",nativeQuery = true)
+
+
+    @Query(value = "select * from dbo.GetListStudentsForGetGradesByThemeIdAndGroupIdAndSubjectIdAndTeacherIdAndEducationId(?1,?2,?3,?4,?5) order by createdAt",nativeQuery = true)
+    Set<GetThemesItems> getThemesItems(String themeId, String groupId, String lessonId, String educationYearId, String teacherId);
 
 }
