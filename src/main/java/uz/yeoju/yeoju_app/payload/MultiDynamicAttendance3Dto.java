a124253f129c1d1d801f3aeaa1664e97f8c7538c
whitecode <guvalakat1603@gmail.com>
2024-03-18 14:12:52 +0500

    MultiDynamicAttendance3Dto dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/MultiDynamicAttendance3Dto.java
@@ -0,0 +1,30 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MultiDynamicAttendance3Dto {
+    public String id;
+    public Integer year;
+    public Integer week;
+    public Integer weekday;
+    public Integer section;
+    public Boolean isCome;
+    public String studentId;
+    public String room;
+
+    public MultiDynamicAttendance3Dto(Integer year, Integer week, Integer weekday, Integer section, Boolean isCome, String studentId, String room) {
+        this.year = year;
+        this.week = week;
+        this.weekday = weekday;
+        this.section = section;
+        this.isCome = isCome;
+        this.studentId = studentId;
+        this.room = room;
+    }
+}
