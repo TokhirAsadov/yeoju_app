52c013401491de63156f15c6e71c4056666fa6c4
whitecode <guvalakat1603@gmail.com>
2024-04-15 11:49:02 +0500

    new field and constructor were added

--- src/main/java/uz/yeoju/yeoju_app/payload/dekanat/NotificationOuterCreateDto.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/dekanat/NotificationOuterCreateDto.java
@@ -12,6 +12,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class NotificationOuterCreateDto {
     public String id;
+    public String dynamicSection;
     public String dekanatId;
     public String educationId;
     public Integer course;
@@ -20,6 +21,17 @@ public class NotificationOuterCreateDto {
     public Set<String> facultiesId;
 
     public Set<String> groupsId;
+
+    public NotificationOuterCreateDto(String dynamicSection, String dekanatId, String educationId, Integer course, Date fromDate, Date toDate, Set<String> facultiesId, Set<String> groupsId) {
+        this.dynamicSection = dynamicSection;
+        this.dekanatId = dekanatId;
+        this.educationId = educationId;
+        this.course = course;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.facultiesId = facultiesId;
+        this.groupsId = groupsId;
+    }
 
     public NotificationOuterCreateDto(String dekanatId, String educationId, Integer course, Date fromDate, Date toDate, Set<String> facultiesId, Set<String> groupsId) {
         this.dekanatId = dekanatId;
