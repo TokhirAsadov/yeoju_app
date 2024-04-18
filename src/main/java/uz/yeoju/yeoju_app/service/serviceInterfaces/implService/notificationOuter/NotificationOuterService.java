eb517d60e821ab69589d35c676dd1675946d4402
whitecode <guvalakat1603@gmail.com>
2024-04-18 09:56:26 +0500

    delete function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/notificationOuter/NotificationOuterService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/notificationOuter/NotificationOuterService.java
@@ -1,5 +1,6 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.notificationOuter;
 
+import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.dekanat.NotificationOuterCreateDto;
 
@@ -8,4 +9,6 @@ public interface NotificationOuterService {
     ApiResponse getAllCounters();
     ApiResponse getStudentNotifications(String studentId);
     ApiResponse createAndUpdate(NotificationOuterCreateDto dto);
+
+    ApiResponse delete(User user, String id);
 }
