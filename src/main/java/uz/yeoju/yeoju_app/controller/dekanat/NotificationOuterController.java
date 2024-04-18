6ab070c09ddaf1d0bea6759b6bef684140ee3a3d
whitecode <guvalakat1603@gmail.com>
2024-04-18 10:08:09 +0500

    DELETE api/v1/desktop/notificationOuter/deleteNO/{id} API was created

--- src/main/java/uz/yeoju/yeoju_app/controller/dekanat/NotificationOuterController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/dekanat/NotificationOuterController.java
@@ -3,10 +3,13 @@ package uz.yeoju.yeoju_app.controller.dekanat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.dekanat.NotificationOuterCreateDto;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.notificationOuter.NotificationOuterService;
 
 @RestController
@@ -39,5 +42,12 @@ public class NotificationOuterController {
     public HttpEntity<?> update(@RequestBody NotificationOuterCreateDto dto){
         ApiResponse response = service.createAndUpdate(dto);
         return ResponseEntity.status(response.isSuccess() ? 200 : 409).body(response);
+    }
+
+    @PreAuthorize("hasRole('TEACHER')")
+    @DeleteMapping("/deleteNO/{id}")
+    public HttpEntity<?> delete(@CurrentUser User user, @PathVariable("id") String id){
+        ApiResponse delete = service.delete(user, id);
+        return ResponseEntity.status(delete.isSuccess() ? 204 : 409).body(delete);
     }
 }
