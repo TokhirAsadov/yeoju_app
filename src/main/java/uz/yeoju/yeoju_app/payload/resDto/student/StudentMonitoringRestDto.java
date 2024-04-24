26ce9f6cc4aeb3ba2ebf9b7429b00a67e4d355ae
whitecode <guvalakat1603@gmail.com>
2024-04-24 11:13:50 +0500

    StudentMonitoringRestDto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/StudentMonitoringRestDto.java
@@ -0,0 +1,10 @@
+package uz.yeoju.yeoju_app.payload.resDto.student;
+
+public interface StudentMonitoringRestDto {
+    String getStudentId();
+    String getGroupId();
+    String getEducationYearId();
+    Integer getYear();
+    Integer getWeek();
+    Integer getDay();
+}
