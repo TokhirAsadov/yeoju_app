ba521f8898d7fc812e1b02b9d2e27c4283e4c68a
whitecode <guvalakat1603@gmail.com>
2024-04-24 11:32:11 +0500

    fields were added

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/StudentMonitoringRestDto.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/StudentMonitoringRestDto.java
@@ -1,5 +1,7 @@
 package uz.yeoju.yeoju_app.payload.resDto.student;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface StudentMonitoringRestDto {
     String getStudentId();
     String getGroupId();
@@ -7,4 +9,16 @@ public interface StudentMonitoringRestDto {
     Integer getYear();
     Integer getWeek();
     Integer getDay();
+
+    @Value("#{@studentRepository.getStudentMonitoringByEducationYear(target.studentId,target.groupId,target.educationYear)}")
+    String getStudentMonitoringByEducationYear();
+
+    @Value("#{@studentRepository.getStudentMonitoringByWeek(target.studentId,target.groupId,target.year,target.week)}")
+    String getStudentMonitoringByWeek();
+
+    @Value("#{@studentRepository.getStudentMonitoringByDay(target.studentId,target.groupId,target.year,target.week,target.day)}")
+    String getStudentMonitoringByDay();
+
+    @Value("#{@studentRepository.getAllSubjectsByDayAndGroupAndStudentId(target.studentId,target.groupId,target.year,target.week,target.day)}")
+    GetAllSubjectsByDayAndGroupAndStudentId getTimeTableOfToday();
 }
