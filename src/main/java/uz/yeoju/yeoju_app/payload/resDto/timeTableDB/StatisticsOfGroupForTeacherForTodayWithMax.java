d51e21e44b60ac74ffc0d8fd823c835bb423fa8c
whitecode <guvalakat1603@gmail.com>
2024-04-26 10:48:34 +0500

    @JsonIgnore was changed

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/StatisticsOfGroupForTeacherForTodayWithMax.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/StatisticsOfGroupForTeacherForTodayWithMax.java
@@ -1,13 +1,18 @@
 package uz.yeoju.yeoju_app.payload.resDto.timeTableDB;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Set;
 
 public interface StatisticsOfGroupForTeacherForTodayWithMax {
+    @JsonIgnore
     String getEducationId();
+    @JsonIgnore
     String getSubjectId();
+    @JsonIgnore
     String getGroupId();
+    @JsonIgnore
     String getTeacherId();
 
     String getYear();
