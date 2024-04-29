e72220a99963fd682c0b19e0d998490965740252
whitecode <guvalakat1603@gmail.com>
2024-04-29 18:03:24 +0500

    BotRestConstants was created

---
+++ src/main/java/uz/yeoju/yeoju_app/bot/BotRestConstants.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.bot;
+
+public interface BotRestConstants {
+    String TELEGRAM_BASE_URL = "https://api.telegram.org/bot";
+    String BOT_TOKEN = "6334964758:AAFMbGS2Z09IslUf5-yr2yRFFxTN9JXtjDs";
+    //https://api.telegram.org/bot6334964758:AAFMbGS2Z09IslUf5-yr2yRFFxTN9JXtjDs/sendMessage?chat_id=645257161&text=hello
+}
