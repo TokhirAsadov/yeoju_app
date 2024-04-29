ed514b7a7e49a3a0d4319ad5e0fab7d18921c0b7
whitecode <guvalakat1603@gmail.com>
2024-04-29 12:11:53 +0500

    TelegramBotController controller was created

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/bot/TelegramBotController.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app.controller.bot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.yeoju.yeoju_app.controller.BaseUrl;
+
+@RestController
+@RequestMapping(BaseUrl.BASE_BOT_URL+"/telegram")
+public class TelegramBotController {
+
+
+
+}
