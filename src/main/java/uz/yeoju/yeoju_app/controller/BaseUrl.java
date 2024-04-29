458a1584d61f16786b3c9324f3ca81c4ca508662
whitecode <guvalakat1603@gmail.com>
2024-04-29 11:33:02 +0500

    new url was created for to use telegram bot

--- src/main/java/uz/yeoju/yeoju_app/controller/BaseUrl.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/BaseUrl.java
@@ -4,7 +4,9 @@ public interface BaseUrl {
     String VERSION = "/v1";
     String BASE = "/api" + VERSION;
     String TYPE = BASE + "/desktop";
+    String TYPE_BOT = BASE + "/bot";
     //String SMS_SERVICE_URL= "https://rest.messagebird.com/messages";
     String BASE_URL = TYPE;
+    String BASE_BOT_URL = TYPE_BOT;
 
 }
