b6b960f4cded097391a82a73e2e1b8bb8c2a4098
whitecode <guvalakat1603@gmail.com>
2024-05-01 11:21:17 +0500

    some changes were added

--- src/main/java/uz/yeoju/yeoju_app/bot/service/WebhookService.java
+++ src/main/java/uz/yeoju/yeoju_app/bot/service/WebhookService.java
@@ -6,6 +6,7 @@ import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import uz.yeoju.yeoju_app.bot.BotRestConstants;
+import uz.yeoju.yeoju_app.bot.payload.ResultTelegram;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +15,8 @@ public class WebhookService {
     private final RestTemplate getRestTemplate;
 
     public void whenStart(Update update){
-        System.out.println("2.ok->  ...");
         SendMessage sendMessage = new SendMessage(update.getMessage().getChatId(),"Xush kelibsiz!.");
-        Object object = getRestTemplate.postForObject(BotRestConstants.TELEGRAM_BASE_URL + BotRestConstants.BOT_TOKEN + "/sendMessage", sendMessage, Object.class);
+        ResultTelegram object = getRestTemplate.postForObject(BotRestConstants.TELEGRAM_BASE_URL + BotRestConstants.BOT_TOKEN + "/sendMessage", sendMessage, ResultTelegram.class);
         System.out.println(object);
     }
 }
