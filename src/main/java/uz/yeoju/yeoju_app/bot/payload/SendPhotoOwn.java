f01df6516fe3e7a234f946440fdf704f32656db3
whitecode <guvalakat1603@gmail.com>
2024-05-01 14:46:06 +0500

    SendPhotoOwn dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/bot/payload/SendPhotoOwn.java
@@ -0,0 +1,19 @@
+package uz.yeoju.yeoju_app.bot.payload;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SendPhotoOwn {
+
+    @JsonProperty(value = "chat_id")
+    private String chatId;
+
+    private String caption;
+
+    private String photo;
+}
