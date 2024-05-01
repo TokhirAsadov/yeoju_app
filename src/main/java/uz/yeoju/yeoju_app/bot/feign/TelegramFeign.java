f86b0e04a629e245cad73d0c55d84c87c871937a
whitecode <guvalakat1603@gmail.com>
2024-05-01 14:45:55 +0500

    sendPhotoToUser API was created

--- src/main/java/uz/yeoju/yeoju_app/bot/feign/TelegramFeign.java
+++ src/main/java/uz/yeoju/yeoju_app/bot/feign/TelegramFeign.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import uz.yeoju.yeoju_app.bot.BotRestConstants;
 import uz.yeoju.yeoju_app.bot.payload.ResultTelegram;
+import uz.yeoju.yeoju_app.bot.payload.SendPhotoOwn;
 
 @FeignClient(url = BotRestConstants.TELEGRAM_BASE_URL_WITHOUT_BOT,name = "TelegramFeign")
 public interface TelegramFeign {
 
     @PostMapping("{path}/sendMessage")
     ResultTelegram sendMessageToUser(@PathVariable String path, @RequestBody SendMessage sendMessage);
+
+    @PostMapping("{path}/sendPhoto")
+    ResultTelegram sendPhotoToUser(@PathVariable String path, @RequestBody SendPhotoOwn sendPhotoOwn);
 }
