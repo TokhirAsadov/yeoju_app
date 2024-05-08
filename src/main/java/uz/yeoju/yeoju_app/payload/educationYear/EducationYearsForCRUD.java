a496296231a889f998fa97405ca9109190020959
whitecode <guvalakat1603@gmail.com>
2024-05-08 11:01:54 +0500

    rest dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/educationYear/EducationYearsForCRUD.java
@@ -0,0 +1,11 @@
+package uz.yeoju.yeoju_app.payload.educationYear;
+
+
+import java.util.Date;
+
+public interface EducationYearsForCRUD {
+    String getId();
+    String getName();
+    Date getStart();
+    Date getEnd();
+}
