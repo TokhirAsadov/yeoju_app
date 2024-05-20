73169a297a039224eaa5397eb860673e277439cd
whitecode <guvalakat1603@gmail.com>
2024-05-20 15:31:46 +0500

    FinalGradeOfStudentRepository was created

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/FinalGradeOfStudentRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository.module;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.module.FinalGradeOfStudent;
+
+public interface FinalGradeOfStudentRepository extends JpaRepository<FinalGradeOfStudent, String> {
+}
