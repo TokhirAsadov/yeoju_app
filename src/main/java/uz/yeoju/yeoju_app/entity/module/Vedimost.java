0b62bd5a7b0adb15dd6d748fb0b1d32bd0bc9238
whitecode <guvalakat1603@gmail.com>
2024-05-20 15:30:23 +0500

    Vedimost was created

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/module/Vedimost.java
@@ -0,0 +1,29 @@
+package uz.yeoju.yeoju_app.entity.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.Group;
+import uz.yeoju.yeoju_app.entity.Lesson;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.educationYear.EducationYear;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Vedimost extends AbsEntity {
+    private Integer level;
+    @ManyToOne
+    private User teacher;
+    @ManyToOne
+    private Lesson lesson;
+    @ManyToOne
+    private EducationYear educationYear;
+    @ManyToOne
+    private Group group;
+}
