6554dbb8635d4dd487725505e94dddb9b73f320e
whitecode <guvalakat1603@gmail.com>
2024-05-20 15:30:06 +0500

    FinalGradeOfStudent was created

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/module/FinalGradeOfStudent.java
@@ -0,0 +1,27 @@
+package uz.yeoju.yeoju_app.entity.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.Lesson;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.educationYear.EducationYear;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FinalGradeOfStudent extends AbsEntity {
+    private Float grade;
+//    private Float extraGrade;
+    @ManyToOne
+    private User student;
+    @ManyToOne
+    private Lesson lesson;
+    @ManyToOne
+    private EducationYear educationYear;
+}
