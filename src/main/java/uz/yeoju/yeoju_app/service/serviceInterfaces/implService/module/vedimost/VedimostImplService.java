cc4f8a5c17015f8512200d597f719dd843e35a3d
whitecode <guvalakat1603@gmail.com>
2024-05-22 10:17:14 +0500

    repositories were added

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostImplService.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.module.VedimostCreaterDto;
+import uz.yeoju.yeoju_app.repository.GroupRepository;
+import uz.yeoju.yeoju_app.repository.LessonRepository;
+import uz.yeoju.yeoju_app.repository.UserRepository;
+import uz.yeoju.yeoju_app.repository.educationYear.EducationYearRepository;
 import uz.yeoju.yeoju_app.repository.module.FinalGradeOfStudentRepository;
 import uz.yeoju.yeoju_app.repository.module.VedimostRepository;
 
@@ -12,6 +16,10 @@ import uz.yeoju.yeoju_app.repository.module.VedimostRepository;
 public class VedimostImplService implements VedimostService{
     private final VedimostRepository vedimostRepository;
     private final FinalGradeOfStudentRepository finalGradeOfStudentRepository;
+    private final UserRepository userRepository;
+    private final LessonRepository lessonRepository;
+    private final EducationYearRepository educationYearRepository;
+    private final GroupRepository groupRepository;
 
     @Override
     public ApiResponse findAllVedimosts() {
