dfe56a38783c56d978835c348ed8d2b10934571b
whitecode <guvalakat1603@gmail.com>
2024-05-22 12:00:15 +0500

    getVedimostByTeacherId query was added

--- src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
@@ -24,6 +24,22 @@ public interface VedimostRepository extends JpaRepository<Vedimost, String> {
             "    join Teacher t on t.user_id=v.teacher_id\n" +
             "    join Lesson l on l.id=v.lesson_id\n" +
             "    join groups g on v.group_id = g.id\n" +
-            "where t.kafedra_id=?1 and v.educationYear_id=?2",nativeQuery = true)
+            "where t.kafedra_id=?1 and v.educationYear_id=?2 order by u.fullName",nativeQuery = true)
     Set<GetVedimostOfKafedra> getVedimostOfKafedra(String kafedraId,String educationYearId);
+
+    @Query(value = "select \n" +
+            "    v.id,\n" +
+            "    v.level,\n" +
+            "    v.deadline,\n" +
+            "    v.timeClose,\n" +
+            "    v.condition,\n" +
+            "    u.fullName as teacher,\n" +
+            "    l.name as lesson,\n" +
+            "    g.name as groupName\n" +
+            "    from Vedimost v\n" +
+            "    join users u on v.teacher_id = u.id\n" +
+            "    join Lesson l on l.id=v.lesson_id\n" +
+            "    join groups g on v.group_id = g.id\n" +
+            "where v.teacher_id=?1 and v.educationYear_id=?2 order by v.createdAt",nativeQuery = true)
+    Set<GetVedimostOfKafedra> getVedimostByTeacherId(String teacherId,String educationYearId);
 }
