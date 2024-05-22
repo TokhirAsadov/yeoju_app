d302f82dfa48e58a29069b0f1650a2804fa81430
whitecode <guvalakat1603@gmail.com>
2024-05-22 11:53:17 +0500

    getVedimostOfKafedra query was created

--- src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
@@ -1,7 +1,29 @@
 package uz.yeoju.yeoju_app.repository.module;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.yeoju.yeoju_app.entity.module.Vedimost;
+import uz.yeoju.yeoju_app.payload.resDto.module.vedimost.GetVedimostOfKafedra;
+
+import java.util.Set;
 
 public interface VedimostRepository extends JpaRepository<Vedimost, String> {
+
+
+    @Query(value = "select \n" +
+            "    v.id,\n" +
+            "    v.level,\n" +
+            "    v.deadline,\n" +
+            "    v.timeClose,\n" +
+            "    v.condition,\n" +
+            "    u.fullName as teacher,\n" +
+            "    l.name as lesson,\n" +
+            "    g.name as groupName\n" +
+            "    from Vedimost v\n" +
+            "    join users u on v.teacher_id = u.id\n" +
+            "    join Teacher t on t.user_id=v.teacher_id\n" +
+            "    join Lesson l on l.id=v.lesson_id\n" +
+            "    join groups g on v.group_id = g.id\n" +
+            "where t.kafedra_id=?1 and v.educationYear_id=?2",nativeQuery = true)
+    Set<GetVedimostOfKafedra> getVedimostOfKafedra(String kafedraId,String educationYearId);
 }
