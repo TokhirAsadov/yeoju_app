a7c624f265a4ead0ad48db57334428b3b192dc44
whitecode <guvalakat1603@gmail.com>
2024-05-22 10:04:20 +0500

    VedimostImplService was created

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostImplService.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.vedimost;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.repository.module.FinalGradeOfStudentRepository;
+import uz.yeoju.yeoju_app.repository.module.VedimostRepository;
+
+@Service
+@RequiredArgsConstructor
+public class VedimostImplService implements VedimostService{
+    private final VedimostRepository vedimostRepository;
+    private final FinalGradeOfStudentRepository finalGradeOfStudentRepository;
+}
