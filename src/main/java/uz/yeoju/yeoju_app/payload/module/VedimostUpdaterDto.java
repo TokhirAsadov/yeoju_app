184773ebe460236430cb0ebbb3bec28ed5ff1477
whitecode <guvalakat1603@gmail.com>
2024-05-22 12:17:19 +0500

    VedimostUpdaterDto dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/module/VedimostUpdaterDto.java
@@ -0,0 +1,34 @@
+package uz.yeoju.yeoju_app.payload.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VedimostUpdaterDto {
+    public String id;
+    public Integer level;
+    public Timestamp deadline;
+    public Timestamp timeClose;
+    public String condition;
+    public String teacherId;
+    public String lessonId;
+    public String educationYearId;
+    public String groupId;
+
+    public VedimostUpdaterDto(Integer level, Timestamp deadline, Timestamp timeClose, String condition, String teacherId, String lessonId, String educationYearId, String groupId) {
+        this.level = level;
+        this.deadline = deadline;
+        this.timeClose = timeClose;
+        this.condition = condition;
+        this.teacherId = teacherId;
+        this.lessonId = lessonId;
+        this.educationYearId = educationYearId;
+        this.groupId = groupId;
+    }
+}
