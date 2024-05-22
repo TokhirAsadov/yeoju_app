34702fde2d0af238f2e2d7f17184de052445caee
whitecode <guvalakat1603@gmail.com>
2024-05-22 11:53:35 +0500

    GetVedimostOfKafedra rest dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/module/vedimost/GetVedimostOfKafedra.java
@@ -0,0 +1,14 @@
+package uz.yeoju.yeoju_app.payload.resDto.module.vedimost;
+
+import java.sql.Timestamp;
+
+public interface GetVedimostOfKafedra {
+    String getId();
+    Integer getLevel();
+    Timestamp getDeadline();
+    Timestamp getTimeClose();
+    String getCondition();
+    String getTeacher();
+    String getLesson();
+    String getGroupName();
+}
