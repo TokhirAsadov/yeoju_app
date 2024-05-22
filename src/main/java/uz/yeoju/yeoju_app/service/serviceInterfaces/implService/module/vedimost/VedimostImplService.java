22ff0a7de945390ca631a4cfaaa5bb6258a5c679
whitecode <guvalakat1603@gmail.com>
2024-05-22 10:43:15 +0500

    createVedimost function was done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostImplService.java
@@ -2,6 +2,9 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.vedimost
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.module.Vedimost;
+import uz.yeoju.yeoju_app.entity.module.VedimostCondition;
+import uz.yeoju.yeoju_app.exceptions.UserNotFoundException;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.module.VedimostCreaterDto;
 import uz.yeoju.yeoju_app.repository.GroupRepository;
@@ -28,6 +31,38 @@ public class VedimostImplService implements VedimostService{
 
     @Override
     public ApiResponse createVedimost(VedimostCreaterDto dto) {
-        return null;
+        boolean existsTeacher = userRepository.existsById(dto.teacherId);
+        if (existsTeacher) {
+            boolean existsLesson = lessonRepository.existsById(dto.lessonId);
+            if (existsLesson) {
+                boolean existsEducationYear = educationYearRepository.existsById(dto.educationYearId);
+                if (existsEducationYear) {
+                    dto.groupsIds.forEach(groupId -> {
+                        groupRepository.findById(groupId).ifPresent(group -> {
+                            Vedimost vedimost = new Vedimost();
+                            vedimost.setLevel(dto.level);
+                            vedimost.setDeadline(dto.deadline);
+//                            vedimost.setTimeClose(dto.timeClose);
+                            vedimost.setCondition(VedimostCondition.valueOf(dto.condition));
+                            vedimost.setTeacher(userRepository.findById(dto.teacherId).orElse(null));
+                            vedimost.setLesson(lessonRepository.findById(dto.lessonId).orElse(null));
+                            vedimost.setGroup(group);
+                            vedimost.setEducationYear(educationYearRepository.findById(dto.educationYearId).orElse(null));
+                            vedimostRepository.save(vedimost);
+                        });
+                    });
+                }
+                else {
+                    throw new UserNotFoundException("Education year was not found by id " +dto.educationYearId);
+                }
+            }
+            else {
+                throw new UserNotFoundException("Lesson was not found by id " +dto.lessonId);
+            }
+        }
+        else {
+            throw new UserNotFoundException("Teacher was not found by id " +dto.teacherId);
+        }
+        return new ApiResponse(true, "Vedimost created successfully");
     }
 }
