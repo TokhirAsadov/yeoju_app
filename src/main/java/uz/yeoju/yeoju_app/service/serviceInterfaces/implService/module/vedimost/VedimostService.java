871944806f0be04fecb4659e32738947ccd55993
whitecode <guvalakat1603@gmail.com>
2024-05-22 10:15:53 +0500

    createVedimost function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostService.java
@@ -1,7 +1,9 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.vedimost;
 
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.module.VedimostCreaterDto;
 
 public interface VedimostService {
     ApiResponse findAllVedimosts();
+    ApiResponse createVedimost(VedimostCreaterDto dto);
 }
