291f07adb547fa8149310fb7ded7a58ba4c38552
whitecode <guvalakat1603@gmail.com>
2024-05-22 10:04:34 +0500

    findAllVedimosts was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostService.java
@@ -3,5 +3,5 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.vedimost
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 
 public interface VedimostService {
-
+    ApiResponse findAllVedimosts();
 }
