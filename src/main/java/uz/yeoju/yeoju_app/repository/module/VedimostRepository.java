e6e9c966b44e751ba390f945ceca4d982e7780dc
whitecode <guvalakat1603@gmail.com>
2024-05-22 12:04:52 +0500

    getAllVedimostByKafedra query was created

--- src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
@@ -38,6 +38,24 @@ public interface VedimostRepository extends JpaRepository<Vedimost, String> {
             "    g.name as groupName\n" +
             "    from Vedimost v\n" +
             "    join users u on v.teacher_id = u.id\n" +
+            "    join Teacher t on t.user_id=v.teacher_id\n" +
+            "    join Lesson l on l.id=v.lesson_id\n" +
+            "    join groups g on v.group_id = g.id\n" +
+            "where t.kafedra_id=?1 order by v.createdAt desc",nativeQuery = true)
+    Set<GetVedimostOfKafedra> getAllVedimostOfKafedra(String kafedraId);
+
+
+    @Query(value = "select \n" +
+            "    v.id,\n" +
+            "    v.level,\n" +
+            "    v.deadline,\n" +
+            "    v.timeClose,\n" +
+            "    v.condition,\n" +
+            "    u.fullName as teacher,\n" +
+            "    l.name as lesson,\n" +
+            "    g.name as groupName\n" +
+            "    from Vedimost v\n" +
+            "    join users u on v.teacher_id = u.id\n" +
             "    join Lesson l on l.id=v.lesson_id\n" +
             "    join groups g on v.group_id = g.id\n" +
             "where v.teacher_id=?1 and v.educationYear_id=?2 order by v.createdAt",nativeQuery = true)
