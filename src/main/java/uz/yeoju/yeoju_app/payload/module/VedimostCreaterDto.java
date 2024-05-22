a49a9bc2bf9a008132534e066943fa9068a22a84
whitecode <guvalakat1603@gmail.com>
2024-05-22 10:14:46 +0500

    VedimostCreaterDto dto was created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/module/VedimostCreaterDto.java
@@ -0,0 +1,23 @@
+package uz.yeoju.yeoju_app.payload.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.module.VedimostCondition;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VedimostCreaterDto {
+    public String id;
+    public Integer level;
+    public Timestamp deadline;
+    public Timestamp timeClose;
+    public VedimostCondition condition;
+    public String teacherId;
+    public String lessonId;
+    public String educationYearId;
+    public String groupId;
+}
