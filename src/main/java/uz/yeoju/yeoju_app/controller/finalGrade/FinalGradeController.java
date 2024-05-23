fbd1247fdef965a6002b17cc62b53012c88e1a08
whitecode <guvalakat1603@gmail.com>
2024-05-23 11:31:50 +0500

    POST api/v1/desktop/finalGrade/createFinalGrades API is created

--- src/main/java/uz/yeoju/yeoju_app/controller/finalGrade/FinalGradeController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/finalGrade/FinalGradeController.java
@@ -1,14 +1,32 @@
 package uz.yeoju.yeoju_app.controller.finalGrade;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.module.FinalGradeCreatorDto;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
 import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.finalGrade.FinalGradeOfStudentService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(BaseUrl.BASE_URL+"/finalGrade")
 @RequiredArgsConstructor
 public class FinalGradeController {
     private final FinalGradeOfStudentService finalGradeOfStudentService;
+
+    @PreAuthorize("hasRole('TEACHER')")
+    @PostMapping("/createFinalGrades")
+    public HttpEntity<?> createFinalGrades(@CurrentUser User user, @RequestBody @Valid FinalGradeCreatorDto dto) {
+        ApiResponse response = finalGradeOfStudentService.createFinalGrades(dto);
+        return ResponseEntity.status(response.isSuccess()?201:401).body(response);
+    }
 }
