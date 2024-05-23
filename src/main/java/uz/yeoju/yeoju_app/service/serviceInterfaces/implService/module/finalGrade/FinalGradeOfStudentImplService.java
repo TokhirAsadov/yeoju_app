8e85118f579cf9d8b9f290c9133a566bdcfb9a20
whitecode <guvalakat1603@gmail.com>
2024-05-23 11:27:18 +0500

    createFinalGrades function is done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/finalGrade/FinalGradeOfStudentImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/finalGrade/FinalGradeOfStudentImplService.java
@@ -2,11 +2,18 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.finalGra
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.module.FinalGradeOfStudent;
+import uz.yeoju.yeoju_app.entity.module.Vedimost;
+import uz.yeoju.yeoju_app.entity.module.VedimostCondition;
+import uz.yeoju.yeoju_app.exceptions.UserNotFoundException;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.module.FinalGradeCreatorDto;
+import uz.yeoju.yeoju_app.repository.UserRepository;
 import uz.yeoju.yeoju_app.repository.module.FinalGradeOfStudentRepository;
 import uz.yeoju.yeoju_app.repository.module.VedimostRepository;
 
+import java.sql.Timestamp;
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
@@ -14,9 +21,31 @@ import java.util.Set;
 public class FinalGradeOfStudentImplService implements FinalGradeOfStudentService {
     private final FinalGradeOfStudentRepository finalGradeOfStudentRepository;
     private final VedimostRepository vedimostRepository;
+    private final UserRepository userRepository;
 
     @Override
-    public ApiResponse createFinalGrades(Set<FinalGradeCreatorDto> dto) {
-        return null;
+    public ApiResponse createFinalGrades(FinalGradeCreatorDto dto) {
+        boolean exists = vedimostRepository.existsById(dto.vedimostId);
+        if (exists) {
+            Vedimost vedimost = vedimostRepository.getById(dto.vedimostId);
+            Set<FinalGradeOfStudent> grades = new HashSet<>();
+            dto.data.forEach(d -> {
+                boolean existsStudent = userRepository.existsById(d.studentId);
+                if (existsStudent) {
+                    grades.add(new FinalGradeOfStudent(Math.round(d.grade * 100) / 100.0, userRepository.findById(d.studentId).get(), vedimost));
+                }
+                else {
+                    throw new UserNotFoundException("Student was not found by id " +d.studentId);
+                }
+            });
+            finalGradeOfStudentRepository.saveAll(grades);
+            vedimost.setCondition(VedimostCondition.DONE);
+            vedimost.setTimeClose(new Timestamp(System.currentTimeMillis()));
+            vedimostRepository.save(vedimost);
+            return new ApiResponse(true, "All grades created and vedimost is closed.");
+        }
+        else {
+            throw new UserNotFoundException("Vedimost was not found by id " +dto.vedimostId);
+        }
     }
 }
