6101620d2dac119f59cc22be9082ee6541d7ff77
whitecode <guvalakat1603@gmail.com>
2024-05-23 11:05:38 +0500

    createFinalGrades function was created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/finalGrade/FinalGradeOfStudentService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/finalGrade/FinalGradeOfStudentService.java
@@ -1,4 +1,10 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.finalGrade;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.module.FinalGradeCreatorDto;
+
+import java.util.Set;
+
 public interface FinalGradeOfStudentService {
+    ApiResponse createFinalGrades(Set<FinalGradeCreatorDto> dto);
 }
