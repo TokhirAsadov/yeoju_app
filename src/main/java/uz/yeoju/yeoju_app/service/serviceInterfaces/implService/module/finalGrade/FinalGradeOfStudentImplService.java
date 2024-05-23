73748b7a9004a0e760f082dbfe9527f63e47a944
whitecode <guvalakat1603@gmail.com>
2024-05-23 11:06:16 +0500

    createFinalGrades function was override

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/finalGrade/FinalGradeOfStudentImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/finalGrade/FinalGradeOfStudentImplService.java
@@ -2,8 +2,21 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.finalGra
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.module.FinalGradeCreatorDto;
+import uz.yeoju.yeoju_app.repository.module.FinalGradeOfStudentRepository;
+import uz.yeoju.yeoju_app.repository.module.VedimostRepository;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class FinalGradeOfStudentImplService implements FinalGradeOfStudentService {
+    private final FinalGradeOfStudentRepository finalGradeOfStudentRepository;
+    private final VedimostRepository vedimostRepository;
+
+    @Override
+    public ApiResponse createFinalGrades(Set<FinalGradeCreatorDto> dto) {
+        return null;
+    }
 }
