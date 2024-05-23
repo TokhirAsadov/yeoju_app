4759d3b315352cbd3855df5de2168a796ff2992c
whitecode <guvalakat1603@gmail.com>
2024-05-23 10:46:30 +0500

    FinalGradeOfStudentImplService was created

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/finalGrade/FinalGradeOfStudentImplService.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.module.finalGrade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FinalGradeOfStudentImplService implements FinalGradeOfStudentService {
+}
