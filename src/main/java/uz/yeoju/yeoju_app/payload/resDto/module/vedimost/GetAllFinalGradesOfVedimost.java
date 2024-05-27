cbf5326400ff77140c5379ab833998dab65fa9d7
whitecode <guvalakat1603@gmail.com>
2024-05-27 13:14:42 +0500

    GetAllFinalGradesOfVedimost rest dto is added

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/module/vedimost/GetAllFinalGradesOfVedimost.java
@@ -0,0 +1,8 @@
+package uz.yeoju.yeoju_app.payload.resDto.module.vedimost;
+
+public interface GetAllFinalGradesOfVedimost {
+    String getStudentId();
+    String getStudent();
+    Float getGrade();
+    Float getExtraGrade();
+}
