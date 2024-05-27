31624c573fa97353c3382712d08dd534b9f6e185
whitecode <guvalakat1603@gmail.com>
2024-05-27 13:30:44 +0500

    GET api/v1/desktop/finalGrade/getGradesWithVedimost/{vedimostId} API is created

--- src/main/java/uz/yeoju/yeoju_app/controller/finalGrade/FinalGradeController.java
+++ src/main/java/uz/yeoju/yeoju_app/controller/finalGrade/FinalGradeController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
@@ -28,5 +25,12 @@ public class FinalGradeController {
     public HttpEntity<?> createFinalGrades(@CurrentUser User user, @RequestBody @Valid FinalGradeCreatorDto dto) {
         ApiResponse response = finalGradeOfStudentService.createFinalGrades(dto);
         return ResponseEntity.status(response.isSuccess()?201:401).body(response);
+    }
+
+    @PreAuthorize("hasAnyRole('KAFEDRA','MONITORING','TEACHER')")
+    @GetMapping("/getGradesWithVedimost/{vedimostId}")
+    public HttpEntity<?> getGradesWithVedimostByVedimostId(@CurrentUser User user, @PathVariable String vedimostId) {
+        ApiResponse response = finalGradeOfStudentService.getGradesWithVedimostByVedimostId(vedimostId);
+        return ResponseEntity.status(response.isSuccess()?200:401).body(response);
     }
 }
