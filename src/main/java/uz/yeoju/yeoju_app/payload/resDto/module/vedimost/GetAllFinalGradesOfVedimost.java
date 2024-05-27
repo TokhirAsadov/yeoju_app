113ae45b1079efb473e94faba2c2281228f765ca
whitecode <guvalakat1603@gmail.com>
2024-05-27 16:37:24 +0500

    add field

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/module/vedimost/GetAllFinalGradesOfVedimost.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/module/vedimost/GetAllFinalGradesOfVedimost.java
@@ -3,6 +3,7 @@ package uz.yeoju.yeoju_app.payload.resDto.module.vedimost;
 public interface GetAllFinalGradesOfVedimost {
     String getStudentId();
     String getStudent();
+    String getLogin();
     Float getGrade();
     Float getExtraGrade();
 }
