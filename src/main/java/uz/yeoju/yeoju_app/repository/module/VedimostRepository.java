5142a7819e965386d97da7fd17d2901e58ae4ad3
whitecode <guvalakat1603@gmail.com>
2024-05-28 11:28:09 +0500

    getVedimostByFacultyId query function is created

--- src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
@@ -141,6 +141,22 @@ public interface VedimostRepository extends JpaRepository<Vedimost, String> {
             "where v.group_id=?1 and v.educationYear_id=?2  order by v.createdAt desc",nativeQuery = true)
     Set<GetVedimostOfKafedra> getVedimostByGroupId(String groupId,String educationYearId);
 
+    @Query(value = "select \n" +
+            "    v.id,\n" +
+            "    v.level,\n" +
+            "    v.deadline,\n" +
+            "    v.timeClose,\n" +
+            "    v.condition,\n" +
+            "    u.fullName as teacher,\n" +
+            "    l.name as lesson,\n" +
+            "    g.name as groupName\n" +
+            "    from Vedimost v\n" +
+            "    join users u on v.teacher_id = u.id\n" +
+            "    join Lesson l on l.id=v.lesson_id\n" +
+            "    join groups g on v.group_id = g.id\n" +
+            "where g.faculty_id=?1 and v.educationYear_id=?2  order by v.createdAt desc",nativeQuery = true)
+    Set<GetVedimostOfKafedra> getVedimostByFacultyId(String facultyId,String educationYearId);
+
     @Query(value = "select Top 1\n" +
             "    v.id,\n" +
             "    v.level,\n" +
