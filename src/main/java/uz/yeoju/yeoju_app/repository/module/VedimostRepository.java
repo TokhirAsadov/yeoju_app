5d0985e3998689a73f76ff7af0acfa46c9c5aa95
whitecode <guvalakat1603@gmail.com>
2024-05-30 11:16:15 +0500

    getVedimostByLessonId function is overloaded

--- src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
@@ -207,6 +207,26 @@ public interface VedimostRepository extends JpaRepository<Vedimost, String> {
             "    join users u on v.teacher_id = u.id\n" +
             "    join Lesson l on l.id=v.lesson_id\n" +
             "    join groups g on v.group_id = g.id\n" +
+            "where v.lesson_id=?1 order by v.createdAt",nativeQuery = true)
+    Set<GetVedimostOfKafedra> getVedimostByLessonId(String lessonId);
+
+    @Query(value = "select \n" +
+            "    v.id,\n" +
+            "    v.courseLeader," +
+            "    v.headOfAcademicAffair," +
+            "    v.headOfDepartment," +
+            "    v.direction,\n" +
+            "    v.level,\n" +
+            "    v.deadline,\n" +
+            "    v.timeClose,\n" +
+            "    v.condition,\n" +
+            "    u.fullName as teacher,\n" +
+            "    l.name as lesson,\n" +
+            "    g.name as groupName\n" +
+            "    from Vedimost v\n" +
+            "    join users u on v.teacher_id = u.id\n" +
+            "    join Lesson l on l.id=v.lesson_id\n" +
+            "    join groups g on v.group_id = g.id\n" +
             "where v.teacher_id=?1 and v.educationYear_id=?2 order by v.createdAt",nativeQuery = true)
     Set<GetVedimostOfKafedra> getVedimostByTeacherId(String teacherId,String educationYearId);
     @Query(value = "select \n" +
