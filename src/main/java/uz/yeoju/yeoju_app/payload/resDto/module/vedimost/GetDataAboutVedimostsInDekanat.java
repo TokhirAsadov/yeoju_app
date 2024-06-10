fde54beda5bbc5b012b3505fc72dcb296a8f39ab
whitecode <guvalakat1603@gmail.com>
2024-06-10 15:34:49 +0500

    rest dto is created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/module/vedimost/GetDataAboutVedimostsInDekanat.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.payload.resDto.module.vedimost;
+
+public interface GetDataAboutVedimostsInDekanat {
+    Integer getTotal();
+    Integer getIsOpen();
+    Integer getIsDone();
+    Integer getIsClose();
+    Integer getIsNotDone();
+}
