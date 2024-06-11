449bd78287d2e30f7ba15466717e32260a479149
whitecode <guvalakat1603@gmail.com>
2024-06-11 11:48:21 +0500

    function is overload

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/module/vedimost/VedimostService.java
@@ -10,7 +10,7 @@ public interface VedimostService {
 
     ApiResponse getVedimostByKafedra(String kafedraId,String educationYearId);
 
-    ApiResponse getVedimostByLessonId(String lessonId, String educationYearId);
+    ApiResponse getVedimostByLessonId(String type,String dekanatOrKafedraId,String lessonId, String educationYearId);
     ApiResponse getVedimostByTeacherId(String type,String dekanatOrKafedraId,String teacherId, String educationYearId);
     ApiResponse getVedimostByTeacherIdAndLessonId(String type,String dekanatOrKafedraId,String teacherId, String educationYearId,String lessonId);
 
