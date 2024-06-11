a4e7133ab3c1b33b48387730da4bda9ccea9a217
whitecode <guvalakat1603@gmail.com>
2024-06-11 11:51:59 +0500

    function is overload

--- src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/module/VedimostRepository.java
@@ -532,6 +532,31 @@ public interface VedimostRepository extends JpaRepository<Vedimost, String> {
             "    l.name as lesson,\n" +
             "    g.name as groupName\n" +
             "    from Vedimost v\n" +
+            "         join users u on v.teacher_id = u.id\n" +
+            "         join Teacher t on u.id = t.user_id\n" +
+            "         join Lesson l on l.id=v.lesson_id\n" +
+            "         join groups g on v.group_id = g.id\n" +
+            "         join Faculty f on g.faculty_id = f.id\n" +
+            "         join Dekanat_Faculty d_f on f.id = d_f.faculties_id\n" +
+            "where v.group_id=?1 and v.educationYear_id=?2 and (d_f.Dekanat_id=?3 or t.kafedra_id=?3 ) order by v.createdAt desc",nativeQuery = true)
+    Set<GetVedimostOfKafedra> getVedimostByGroupId(String groupId,String educationYearId,String dekanatOrKafedraId);
+
+    @Query(value = "select \n" +
+            "    v.id,\n" +
+            "    v.courseLeader," +
+            "    v.headOfAcademicAffair," +
+            "    v.headOfDepartment," +
+            "    v.direction,\n" +
+            "    v.level,\n" +
+            "    v.deadline,\n" +
+            "    v.timeClose,\n" +
+            "    v.condition,\n" +
+            "    u.id as teacherId,\n" +
+            "    l.id as lessonId,\n" +
+            "    u.fullName as teacher,\n" +
+            "    l.name as lesson,\n" +
+            "    g.name as groupName\n" +
+            "    from Vedimost v\n" +
             "    join users u on v.teacher_id = u.id\n" +
             "    join Lesson l on l.id=v.lesson_id\n" +
             "    join groups g on v.group_id = g.id\n" +
@@ -560,28 +585,6 @@ public interface VedimostRepository extends JpaRepository<Vedimost, String> {
             "where v.educationYear_id=?1  order by v.createdAt desc",nativeQuery = true)
     Set<GetVedimostOfKafedra> getVedimostByEducationYearId(String educationYearId);
 
-
-    @Query(value = "select \n" +
-            "    v.id,\n" +
-            "    v.courseLeader," +
-            "    v.headOfAcademicAffair," +
-            "    v.headOfDepartment," +
-            "    v.direction,\n" +
-            "    v.level,\n" +
-            "    v.deadline,\n" +
-            "    v.timeClose,\n" +
-            "    v.condition,\n" +
-            "    u.id as teacherId,\n" +
-            "    l.id as lessonId,\n" +
-            "    u.fullName as teacher,\n" +
-            "    l.name as lesson,\n" +
-            "    g.name as groupName\n" +
-            "    from Vedimost v\n" +
-            "    join users u on v.teacher_id = u.id\n" +
-            "    join Lesson l on l.id=v.lesson_id\n" +
-            "    join groups g on v.group_id = g.id\n" +
-            "where v.group_id=?1 order by v.createdAt desc",nativeQuery = true)
-    Set<GetVedimostOfKafedra> getVedimostByGroupId(String groupId);
 
     @Query(value = "select \n" +
             "    v.id,\n" +
