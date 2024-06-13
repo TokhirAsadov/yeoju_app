80aa36926885b408ff3f19be9ef78eb86f7791b2
whitecode <guvalakat1603@gmail.com>
2024-06-13 13:37:45 +0500

    getAllErrorsForSpecialUser function is created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/errorReminder/ErrorReminderService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/errorReminder/ErrorReminderService.java
@@ -1,4 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.errorReminder;
 
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
 public interface ErrorReminderService {
+    ApiResponse getAllErrorsForSpecialUser(User user);
 }
