20c390e45c9424d1f4ae2da7714278e3460c8eae
whitecode <guvalakat1603@gmail.com>
2024-06-13 13:51:56 +0500

    @Data is added

--- src/main/java/uz/yeoju/yeoju_app/entity/uquvbulim/ErrorReminder.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/uquvbulim/ErrorReminder.java
@@ -1,6 +1,7 @@
 package uz.yeoju.yeoju_app.entity.uquvbulim;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
@@ -9,6 +10,7 @@ import javax.persistence.Entity;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class ErrorReminder extends AbsEntity {
     private String error;
 }
