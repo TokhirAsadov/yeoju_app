467db38f2b6308b64e0dd5d0667624139e151164
whitecode <guvalakat1603@gmail.com>
2024-06-13 13:58:46 +0500

    ErrorReminderController is created

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/uquvbulim/ErrorReminderController.java
@@ -0,0 +1,22 @@
+package uz.yeoju.yeoju_app.controller.uquvbulim;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.errorReminder.ErrorReminderService;
+
+@RestController
+@RequestMapping(BaseUrl.BASE_URL+"/errorReminder")
+@RequiredArgsConstructor
+public class ErrorReminderController {
+    private final ErrorReminderService service;
+
+
+}
