4b33efcdac4b317a2fd64772c7ac258648699fad
whitecode <guvalakat1603@gmail.com>
2024-06-13 13:37:57 +0500

    getAllErrorsForSpecialUser function is override

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/errorReminder/ErrorReminderImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/uquvbulim/errorReminder/ErrorReminderImplService.java
@@ -2,10 +2,17 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.uquvbulim.error
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.repository.uquvbulimi.ErrorReminderRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ErrorReminderImplService implements ErrorReminderService{
     private final ErrorReminderRepository errorReminderRepository;
+
+    @Override
+    public ApiResponse getAllErrorsForSpecialUser(User user) {
+        return null;
+    }
 }
