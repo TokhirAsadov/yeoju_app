b36e8ab590002fe5b40e65b642d0e7ded7fd5316
whitecode <guvalakat1603@gmail.com>
2024-06-13 13:14:43 +0500

    repository is created for to use ORM

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/uquvbulimi/ErrorReminderRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository.uquvbulimi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.uquvbulim.ErrorReminder;
+
+public interface ErrorReminderRepository extends JpaRepository<ErrorReminder, String> {
+}
