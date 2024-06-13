3c8504d793ee828cb3e7aeaee11ed0e31af858e7
whitecode <guvalakat1603@gmail.com>
2024-06-13 13:52:24 +0500

    findAllByCreatedByAndActiveOrderByCreatedAtDesc function is created

--- src/main/java/uz/yeoju/yeoju_app/repository/uquvbulimi/ErrorReminderRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/uquvbulimi/ErrorReminderRepository.java
@@ -3,5 +3,8 @@ package uz.yeoju.yeoju_app.repository.uquvbulimi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.uquvbulim.ErrorReminder;
 
+import java.util.List;
+
 public interface ErrorReminderRepository extends JpaRepository<ErrorReminder, String> {
+    List<ErrorReminder> findAllByCreatedByAndActiveOrderByCreatedAtDesc(String createdBy, Boolean active);
 }
