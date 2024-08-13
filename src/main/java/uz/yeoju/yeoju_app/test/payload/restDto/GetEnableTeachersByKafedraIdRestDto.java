d051ce0fa3402ff8a55d5661dc5367ca36a4b32b
Takhir <guvalakat1603@gmail.com>
2024-08-13 09:56:30 +0500

    new field is added

--- src/main/java/uz/yeoju/yeoju_app/test/payload/restDto/GetEnableTeachersByKafedraIdRestDto.java
+++ src/main/java/uz/yeoju/yeoju_app/test/payload/restDto/GetEnableTeachersByKafedraIdRestDto.java
@@ -1,9 +1,14 @@
 package uz.yeoju.yeoju_app.test.payload.restDto;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Set;
+
 public interface GetEnableTeachersByKafedraIdRestDto {
     String getId();
     String getTeacherId();
     String getTeacherName();
 
-
+    @Value("#{@enableTeacherControlTestRepository.getLessonsOfEnableTeachers(target.id)}")
+    Set<GetLessonsOfEnableTeachersRestDto> getLessons();
 }
