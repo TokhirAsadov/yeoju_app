8c48cc8fe9e97fbf3d495ba269418a4f9e14f70d
Takhir <guvalakat1603@gmail.com>
2024-08-13 09:41:56 +0500

    findAllEnableTeacherByKafedraId function is override

--- src/main/java/uz/yeoju/yeoju_app/test/service/EnableTeacherControlTestImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/test/service/EnableTeacherControlTestImplService.java
@@ -2,10 +2,16 @@ package uz.yeoju.yeoju_app.test.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.test.repository.EnableTeacherControlTestRepository;
 
 @Service
 @RequiredArgsConstructor
 public class EnableTeacherControlTestImplService implements EnableTeacherControlTestService{
     private final EnableTeacherControlTestRepository repository;
+
+    @Override
+    public ApiResponse findAllEnableTeacherByKafedraId(String kafkaId) {
+        return null;
+    }
 }
