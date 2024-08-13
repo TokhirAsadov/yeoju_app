d9dad22f9cc3eea2e4f2efd48c45fd53cc8fa943
Takhir <guvalakat1603@gmail.com>
2024-08-13 09:49:53 +0500

    GetEnableTeachersByKafedraIdRestDto rest dto is created

---
+++ src/main/java/uz/yeoju/yeoju_app/test/payload/restDto/GetEnableTeachersByKafedraIdRestDto.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.test.payload.restDto;
+
+public interface GetEnableTeachersByKafedraIdRestDto {
+    String getId();
+    String getTeacherId();
+    String getTeacherName();
+
+
+}
