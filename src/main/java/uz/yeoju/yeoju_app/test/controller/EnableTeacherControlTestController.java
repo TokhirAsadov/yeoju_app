e0d0259f2cbdee8a196577bba34479c517a4967b
Takhir <guvalakat1603@gmail.com>
2024-08-13 10:42:19 +0500

    POST api/v1/desktop/controlTest/createEnableTeacher api is created

--- src/main/java/uz/yeoju/yeoju_app/test/controller/EnableTeacherControlTestController.java
+++ src/main/java/uz/yeoju/yeoju_app/test/controller/EnableTeacherControlTestController.java
@@ -4,13 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
 import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.secret.CurrentUser;
+import uz.yeoju.yeoju_app.test.payload.CreatorEnableTeacherControlTestDto;
 import uz.yeoju.yeoju_app.test.service.EnableTeacherControlTestService;
 
 @RestController
@@ -31,4 +30,10 @@ public class EnableTeacherControlTestController {
         return ResponseEntity.ok(service.getAllTeacherLessonsByTeacherId(teacherId));
     }
 
+    @PostMapping("/createEnableTeacher")
+    @PreAuthorize("hasRole('KAFEDRA')")
+    public HttpEntity<?> createEnableTeacher(@CurrentUser User user, @RequestBody CreatorEnableTeacherControlTestDto dto) {
+        ApiResponse response = service.createAndUpdateEnableTeacher(dto);
+        return ResponseEntity.status(response.isSuccess()?201:401).body(response);
+    }
 }
