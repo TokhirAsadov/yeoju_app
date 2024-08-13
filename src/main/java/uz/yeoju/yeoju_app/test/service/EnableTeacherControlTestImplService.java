8653b12e7b1a862934de2f5d6f8a7aa7c6762a7b
Takhir <guvalakat1603@gmail.com>
2024-08-13 10:03:15 +0500

    save function is created

--- src/main/java/uz/yeoju/yeoju_app/test/service/EnableTeacherControlTestImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/test/service/EnableTeacherControlTestImplService.java
@@ -2,14 +2,22 @@ package uz.yeoju.yeoju_app.test.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.teacher.Teacher;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.repository.LessonRepository;
+import uz.yeoju.yeoju_app.repository.TeacherRepository;
+import uz.yeoju.yeoju_app.repository.UserRepository;
 import uz.yeoju.yeoju_app.test.payload.CreatorEnableTeacherControlTestDto;
 import uz.yeoju.yeoju_app.test.repository.EnableTeacherControlTestRepository;
+
 
 @Service
 @RequiredArgsConstructor
 public class EnableTeacherControlTestImplService implements EnableTeacherControlTestService{
     private final EnableTeacherControlTestRepository repository;
+    private final UserRepository userRepository;
+    private final TeacherRepository teacherRepository;
+    private final LessonRepository lessonRepository;
 
     @Override
     public ApiResponse findAllEnableTeacherByKafedraId(String kafkaId) {
@@ -18,8 +26,16 @@ public class EnableTeacherControlTestImplService implements EnableTeacherControl
 
     @Override
     public ApiResponse createAndUpdateEnableTeacher(CreatorEnableTeacherControlTestDto dto) {
+        if (dto.id==null || dto.id.isEmpty()){
+            return save(dto);
+        }
+
+
         return null;
     }
 
+    private ApiResponse save(CreatorEnableTeacherControlTestDto dto) {
+        return null;
+    }
 
 }
