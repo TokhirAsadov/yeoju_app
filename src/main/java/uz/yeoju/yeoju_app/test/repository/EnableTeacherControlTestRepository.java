c4db15a2b580f352a60c5626d8daf9eda620c24c
Takhir <guvalakat1603@gmail.com>
2024-08-13 09:38:31 +0500

    EnableTeacherControlTest repository is created

---
+++ src/main/java/uz/yeoju/yeoju_app/test/repository/EnableTeacherControlTestRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.test.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.test.entity.EnableTeacherControlTest;
+
+public interface EnableTeacherControlTestRepository extends JpaRepository<EnableTeacherControlTest,String> {
+}
