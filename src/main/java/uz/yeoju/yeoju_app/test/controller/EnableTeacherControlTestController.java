0de2ad82a69515ccef5b33eefacad0d1abd54274
Takhir <guvalakat1603@gmail.com>
2024-08-13 10:39:49 +0500

    GET api/v1/desktop/controlTest/getAllTeacherLessons/{teacherId} api is created

--- src/main/java/uz/yeoju/yeoju_app/test/controller/EnableTeacherControlTestController.java
+++ src/main/java/uz/yeoju/yeoju_app/test/controller/EnableTeacherControlTestController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
 import uz.yeoju.yeoju_app.test.service.EnableTeacherControlTestService;
 
 @RestController
@@ -18,8 +20,15 @@ public class EnableTeacherControlTestController {
     private final EnableTeacherControlTestService service;
 
     @GetMapping("/findAllEnableTeacher/{kafedraId}")
-    @PreAuthorize("hasRole('ROLE_KAFEDRA')")
-    public HttpEntity<?> findAllEnableTeacher(@PathVariable("kafedraId") String kafedraId) {
+    @PreAuthorize("hasRole('KAFEDRA')")
+    public HttpEntity<?> findAllEnableTeacher(@CurrentUser User user, @PathVariable("kafedraId") String kafedraId) {
         return ResponseEntity.ok(service.findAllEnableTeacherByKafedraId(kafedraId));
     }
+
+    @GetMapping("/getAllTeacherLessons/{teacherId}")
+    @PreAuthorize("hasAnyRole('KAFEDRA','TEACHER')")
+    public HttpEntity<?> getAllTeacherLessonsByTeacherId(@CurrentUser User user,@PathVariable("teacherId") String teacherId) {
+        return ResponseEntity.ok(service.getAllTeacherLessonsByTeacherId(teacherId));
+    }
+
 }
