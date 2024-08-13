f3d500ef8355c5b3afe6147cc32681f186528fe3
Takhir <guvalakat1603@gmail.com>
2024-08-13 09:41:48 +0500

    findAllEnableTeacherByKafedraId function is created

--- src/main/java/uz/yeoju/yeoju_app/test/service/EnableTeacherControlTestService.java
+++ src/main/java/uz/yeoju/yeoju_app/test/service/EnableTeacherControlTestService.java
@@ -1,4 +1,7 @@
 package uz.yeoju.yeoju_app.test.service;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
 public interface EnableTeacherControlTestService {
+    ApiResponse findAllEnableTeacherByKafedraId(String kafkaId);
 }
