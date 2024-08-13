b27be02bc221e176b0371d248977d478f7166c23
Takhir <guvalakat1603@gmail.com>
2024-08-13 10:36:06 +0500

    GET api/v1/desktop/controlTest/findAllEnableTeacher/{kafedraId} api is created

--- src/main/java/uz/yeoju/yeoju_app/test/controller/EnableTeacherControlTestController.java
+++ src/main/java/uz/yeoju/yeoju_app/test/controller/EnableTeacherControlTestController.java
@@ -1,6 +1,11 @@
 package uz.yeoju.yeoju_app.test.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.yeoju.yeoju_app.controller.BaseUrl;
@@ -12,4 +17,9 @@ import uz.yeoju.yeoju_app.test.service.EnableTeacherControlTestService;
 public class EnableTeacherControlTestController {
     private final EnableTeacherControlTestService service;
 
+    @GetMapping("/findAllEnableTeacher/{kafedraId}")
+    @PreAuthorize("hasRole('ROLE_KAFEDRA')")
+    public HttpEntity<?> findAllEnableTeacher(@PathVariable("kafedraId") String kafedraId) {
+        return ResponseEntity.ok(service.findAllEnableTeacherByKafedraId(kafedraId));
+    }
 }
