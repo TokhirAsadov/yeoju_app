d4a63fd72b857ac65cbe60ee22533617c1ba2c9c
Takhir <guvalakat1603@gmail.com>
2024-09-26 16:00:14 +0500

    referral

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/dekanat/Referral.java
@@ -0,0 +1,40 @@
+package uz.yeoju.yeoju_app.entity.dekanat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.Faculty;
+import uz.yeoju.yeoju_app.entity.Group;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Referral extends AbsEntity {// sirtqi talimda uqiyatgan talabalar uchun yo`llanma talabaning ish joyi uchun
+
+    private Long queue;
+    @ManyToOne
+    private Dekanat dekanat;
+
+    private Long numberOfDecision;
+    private Date decisionDate;
+
+    private Integer course;
+    private Date fromDate;
+    private Date toDate;
+
+    @ManyToMany
+    private Set<Faculty> faculties;
+
+    @ManyToMany
+    private Set<Group> groups;
+    private String dynamicSection;
+
+}
