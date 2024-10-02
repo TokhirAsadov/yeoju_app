5952d0f4f2a79f9e42d7f5bbf1d0f4b11c8acf80
Takhir <guvalakat1603@gmail.com>
2024-10-02 11:41:16 +0500

    GetReferralDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/GetReferralDto.java
@@ -0,0 +1,24 @@
+package uz.yeoju.yeoju_app.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetReferralDto {
+    public String id;
+    public Long queue;
+    public String dynamicSection;
+    public Long numberOfDecision;
+    public Date decisionDate;
+    public Integer course;
+    public Date fromDate;
+    public Date toDate;
+    public Set<String> facultyNames;
+    public Set<String> groupNames;
+}
