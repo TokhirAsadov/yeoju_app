ce99b33542b9ad3e816837b8bd4e6f324308488b
Takhir <guvalakat1603@gmail.com>
2024-10-02 11:42:04 +0500

    createAndUpdate

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/referral/ReferralService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/referral/ReferralService.java
@@ -2,13 +2,14 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.referral;
 
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.ReferralCreateDto;
 import uz.yeoju.yeoju_app.payload.dekanat.NotificationOuterCreateDto;
 
 public interface ReferralService {
     ApiResponse findAllReferrals();
 //    ApiResponse getAllCounters();
 //    ApiResponse getStudentNotifications(String studentId);
-//    ApiResponse createAndUpdate(NotificationOuterCreateDto dto);
+    ApiResponse createAndUpdate(ReferralCreateDto dto);
 //
 //    ApiResponse delete(User user, String id);
 }
