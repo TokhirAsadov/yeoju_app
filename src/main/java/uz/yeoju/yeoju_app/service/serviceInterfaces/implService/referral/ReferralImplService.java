90efe36205b88ec5e40c43e60d540291fab0419e
Takhir <guvalakat1603@gmail.com>
2024-10-02 11:41:41 +0500

    generateDto

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/referral/ReferralImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/referral/ReferralImplService.java
@@ -2,8 +2,14 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.referral;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.Faculty;
+import uz.yeoju.yeoju_app.entity.Group;
+import uz.yeoju.yeoju_app.entity.dekanat.Referral;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.GetReferralDto;
 import uz.yeoju.yeoju_app.repository.ReferralRepository;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +19,21 @@ public class ReferralImplService implements ReferralService{
 
     @Override
     public ApiResponse findAllReferrals() {
-        return null;
+        return new ApiResponse(true,"all notifications", referralRepository.findAll().stream().map(this::generateDto).collect(Collectors.toSet()));
+    }
+
+    public GetReferralDto generateDto(Referral notification){
+        return new GetReferralDto(
+                notification.getId(),
+                notification.getQueue(),
+                notification.getDynamicSection(),
+                notification.getNumberOfDecision(),
+                notification.getDecisionDate(),
+                notification.getCourse(),
+                notification.getFromDate(),
+                notification.getToDate(),
+                notification.getFaculties().stream().map(Faculty::getShortName).collect(Collectors.toSet()),
+                notification.getGroups().stream().map(Group::getName).collect(Collectors.toSet())
+        );
     }
 }
