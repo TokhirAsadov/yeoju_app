dcf55609d846eeb295e1f1820ef07438501e725a
Takhir <guvalakat1603@gmail.com>
2024-10-09 10:26:15 +0500

    GetMonitoringByDay

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/staff/GetMonitoringByDay.java
@@ -0,0 +1,11 @@
+package uz.yeoju.yeoju_app.payload.staff;
+
+import java.util.Date;
+
+
+public interface GetMonitoringByDay {
+//     String getId();
+//     String getFullName();
+     Date getTime();
+     String getRoom();
+}
