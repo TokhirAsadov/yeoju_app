caf6b6426691df95d905855ff807ac4848061039
Takhir <guvalakat1603@gmail.com>
2024-10-20 14:19:19 +0500

    GetClassroomAttendance rest dto is added

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/statistics/GetClassroomAttendance.java
@@ -0,0 +1,8 @@
+package uz.yeoju.yeoju_app.payload.resDto.statistics;
+
+public interface GetClassroomAttendance {
+    String getClassroom();
+    Integer getPeriod();
+    Integer getAllStudent();
+    Integer getComeCount();
+}
