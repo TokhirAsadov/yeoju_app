90055a584253b63dd722590c0331a8afe3e440a2
Takhir <guvalakat1603@gmail.com>
2024-10-21 08:25:50 +0500

    getAllClassroomStatistics function is created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/statistics/CardDbStatisticsService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/statistics/CardDbStatisticsService.java
@@ -1,4 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.statistics;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
 public interface CardDbStatisticsService {
+    ApiResponse getAllClassroomStatistics(Integer year, Integer week,Integer weekday);
+
 }
