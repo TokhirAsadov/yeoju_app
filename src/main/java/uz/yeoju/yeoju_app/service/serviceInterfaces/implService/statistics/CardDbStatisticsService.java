bcdbef944a9e5e7b97c081fd1261ce8a10ee6125
Takhir <guvalakat1603@gmail.com>
2024-10-21 10:18:08 +0500

    getTotalAllClassroomStatisticsWithWeek function is created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/statistics/CardDbStatisticsService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/statistics/CardDbStatisticsService.java
@@ -5,6 +5,7 @@ import uz.yeoju.yeoju_app.payload.ApiResponse;
 public interface CardDbStatisticsService {
     ApiResponse getAllClassroomStatistics(Integer year, Integer week,Integer weekday);
     ApiResponse getTotalAllClassroomStatistics(Integer year, Integer week,Integer weekday);
+    ApiResponse getTotalAllClassroomStatisticsWithWeek(Integer year, Integer week);
     ApiResponse getTotalClassroomAttendanceByEduType(Integer year, Integer week,Integer weekday,String eduType);
 
     ApiResponse getTotalClassroomAttendanceByEduTypeAndFaculty(Integer year, Integer week, Integer weekday, String eduType, String faculty);
