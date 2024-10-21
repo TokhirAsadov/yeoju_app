10c1253920aa385cd5569ed349c60560143ed5d3
Takhir <guvalakat1603@gmail.com>
2024-10-21 08:26:24 +0500

    getTotalClassroomAttendanceByEduType function is created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/statistics/CardDbStatisticsService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/statistics/CardDbStatisticsService.java
@@ -5,5 +5,7 @@ import uz.yeoju.yeoju_app.payload.ApiResponse;
 public interface CardDbStatisticsService {
     ApiResponse getAllClassroomStatistics(Integer year, Integer week,Integer weekday);
     ApiResponse getTotalAllClassroomStatistics(Integer year, Integer week,Integer weekday);
+    ApiResponse getTotalClassroomAttendanceByEduType(Integer year, Integer week,Integer weekday,String eduType);
+
 
 }
