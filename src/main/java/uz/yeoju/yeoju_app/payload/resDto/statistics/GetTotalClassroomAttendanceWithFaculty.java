09e8fdc5da7eecc8e91a459fb61bf5844758e29f
Takhir <guvalakat1603@gmail.com>
2024-10-28 16:35:43 +0500

    rest dto is created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/statistics/GetTotalClassroomAttendanceWithFaculty.java
@@ -0,0 +1,8 @@
+package uz.yeoju.yeoju_app.payload.resDto.statistics;
+
+public interface GetTotalClassroomAttendanceWithFaculty {
+
+    Integer getTotalAllStudent();
+    Integer getTotalComeCount();
+    String getShortName();
+}
