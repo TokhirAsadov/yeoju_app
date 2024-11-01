f56a26955e24378e33d2a81fdf45c7def5b9486d
Takhir <guvalakat1603@gmail.com>
2024-11-01 09:24:17 +0500

    changing is added

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/GetStudentStatisticsForDeanOneWeekSection.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/student/GetStudentStatisticsForDeanOneWeekSection.java
@@ -1,9 +1,11 @@
 package uz.yeoju.yeoju_app.payload.resDto.student;
 
 import org.springframework.beans.factory.annotation.Value;
+import uz.yeoju.yeoju_app.payload.resDto.kafedra.StudentsDynamicAttendance;
 import uz.yeoju.yeoju_app.payload.resDto.kafedra.TeacherStatisticsOfWeekday;
 
 import java.util.List;
+import java.util.Set;
 
 public interface GetStudentStatisticsForDeanOneWeekSection {
     String getStudentId();
@@ -16,6 +18,9 @@ public interface GetStudentStatisticsForDeanOneWeekSection {
     Integer getWeek();
     Integer getYear();
 
-    @Value("#{@userRepository.getTimesForRoomStatisticsByUserIdAndWeekNEW(target.studentId,target.room,target.year,target.week,target.weekday,target.section)}")
-    List<TeacherStatisticsOfWeekday> getStatistics();
+//    @Value("#{@userRepository.getTimesForRoomStatisticsByUserIdAndWeekNEW(target.studentId,target.room,target.year,target.week,target.weekday,target.section)}")
+//    List<TeacherStatisticsOfWeekday> getStatistics();
+
+    @Value("#{@educationYearRepository.getTimesForRoomStatisticsByUserIdUnionNEW(target.studentId,target.room,target.year,target.week,target.weekday,target.section)}")
+    Set<StudentsDynamicAttendance> getStatistics();
 }
