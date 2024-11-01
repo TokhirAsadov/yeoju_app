8bb33069befc42148ead94cb80e09c20a43d5b67
Takhir <guvalakat1603@gmail.com>
2024-11-01 09:24:33 +0500

    GetTotalClassroomAttendanceEveryGroup rest dto is created

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/statistics/GetTotalClassroomAttendanceEveryGroup.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/statistics/GetTotalClassroomAttendanceEveryGroup.java
@@ -3,7 +3,9 @@ package uz.yeoju.yeoju_app.payload.resDto.statistics;
 public interface GetTotalClassroomAttendanceEveryGroup {
     Integer getTotalAllStudent();
     Integer getTotalComeCount();
-    String getShortName();
+    String getFacultyShortName();
+    String getFacultyName();
+    String getEduType();
     String getGroupName();
     Integer getCourse();
 }
