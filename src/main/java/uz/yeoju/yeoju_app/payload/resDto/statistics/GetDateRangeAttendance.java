d21ad0a2643b4bc66db217176a3b4433dfa15c0a
Takhir <guvalakat1603@gmail.com>
2024-11-04 13:33:35 +0500

    GetDateRangeAttendance rest dto is created

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/statistics/GetDateRangeAttendance.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.payload.resDto.statistics;
+
+import java.util.Date;
+
+public interface GetDateRangeAttendance {
+    Date getDateValue();
+    Integer getTotalAllStudent();
+    Integer getTotalComeCount();
+}
