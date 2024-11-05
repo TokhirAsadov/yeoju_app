0349358c2e4a2544af61bfe1a9bbebc0f3504119
Takhir <guvalakat1603@gmail.com>
2024-11-05 11:36:07 +0500

    changingTeacherData function is created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTable/changing/TimeTableChangingService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTable/changing/TimeTableChangingService.java
@@ -1,4 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.timeTable.changing;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.timetableChanging.ChangingTeacherDetailsDto;
+
 public interface TimeTableChangingService {
+    ApiResponse changingTeacherData(ChangingTeacherDetailsDto dto);
 }
