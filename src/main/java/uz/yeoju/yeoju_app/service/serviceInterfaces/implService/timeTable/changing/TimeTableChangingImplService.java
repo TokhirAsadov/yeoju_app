721b32a5cf4f02bd1bc4e9676fc26e3549d9d5f9
Takhir <guvalakat1603@gmail.com>
2024-11-05 11:36:29 +0500

    changingTeacherData function is implemented

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTable/changing/TimeTableChangingImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTable/changing/TimeTableChangingImplService.java
@@ -1,4 +1,11 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.timeTable.changing;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.timetableChanging.ChangingTeacherDetailsDto;
+
 public class TimeTableChangingImplService implements TimeTableChangingService{
+    @Override
+    public ApiResponse changingTeacherData(ChangingTeacherDetailsDto dto) {
+        return null;
+    }
 }
