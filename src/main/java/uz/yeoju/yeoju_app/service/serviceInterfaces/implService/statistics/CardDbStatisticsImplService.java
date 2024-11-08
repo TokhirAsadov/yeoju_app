7791f97befe1064a253b3df0ca975674357e238a
Takhir <guvalakat1603@gmail.com>
2024-11-08 08:37:49 +0500

    getPassedTeachers function is done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/statistics/CardDbStatisticsImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/statistics/CardDbStatisticsImplService.java
@@ -2,11 +2,20 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.statistics;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.repository.statistics.CardDbStatisticsRepository;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class CardDbStatisticsImplService implements CardDbStatisticsService{
     private final CardDbStatisticsRepository repository;
+
+    @Override
+    public ApiResponse getPassedTeachers(Integer year, Integer week, Integer weekday) {
+        return new ApiResponse(true,"all passed teachers", repository.getPassedTeachers(year, week, weekday));
+    }
+
 
 }
