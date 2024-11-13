96c3afda77695317c461abcd9b6a19ab3f09ce5a
Takhir <guvalakat1603@gmail.com>
2024-11-13 09:06:24 +0500

    getDataOfTeachers function is created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTable/changing/TimeTableChangingService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTable/changing/TimeTableChangingService.java
@@ -1,8 +1,10 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.timeTable.changing;
 
+import uz.yeoju.yeoju_app.entity.educationYear.WeekType;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.timetableChanging.ChangingTeacherDetailsDto;
 
 public interface TimeTableChangingService {
     ApiResponse changingTeacherData(ChangingTeacherDetailsDto dto);
+    ApiResponse getDataOfTeachers(WeekType weekType, Integer year, Integer week);
 }
