a477adb69f9fdb785ff09dd4e73ecacc3792aa87
Takhir <guvalakat1603@gmail.com>
2024-11-21 14:00:32 +0500

    changingRoomOfLesson fucntion is created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTable/changing/TimeTableChangingService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/timeTable/changing/TimeTableChangingService.java
@@ -2,9 +2,11 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.timeTable.chang
 
 import uz.yeoju.yeoju_app.entity.educationYear.WeekType;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.timetableChanging.ChangingRoomOfLessonDetailsDto;
 import uz.yeoju.yeoju_app.payload.timetableChanging.ChangingTeacherDetailsDto;
 
 public interface TimeTableChangingService {
     ApiResponse changingTeacherData(ChangingTeacherDetailsDto dto);
+    ApiResponse changingRoomOfLesson(ChangingRoomOfLessonDetailsDto dto);
     ApiResponse getDataOfTeachers(WeekType weekType, Integer year, Integer week);
 }
