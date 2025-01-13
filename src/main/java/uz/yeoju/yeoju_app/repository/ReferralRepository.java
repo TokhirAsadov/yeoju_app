9b6312f2b0f29c8ac69766c566b7d91ff6cf7071
Takhir <guvalakat1603@gmail.com>
2025-01-13 21:45:03 +0500

    GetStudentReferrals is used

--- src/main/java/uz/yeoju/yeoju_app/repository/ReferralRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/ReferralRepository.java
@@ -3,30 +3,27 @@ package uz.yeoju.yeoju_app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import uz.yeoju.yeoju_app.entity.dekanat.NotificationOuter;
 import uz.yeoju.yeoju_app.entity.dekanat.Referral;
-import uz.yeoju.yeoju_app.payload.resDto.dekan.dekanat.GetStudentNotificationOuters;
+import uz.yeoju.yeoju_app.payload.resDto.dekan.dekanat.GetStudentReferrals;
 
 import java.util.Set;
 
 public interface ReferralRepository extends JpaRepository<Referral,String> {
-    @Query(value = "select n.id,ey.name as educationYear, n.fromDate,n.toDate,n.createdAt,n.queue from NotificationOuter n \n" +
-            "join EducationYear ey on ey.id = n.educationYear_id\n" +
-            "join NotificationOuter_groups NOg on n.id = NOg.NotificationOuter_id\n" +
-            "join groups g on NOg.groups_id = g.id\n" +
-            "join Student S on g.id = S.group_id\n" +
-            "where s.user_id=:studentId order by n.createdAt desc ",nativeQuery = true)
-    Set<GetStudentNotificationOuters> getStudentNotificationOuters(@Param("studentId") String studentId);
+    @Query(value = "select r.id, r.fromDate,r.toDate,r.createdAt,r.queue,r.decisionDate,r.numberOfDecision from Referral r\n" +
+            "       join Referral_groups r_g on r.id = r_g.Referral_id\n" +
+            "       join groups g on r_g.groups_id = g.id\n" +
+            "       join Student S on g.id = S.group_id\n" +
+            "where s.user_id=:studentId order by r.createdAt desc",nativeQuery = true)
+    Set<GetStudentReferrals> getStudentReferrals(@Param("studentId") String studentId);
 
 
-    @Query(value = "select n.id,ey.name as educationYear, n.fromDate,n.toDate,n.createdAt,n.queue,n.dynamicSection,NOC.queue as studentQueue from NotificationOuter n\n" +
-            "   join EducationYear ey on ey.id = n.educationYear_id\n" +
-            "   join NotificationOuter_groups NOg on n.id = NOg.NotificationOuter_id\n" +
-            "   join groups g on NOg.groups_id = g.id\n" +
-            "   join Student S on g.id = S.group_id\n" +
-            "   join NotificationOuterCounter NOC on n.id = NOC.notificationOuter_id                                                                              \n" +
-            "where s.user_id=:studentId and NOC.user_id=:studentId order by n.createdAt desc",nativeQuery = true)
-    Set<GetStudentNotificationOuters> getStudentNotificationOuters2(@Param("studentId") String studentId);
+    @Query(value = "select r.id, r.fromDate,r.toDate,r.createdAt,r.queue,r.decisionDate,r.numberOfDecision,r_c.queue as studentQueue from Referral r\n" +
+            "    join Referral_groups r_g on r.id = r_g.Referral_id\n" +
+            "    join groups g on r_g.groups_id = g.id\n" +
+            "    join Student S on g.id = S.group_id\n" +
+            "  join ReferralCounter r_c on r.id = r_c.referral_id\n" +
+            "where s.user_id=:studentId and r_c.user_id=:studentId order by r.createdAt desc",nativeQuery = true)
+    Set<GetStudentReferrals> getStudentNotificationOuters2(@Param("studentId") String studentId);
 
     @Query(value = "select max(queue) from Referral",nativeQuery = true)
     Long maxQueue();
