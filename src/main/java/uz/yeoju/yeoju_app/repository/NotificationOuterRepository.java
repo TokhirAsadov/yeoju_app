cbdb95367a52848fdef97297759136f3c14917bb
Takhir <guvalakat1603@gmail.com>
2025-01-23 12:24:59 +0500

    query is changed

--- src/main/java/uz/yeoju/yeoju_app/repository/NotificationOuterRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/NotificationOuterRepository.java
@@ -22,12 +22,18 @@ public interface NotificationOuterRepository extends JpaRepository<NotificationO
     Set<GetStudentNotificationOuters> getStudentNotificationOuters(@Param("studentId") String studentId);
 
 
-    @Query(value = "select n.id,ey.name as educationYear, n.fromDate,n.toDate,n.createdAt,n.queue,n.dynamicSection,NOC.queue as studentQueue from NotificationOuter n\n" +
-            "   join EducationYear ey on ey.id = n.educationYear_id\n" +
-            "   join NotificationOuter_groups NOg on n.id = NOg.NotificationOuter_id\n" +
-            "   join groups g on NOg.groups_id = g.id\n" +
-            "   join Student S on g.id = S.group_id\n" +
-            "   join NotificationOuterCounter NOC on n.id = NOC.notificationOuter_id                                                                              \n" +
+    @Query(value = "select n.id,ey.name as educationYear, u2.fullName as dekan, n.fromDate,n.toDate,n.createdAt,n.queue,n.dynamicSection,NOC.queue as studentQueue from NotificationOuter n\n" +
+            "      join EducationYear ey on ey.id = n.educationYear_id\n" +
+            "      join NotificationOuter_groups NOg on n.id = NOg.NotificationOuter_id\n" +
+            "      join groups g on NOg.groups_id = g.id\n" +
+            "      join Faculty f on g.faculty_id = f.id\n" +
+            "      join EducationType et on et.id=g.educationType_id\n" +
+            "      join Dekanat_Faculty df on f.id = df.faculties_id\n" +
+            "      join Dekanat d on df.Dekanat_id = d.id\n" +
+            "      join Dekanat_EducationType de on de.eduType_id=et.id and de.Dekanat_id=d.id\n" +
+            "      join users u2 on d.owner_id=u2.id\n" +
+            "      join Student S on g.id = S.group_id\n" +
+            "      join NotificationOuterCounter NOC on n.id = NOC.notificationOuter_id\n" +
             "where s.user_id=:studentId and NOC.user_id=:studentId order by n.createdAt desc",nativeQuery = true)
     Set<GetStudentNotificationOuters> getStudentNotificationOuters2(@Param("studentId") String studentId);
 
