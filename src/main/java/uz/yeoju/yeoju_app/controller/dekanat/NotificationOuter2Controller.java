79f8fdfe4e8f7862fcb15a68284d0a2fa31183c8
Takhir <guvalakat1603@gmail.com>
2025-02-10 11:59:25 +0500

    NotificationOuter2Controller

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/dekanat/NotificationOuter2Controller.java
@@ -0,0 +1,53 @@
+package uz.yeoju.yeoju_app.controller.dekanat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.dekanat.NotificationOuterCreateDto;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.notificationOuter2.NotificationOuter2Service;
+
+@RestController
+@RequestMapping(BaseUrl.BASE_URL+"/operation")
+@RequiredArgsConstructor
+public class NotificationOuter2Controller {
+    private final NotificationOuter2Service service;
+
+    @GetMapping("/getAllOuterNotifications")
+    public HttpEntity<?> getAllOuterNotifications(@CurrentUser User user) {
+        return ResponseEntity.ok(service.findAllNotifications(user));
+    }
+
+    @GetMapping("/getAllCounters")
+    public HttpEntity<?> getAllCounters() {
+        return ResponseEntity.ok(service.getAllCounters());
+    }
+
+    @GetMapping("/getStudentOuterNotifications/{studentId}")
+    public HttpEntity<?> getStudentOuterNotifications(@PathVariable("studentId") String studentId) {
+        return ResponseEntity.ok(service.getStudentNotifications(studentId));
+    }
+
+    @PostMapping("/save")
+    public HttpEntity<?> save(@RequestBody NotificationOuterCreateDto dto){
+        ApiResponse response = service.createAndUpdate(dto);
+        return ResponseEntity.status(response.isSuccess() ? 201 : 409).body(response);
+    }
+    @PutMapping("/update")
+    public HttpEntity<?> update(@RequestBody NotificationOuterCreateDto dto){
+        ApiResponse response = service.createAndUpdate(dto);
+        return ResponseEntity.status(response.isSuccess() ? 200 : 409).body(response);
+    }
+
+    @PreAuthorize("hasRole('DEKAN')")
+    @DeleteMapping("/deleteNO/{id}")
+    public HttpEntity<?> delete(@CurrentUser User user, @PathVariable("id") String id){
+        ApiResponse delete = service.delete(user, id);
+        return ResponseEntity.status(delete.isSuccess() ? 204 : 409).body(delete);
+    }
+}
