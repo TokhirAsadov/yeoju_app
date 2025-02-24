e9616ac5a32652395d8f7312d6fb9751cd8ec7e0
Takhir <guvalakat1603@gmail.com>
2025-02-24 15:41:51 +0500

    createDiploma

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/diploma/DiplomaImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/diploma/DiplomaImplService.java
@@ -1,6 +1,9 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dekanat.diploma;
 
 import org.springframework.stereotype.Service;
+import uz.yeoju.yeoju_app.entity.dekanat.Diploma;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.dekanat.DiplomaCreator;
 import uz.yeoju.yeoju_app.repository.DiplomaRepository;
 
 @Service
@@ -9,5 +12,45 @@ public class DiplomaImplService implements DiplomaService{
 
     public DiplomaImplService(DiplomaRepository diplomaRepository) {
         this.diplomaRepository = diplomaRepository;
+    }
+
+    @Override
+    public ApiResponse createDiploma(DiplomaCreator creator) {
+        Boolean existsed = diplomaRepository.existsDiplomaByLogin(creator.getLogin());
+        if(!existsed){
+            if (!diplomaRepository.existsDiplomaByDiplomId(creator.getDiplomId())) {
+                if (!diplomaRepository.existsDiplomaByDiplomRaqami(creator.diplomRaqami)){
+                    Diploma diploma = Diploma.builder()
+                            .diplomId(creator.getDiplomId())
+                            .diplomSeriya(creator.getDiplomSeriya())
+                            .diplomRaqami(creator.getDiplomRaqami())
+                            .fName(creator.getFName())
+                            .lName(creator.getLName())
+                            .mName(creator.getMName())
+                            .fNameEng(creator.getFNameEng())
+                            .lNameEng(creator.getLNameEng())
+                            .yonalishQisqa(creator.getYonalishQisqa())
+                            .yonalishEng(creator.getYonalishEng())
+                            .yonalishUzb(creator.getYonalishUzb())
+                            .maktab(creator.getMaktab())
+                            .bachelorOf(creator.getBachelorOf())
+                            .imtiyoz(creator.getImtiyoz())
+                            .imtiyozEng(creator.getImtiyozEng())
+                            .login(creator.getLogin())
+                            .build();
+                    diplomaRepository.save(diploma);
+                    return new ApiResponse(true,creator.getLogin()+" diploma is saved");
+                }
+                else {
+                    throw new IllegalArgumentException("The diploma is already with raqam: "+creator.getDiplomRaqami());
+                }
+            }
+            else {
+                throw new IllegalArgumentException("The diploma is already with ID: "+creator.getDiplomId());
+            }
+        }
+        else {
+            throw new IllegalArgumentException("The diploma is already with login: "+(creator.getLogin()));
+        }
     }
 }
