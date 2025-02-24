13f3b58ff519973f7eb614eac5a8da623a175e88
Takhir <guvalakat1603@gmail.com>
2025-02-24 15:41:47 +0500

    createDiploma

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/diploma/DiplomaService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/diploma/DiplomaService.java
@@ -1,4 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dekanat.diploma;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.dekanat.DiplomaCreator;
+
 public interface DiplomaService {
+    ApiResponse createDiploma(DiplomaCreator creator);
 }
