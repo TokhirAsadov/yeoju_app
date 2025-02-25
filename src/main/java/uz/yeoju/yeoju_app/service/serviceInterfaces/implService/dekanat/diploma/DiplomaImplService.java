feaaaee577b23cb4d7dff8274085b3e29184aba4
Takhir <guvalakat1603@gmail.com>
2025-02-25 08:48:57 +0500

    uploadDiploma

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/diploma/DiplomaImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/diploma/DiplomaImplService.java
@@ -1,10 +1,24 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dekanat.diploma;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import uz.yeoju.yeoju_app.entity.Student;
+import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.entity.dekanat.Diploma;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.dekanat.DiplomaCreator;
 import uz.yeoju.yeoju_app.repository.DiplomaRepository;
+
+import java.util.Iterator;
+import java.util.Objects;
 
 @Service
 public class DiplomaImplService implements DiplomaService{
@@ -103,4 +117,95 @@ public class DiplomaImplService implements DiplomaService{
             throw new IllegalArgumentException("The diploma is not found with ID: "+creator.id);
         }
     }
+
+    @Override
+    @Transactional
+    public ApiResponse uploadDiploma(MultipartHttpServletRequest request) {
+        Iterator<String> fileNames = request.getFileNames();
+        while (fileNames.hasNext()) {
+            MultipartFile file = request.getFile(fileNames.next());
+            if (file != null) {
+                return readDataFromExcel(file);
+            }
+        }
+        return null;
+    }
+
+    @Transactional
+    public ApiResponse readDataFromExcel(MultipartFile file) {
+        try {
+            Workbook workbook = getWorkBook(file);
+            Sheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rows = sheet.iterator();
+            rows.next();
+            while (rows.hasNext()){
+                Row row = rows.next();
+                Boolean existsed = diplomaRepository.existsDiplomaByLogin(row.getCell(0).getStringCellValue());
+                if(!existsed){
+                    if (!diplomaRepository.existsDiplomaByDiplomId(row.getCell(1).getStringCellValue())) {
+                        if (!diplomaRepository.existsDiplomaByDiplomRaqami(row.getCell(3).getStringCellValue())){
+                            Diploma diploma = Diploma.builder()
+                                    .login(row.getCell(0).getStringCellValue())
+                                    .diplomId(row.getCell(1).getStringCellValue())
+                                    .diplomSeriya(row.getCell(2).getStringCellValue())
+                                    .diplomRaqami(row.getCell(3).getStringCellValue())
+                                    .lName(row.getCell(4).getStringCellValue())
+                                    .fName(row.getCell(5).getStringCellValue())
+                                    .mName(row.getCell(6).getStringCellValue())
+                                    .lNameEng(row.getCell(7).getStringCellValue())
+                                    .fNameEng(row.getCell(8).getStringCellValue())
+                                    .yonalishQisqa(row.getCell(9).getStringCellValue())
+                                    .yonalishUzb(row.getCell(10).getStringCellValue())
+                                    .yonalishEng(row.getCell(11).getStringCellValue())
+                                    .maktab(row.getCell(12).getStringCellValue())
+                                    .bachelorOf(row.getCell(13).getStringCellValue())
+                                    .build();
+
+                            if (row.getCell(14)!=null && !Objects.equals(row.getCell(14).getStringCellValue(), "")){
+                                diploma.setImtiyoz(row.getCell(14).getStringCellValue());
+                            }
+                            if (row.getCell(15)!=null && !Objects.equals(row.getCell(15).getStringCellValue(), "")){
+                                diploma.setImtiyozEng(row.getCell(15).getStringCellValue());
+                            }
+
+                            diplomaRepository.save(diploma);
+                        }
+                        else {
+                            throw new IllegalArgumentException("The diploma is already with raqam: "+row.getCell(3).getStringCellValue());
+                        }
+                    }
+                    else {
+                        throw new IllegalArgumentException("The diploma is already with ID: "+row.getCell(1).getStringCellValue());
+                    }
+                }
+                else {
+                    throw new IllegalArgumentException("The diploma is already with login: "+row.getCell(0).getStringCellValue());
+                }
+            }
+
+            return new ApiResponse(true,"Diplomas are saved.");
+        }catch (Exception e){
+            return new ApiResponse(false,"Error is occurred while saving diplomas");
+        }
+    }
+
+    private Workbook getWorkBook(MultipartFile file) {
+        Workbook workbook = null;
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        try {
+            assert extension != null;
+            if (extension.equalsIgnoreCase("xlsx")){
+                workbook = new XSSFWorkbook(file.getInputStream());
+
+            } else if (extension.equalsIgnoreCase("xls")){
+                workbook = new HSSFWorkbook(file.getInputStream());
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return workbook;
+    }
+
+
 }
