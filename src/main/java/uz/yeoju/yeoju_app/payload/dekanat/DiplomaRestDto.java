645c9303d63508b2d9ca5b2d45d710743ebebf42
Takhir <guvalakat1603@gmail.com>
2025-02-25 13:49:20 +0500

    @JsonProperty

--- src/main/java/uz/yeoju/yeoju_app/payload/dekanat/DiplomaRestDto.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/dekanat/DiplomaRestDto.java
@@ -1,17 +1,25 @@
 package uz.yeoju.yeoju_app.payload.dekanat;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public interface DiplomaRestDto {
     String getUserId();
     String getFullName();
     String getLogin();
     String getDiplomId();
+    @JsonProperty("ID")
     String getID();
     String getDiplomSeriya();
     String getDiplomRaqami();
+    @JsonProperty("fName")
     String getFName();
+    @JsonProperty("mName")
     String getMName();
+    @JsonProperty("lName")
     String getLName();
+    @JsonProperty("fNameEng")
     String getFNameEng();
+    @JsonProperty("lNameEng")
     String getLNameEng();
     String getYonalishQisqa();
     String getYonalishUzb();
