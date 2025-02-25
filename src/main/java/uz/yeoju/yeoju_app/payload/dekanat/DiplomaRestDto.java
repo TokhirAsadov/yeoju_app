26e610ea401f0c8f51d277d1f215a259fec4dc2d
Takhir <guvalakat1603@gmail.com>
2025-02-25 10:35:11 +0500

    DiplomaRestDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/dekanat/DiplomaRestDto.java
@@ -0,0 +1,23 @@
+package uz.yeoju.yeoju_app.payload.dekanat;
+
+public interface DiplomaRestDto {
+    String getUserId();
+    String getFullName();
+    String getLogin();
+    String getDiplomId();
+    String getID();
+    String getDiplomSeriya();
+    String getDiplomRaqami();
+    String getFName();
+    String getMName();
+    String getLName();
+    String getFNameEng();
+    String getLNameEng();
+    String getYonalishQisqa();
+    String getYonalishUzb();
+    String getYonalishEng();
+    String getMaktab();
+    String getBachelorOf();
+    String getImtiyoz();
+    String getImtiyozEng();
+}
