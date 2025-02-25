ae0851fc6a8594f5588baa2a26a14fd1f8205616
Takhir <guvalakat1603@gmail.com>
2025-02-25 08:48:50 +0500

    uploadDiploma

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/diploma/DiplomaService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/diploma/DiplomaService.java
@@ -1,9 +1,12 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dekanat.diploma;
 
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.dekanat.DiplomaCreator;
 
 public interface DiplomaService {
     ApiResponse createDiploma(DiplomaCreator creator);
     ApiResponse updateDiploma(DiplomaCreator creator);
+
+    ApiResponse uploadDiploma(MultipartHttpServletRequest request);
 }
