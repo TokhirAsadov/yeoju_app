5d4e9e1384167c87b62655ab5e1b040cb84ea2e8
Takhir <guvalakat1603@gmail.com>
2025-05-08 10:22:51 +0500

    GetFaculties

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/dekan/GetFaculties.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.payload.resDto.dekan;
+
+public interface GetFaculties {
+    String getId();
+    String getName();
+    String getNameEn();
+    String getShortName();
+    String getSchoolCode();
+}
