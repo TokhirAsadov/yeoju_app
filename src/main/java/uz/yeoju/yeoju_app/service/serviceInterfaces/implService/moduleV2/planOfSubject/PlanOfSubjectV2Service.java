2aa11adacd3ebf9b1eb094d15cea761501e57aa1
Takhir <guvalakat1603@gmail.com>
2025-05-15 10:43:01 +0500

    PlanOfSubjectV2Service

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/planOfSubject/PlanOfSubjectV2Service.java
@@ -0,0 +1,19 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.planOfSubject;
+
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.ApiResponseTwoObj;
+import uz.yeoju.yeoju_app.payload.module.CreatePlanOfStudent;
+import uz.yeoju.yeoju_app.payload.moduleV2.CreatePlanOfStudentV2;
+
+public interface PlanOfSubjectV2Service {
+     ApiResponseTwoObj getPlansForTeacherSciences(String teacherId, String educationId, String subjectId, Integer groupLevel);
+     ApiResponseTwoObj getAllPlansForTeacherSciences(String teacherId, String educationId);
+
+     ApiResponse createPlan(User user, CreatePlanOfStudentV2 dto);
+
+     ApiResponse getExistPlans(String teacherId, String educationId, String subjectId, Integer groupLevel);
+
+
+     ApiResponse getTeacherWIthSubjectForPlan(String id,String educationYearId);
+}
