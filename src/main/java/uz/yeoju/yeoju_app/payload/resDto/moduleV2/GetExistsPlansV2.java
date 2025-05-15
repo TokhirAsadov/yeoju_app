1e8c0e80f83151a940f59ed4698185f08affb1dc
Takhir <guvalakat1603@gmail.com>
2025-05-15 09:48:59 +0500

    GetExistsPlansV2

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/moduleV2/GetExistsPlansV2.java
@@ -0,0 +1,10 @@
+package uz.yeoju.yeoju_app.payload.resDto.moduleV2;
+
+public interface GetExistsPlansV2 {
+    String getId();
+    String getEduType();
+    String getLessonId();
+    String getLessonName();
+    String getEduLang();
+    Integer getLevel();
+}
