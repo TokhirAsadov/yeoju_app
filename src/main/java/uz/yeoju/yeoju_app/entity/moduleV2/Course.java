f475316d2384db1230c4162ad4b44309cc753120
Takhir <guvalakat1603@gmail.com>
2025-05-15 09:46:48 +0500

    Course

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/Course.java
@@ -0,0 +1,30 @@
+package uz.yeoju.yeoju_app.entity.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course extends AbsEntity {
+    private String title;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "plan_id")
+    private PlanOfSubjectV2 plan;
+
+    @OneToMany(mappedBy = "course")
+    private List<Module> modules = new ArrayList<>();
+
+    public Course(String title, PlanOfSubjectV2 plan) {
+        this.title = title;
+        this.plan = plan;
+    }
+}
