a3fcc7c1f1cdd6e739a40320ce43a9918c34b4dc
Takhir <guvalakat1603@gmail.com>
2025-05-15 09:47:23 +0500

    CourseService

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/course/CourseService.java
@@ -0,0 +1,12 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.course;
+
+import org.springframework.data.domain.Pageable;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.moduleV2.CourseCreator;
+
+public interface CourseService {
+    void createCourse(CourseCreator creator);
+    boolean deleteCourse(String courseId);
+    ApiResponse findAll(Pageable pageable);
+    ApiResponse findById(String id);
+}
