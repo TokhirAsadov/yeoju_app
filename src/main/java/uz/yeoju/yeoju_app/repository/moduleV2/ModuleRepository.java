7f1f2df25211fa91bdcbd0484a6f8710bb544ea6
Takhir <guvalakat1603@gmail.com>
2025-05-15 10:06:55 +0500

    ModuleRepository

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/ModuleRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository.moduleV2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.moduleV2.Module;
+
+public interface ModuleRepository extends JpaRepository<Module,String> {
+}
