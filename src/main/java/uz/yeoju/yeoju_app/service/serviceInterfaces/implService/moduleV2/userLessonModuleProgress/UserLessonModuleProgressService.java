987c50078b316bab019839ab6b1575f2a317e5f0
Takhir <guvalakat1603@gmail.com>
2025-05-16 09:30:26 +0500

    UserLessonModuleProgressService.java

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/userLessonModuleProgress/UserLessonModuleProgressService.java
@@ -0,0 +1,12 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.userLessonModuleProgress;
+
+import org.springframework.data.domain.Pageable;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.moduleV2.UserLessonModuleProgressCreator;
+
+public interface UserLessonModuleProgressService {
+    void create(UserLessonModuleProgressCreator creator);
+    boolean delete(String id);
+    ApiResponse findAll(Pageable pageable);
+    ApiResponse findById(String id);
+}
