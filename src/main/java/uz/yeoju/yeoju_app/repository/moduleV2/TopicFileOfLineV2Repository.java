bb0b029177b0808e699f51fabc2c987b60177147
Takhir <guvalakat1603@gmail.com>
2025-05-16 09:12:19 +0500

    TopicFileOfLineV2Repository

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/TopicFileOfLineV2Repository.java
@@ -0,0 +1,16 @@
+package uz.yeoju.yeoju_app.repository.moduleV2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import uz.yeoju.yeoju_app.entity.moduleV2.TopicFileOfLineV2;
+import uz.yeoju.yeoju_app.payload.resDto.module.GetFileOfLine;
+
+import java.util.Optional;
+import java.util.Set;
+
+public interface TopicFileOfLineV2Repository extends JpaRepository<TopicFileOfLineV2,String> {
+    Optional<TopicFileOfLineV2> findTopicFileOfLineV2ByName(String name);
+
+    @Query(value = "select t.id,t.name,t.type,t.fileType,t.url from TopicFileOfLineV2 t join TopicFileOfLineV2_LessonModule t_l on t.id = t_l.TopicFileOfLineV2_id where t_l.lessons_id=?1 order by t.createdAt",nativeQuery = true)
+    Set<GetFileOfLine> getFilesByLessonModuleId(String lessonModuleId);
+}
