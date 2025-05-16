38c572bf29f5fb6f33c97b2086177593735e7ef4
Takhir <guvalakat1603@gmail.com>
2025-05-16 09:29:57 +0500

    UserLessonModuleProgress

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/UserLessonModuleProgress.java
@@ -0,0 +1,29 @@
+package uz.yeoju.yeoju_app.entity.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLessonModuleProgress extends AbsEntity {
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "lesson_id")
+    private LessonModule lesson;
+
+    private boolean completed;
+}
