6adbd78d82005e4bbbbdd946886b233a1946ba3b
Takhir <guvalakat1603@gmail.com>
2025-05-16 09:30:16 +0500

    UserLessonModuleProgressRepository

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/UserLessonModuleProgressRepository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository.moduleV2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.moduleV2.UserLessonModuleProgress;
+
+public interface UserLessonModuleProgressRepository extends JpaRepository<UserLessonModuleProgress,String> {
+}
