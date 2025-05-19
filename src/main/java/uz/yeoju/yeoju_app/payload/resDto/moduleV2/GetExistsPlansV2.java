92054f150422c3abf2f0b308b9da1249f318e5ec
Takhir <guvalakat1603@gmail.com>
2025-05-19 09:23:56 +0500

    adding

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/moduleV2/GetExistsPlansV2.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/moduleV2/GetExistsPlansV2.java
@@ -1,10 +1,16 @@
 package uz.yeoju.yeoju_app.payload.resDto.moduleV2;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface GetExistsPlansV2 {
     String getId();
+    String getTeacherId();
     String getEduType();
     String getLessonId();
     String getLessonName();
     String getEduLang();
     Integer getLevel();
+
+    @Value("#{@planOfSubjectV2Repository.getTeacherWIthSubjectForPlanGetData(target.teacherId)}")
+    GetTeacherWIthSubjectForPlanV2 getUserData();
 }
