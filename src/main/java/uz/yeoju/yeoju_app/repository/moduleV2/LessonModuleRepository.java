419ac1e6be2c81bf3df3d510df22b8f668afa84a
Takhir <guvalakat1603@gmail.com>
2025-05-20 16:04:12 +0500

    removing

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/LessonModuleRepository.java
+++
@@ -1,7 +0,0 @@
-package uz.yeoju.yeoju_app.repository.moduleV2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.yeoju.yeoju_app.entity.moduleV2.LessonModule;
-
-public interface LessonModuleRepository extends JpaRepository<LessonModule,String> {
-}
