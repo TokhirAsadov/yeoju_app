6d08ab536364ab2afa5a0567db01d144aac5e47e
Takhir <guvalakat1603@gmail.com>
2025-05-20 15:04:15 +0500

    CourseResponse

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CourseResponse.java
@@ -0,0 +1,13 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseResponse {
+    public String id;
+    public String title;
+}
