e263e756f41832e4b3a004163bdc8403d1e42d72
Takhir <guvalakat1603@gmail.com>
2025-05-20 14:35:57 +0500

    ModuleAdminResponse

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleProgressResponse.java
@@ -0,0 +1,17 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModuleProgressResponse {
+    private String title;
+    private double progress;
+    private String done;
+    private List<LessonModuleResponse> lessons;
+}
