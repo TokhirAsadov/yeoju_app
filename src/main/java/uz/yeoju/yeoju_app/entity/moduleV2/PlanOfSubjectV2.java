e82e4d0c475be2d9611ce305651334f2093f039b
Takhir <guvalakat1603@gmail.com>
2025-05-21 11:41:04 +0500

    changing

--- src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/PlanOfSubjectV2.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/PlanOfSubjectV2.java
@@ -1,5 +1,6 @@
 package uz.yeoju.yeoju_app.entity.moduleV2;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,10 @@ import uz.yeoju.yeoju_app.entity.*;
 import uz.yeoju.yeoju_app.entity.educationYear.EducationYear;
 import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -24,6 +27,9 @@ public class PlanOfSubjectV2 extends AbsEntity {
     @ManyToOne
     private Lesson subject;
 
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    private Set<Group> groups;
+
     @ManyToOne
     private EducationType educationType;
 
@@ -31,4 +37,27 @@ public class PlanOfSubjectV2 extends AbsEntity {
     private EducationLanguage educationLanguage;
 
     private Integer level;
+
+    @OneToMany(mappedBy = "plan", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Course> courses = new ArrayList<>();
+
+    public PlanOfSubjectV2(User user, EducationYear educationYear, Lesson subject, Set<Group> groups, EducationType educationType, EducationLanguage educationLanguage, Integer level) {
+        this.user = user;
+        this.educationYear = educationYear;
+        this.subject = subject;
+        this.groups = groups;
+        this.educationType = educationType;
+        this.educationLanguage = educationLanguage;
+        this.level = level;
+    }
+
+    public PlanOfSubjectV2(User user, EducationYear educationYear, Lesson subject, EducationType educationType, EducationLanguage educationLanguage, Integer level) {
+        this.user = user;
+        this.educationYear = educationYear;
+        this.subject = subject;
+        this.educationType = educationType;
+        this.educationLanguage = educationLanguage;
+        this.level = level;
+    }
 }
