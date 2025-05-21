00323236c4b8f0ddff8a807ff52b2d9f3aa4958e
Takhir <guvalakat1603@gmail.com>
2025-05-21 09:24:35 +0500

    adding

--- src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/DownloadCounterV2.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/DownloadCounterV2.java
@@ -1,13 +1,13 @@
 package uz.yeoju.yeoju_app.entity.moduleV2;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -17,6 +17,7 @@ public class DownloadCounterV2 extends AbsEntity {
     @ManyToOne
     private User user;
     @ManyToOne
+    @JoinColumn(name = "topic_id")
     private TopicFileOfLineV2 topic;
     private Integer count=0;
 }
