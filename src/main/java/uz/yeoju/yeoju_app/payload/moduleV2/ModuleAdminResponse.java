27d76637779ebc96a555111bc7a4373cdb2f869f
Takhir <guvalakat1603@gmail.com>
2025-05-21 11:44:05 +0500

    changing

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleAdminResponse.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleAdminResponse.java
@@ -4,13 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ModuleAdminResponse {
+    private String id;
     private String title;
-    private List<LessonAdminResponse> lessons;
+    private String theme;
 }
 
