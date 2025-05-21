1eb457501bedd1f6832f050fec39fac110107cf6
Takhir <guvalakat1603@gmail.com>
2025-05-21 11:43:06 +0500

    updateModule() function is created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/module/ModuleService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/module/ModuleService.java
@@ -3,9 +3,11 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.module
 import org.springframework.data.domain.Pageable;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.moduleV2.ModuleCreator;
+import uz.yeoju.yeoju_app.payload.moduleV2.ModuleUpdater;
 
 public interface ModuleService {
     void createModule(ModuleCreator creator);
+    ApiResponse updateModule(ModuleUpdater updater);
     boolean deleteModule(String moduleId);
     ApiResponse findAll(Pageable pageable);
     ApiResponse findById(String id);
