ac2e474ad04cadc50193db340af9a7226f3b9f8c
Takhir <guvalakat1603@gmail.com>
2025-05-21 09:24:56 +0500

    ModuleUpdater

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleUpdater.java
@@ -0,0 +1,14 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModuleUpdater {
+    public String id;
+    public String title;
+    public String theme;
+}
