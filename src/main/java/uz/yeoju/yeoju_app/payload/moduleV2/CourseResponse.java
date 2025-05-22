7053c5e7d19f8069b3ec990f03a41072cd9b413b
Takhir <guvalakat1603@gmail.com>
2025-05-22 11:45:41 +0500

    field is added

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CourseResponse.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CourseResponse.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CourseResponse {
     public String id;
     public String title;
+    private List<ModuleProgressResponse> modules;
+
+    public CourseResponse(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
 }
