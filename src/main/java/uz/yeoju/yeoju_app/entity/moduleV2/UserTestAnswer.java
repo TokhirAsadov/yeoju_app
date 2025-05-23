d6427d07d1170f81f007132f67ce79b3cb5aac84
Takhir <guvalakat1603@gmail.com>
2025-05-23 09:31:17 +0500

    UserTestAnswer

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/UserTestAnswer.java
@@ -0,0 +1,34 @@
+package uz.yeoju.yeoju_app.entity.moduleV2;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserTestAnswer extends AbsEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    @JsonIgnore
+    private TestQuestion question;
+
+    private String selectedOption; // foydalanuvchi tanlagan variant: "A", "B", "C", "D"
+
+    private boolean isCorrect;
+}
+
