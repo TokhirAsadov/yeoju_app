24078909efb40d8a8fb849a1c21525eed6abef5b
Takhir <guvalakat1603@gmail.com>
2025-05-23 09:31:29 +0500

    UserTestAnswerCreator

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/UserTestAnswerCreator.java
@@ -0,0 +1,18 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserTestAnswerCreator {
+    public String userId;
+    public String testQuestionId;
+    public String selectedOption;
+}
+
