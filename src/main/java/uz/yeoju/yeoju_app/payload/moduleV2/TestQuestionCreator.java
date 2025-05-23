17c6a456f33dfbc75d9e14fe64c30c6629c3e9d4
Takhir <guvalakat1603@gmail.com>
2025-05-23 09:30:57 +0500

    TestQuestionCreator

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/TestQuestionCreator.java
@@ -0,0 +1,17 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestQuestionCreator {
+    public String courseTestId;
+    public String questionText;
+    public List<String> options;
+    public String correctAnswerText;
+}
