f91ef9684f021577141672a6823fb65d00f527fd
Takhir <guvalakat1603@gmail.com>
2025-05-24 10:29:55 +0500

    TestQuestionService

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testQuestion/TestQuestionService.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.testQuestion;
+
+import org.springframework.data.domain.Pageable;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.moduleV2.TestQuestionCreator;
+
+public interface TestQuestionService {
+
+}
