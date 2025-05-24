6b8269ba7dc09befa31cd35bd3cc93caed3cdafd
Takhir <guvalakat1603@gmail.com>
2025-05-24 10:37:17 +0500

    fixing

--- src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/CourseTest.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/CourseTest.java
@@ -1,12 +1,20 @@
 package uz.yeoju.yeoju_app.entity.moduleV2;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+import org.hibernate.annotations.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 
 import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +22,26 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CourseTest extends AbsEntity {
+@EntityListeners(AuditingEntityListener.class)
+public class CourseTest  {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private String id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    @NotNull
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
 
     private String title;
 
