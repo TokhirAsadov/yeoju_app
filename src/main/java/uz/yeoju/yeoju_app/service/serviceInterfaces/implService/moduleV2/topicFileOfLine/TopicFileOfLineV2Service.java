ad16d5bdd4af99cc83310be49b546a2e0f491bfe
Takhir <guvalakat1603@gmail.com>
2025-05-25 09:49:40 +0500

    deleteFileFromSystem()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/topicFileOfLine/TopicFileOfLineV2Service.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/topicFileOfLine/TopicFileOfLineV2Service.java
@@ -7,7 +7,8 @@ import uz.yeoju.yeoju_app.payload.ApiResponse;
 import java.io.IOException;
 
 public interface TopicFileOfLineV2Service {
-    ApiResponse saveFileToSystem(MultipartHttpServletRequest request, String lessonModuleId,TopicFileType type, String fileName, String fileUrl);
+    ApiResponse saveFileToSystem(MultipartHttpServletRequest request, String moduleId,TopicFileType type, String fileName, String fileUrl);
     byte[] downloadImageFromFileSystem(String subject,String fileName) throws IOException;
     ApiResponse findByName(String name);
+    ApiResponse deleteFileFromSystem(String fileName, String subjectName);
 }
