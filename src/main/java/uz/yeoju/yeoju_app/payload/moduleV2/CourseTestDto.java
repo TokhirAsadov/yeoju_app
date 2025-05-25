2007c2ea38cd5a8557f6defffdcc07c5dce6ec80
Takhir <guvalakat1603@gmail.com>
2025-05-25 09:45:54 +0500

    CourseTestDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CourseTestDto.java
@@ -0,0 +1,16 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseTestDto {
+    private String id;
+    private String title;
+    private String createdAt;
+    private String updatedAt;
+    private int question_count;
+}
