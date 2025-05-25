62a749ef48c41db7eac9b158f5a8f30dd3b83374
Takhir <guvalakat1603@gmail.com>
2025-05-25 09:46:12 +0500

    ModuleDto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleDto.java
@@ -0,0 +1,14 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModuleDto {
+    private String module_id;
+    private String module_title;
+    private String module_theme;
+}
