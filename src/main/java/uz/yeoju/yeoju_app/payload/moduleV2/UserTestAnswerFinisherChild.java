7fac43baff0cbaba32b6615ac522c48425bd6907
Takhir <guvalakat1603@gmail.com>
2025-05-25 12:08:41 +0500

    UserTestAnswerFinisherChild

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/UserTestAnswerFinisherChild.java
@@ -0,0 +1,15 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserTestAnswerFinisherChild {
+    public String testQuestionId;
+    public String selectedOption;
+}
+
