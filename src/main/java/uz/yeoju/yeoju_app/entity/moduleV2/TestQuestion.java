b3e953b5532df1ef9a126d6bd5f9ce832dda599c
Takhir <guvalakat1603@gmail.com>
2025-05-27 10:25:05 +0500

    Changing to V2

--- src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/TestQuestion.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/TestQuestion.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -15,9 +16,13 @@ import java.util.List;
 @AllArgsConstructor
 public class TestQuestion extends AbsEntity {
 
+    @Enumerated(EnumType.STRING)
+    private TestType type;
+
+
     @ManyToOne
     @JoinColumn(name = "test_id")
-    private CourseTest test;
+    private Test test;
 
     private String questionText;
 
@@ -26,8 +31,18 @@ public class TestQuestion extends AbsEntity {
             name = "TestQuestion_options",
             joinColumns = @JoinColumn(name = "TestQuestion_id")
     )
-    private List<String> options; // ["O'zbekiston", "Qozog'iston", "Tojikiston", "Turkmaniston"]
+    private List<String> options; // variantlar (faqat SINGLE/MULTIPLE uchun kerak)
 
+    @ElementCollection
+    @CollectionTable(
+            name = "TestQuestion_correct_answers",
+            joinColumns = @JoinColumn(name = "TestQuestion_id")
+    )
     @JsonIgnore
-    private String correctAnswerText; // Masalan: "O'zbekiston"
+    private List<String> correctAnswers; // MULTIPLE uchun ko‘p to‘g‘ri javob, SINGLE uchun 1 dona, WRITTEN uchun bo‘sh qolishi mumkin
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<UserTestAnswer> answers = new ArrayList<>();
+
 }
