794666ce22b4373a1a61425595faa7fe3e06f884
Takhir <guvalakat1603@gmail.com>
2025-05-27 10:27:01 +0500

    Changing to V2

--- src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/UserTestAnswer.java
+++ src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/UserTestAnswer.java
@@ -7,9 +7,8 @@ import lombok.NoArgsConstructor;
 import uz.yeoju.yeoju_app.entity.User;
 import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
@@ -27,7 +26,16 @@ public class UserTestAnswer extends AbsEntity {
     @JsonIgnore
     private TestQuestion question;
 
-    private String selectedOption; // foydalanuvchi tanlagan variant: "A", "B", "C", "D"
+//    private String selectedOption; // foydalanuvchi tanlagan variant: "A", "B", "C", "D"
+
+    @ElementCollection
+    @CollectionTable(
+            name = "UserTestAnswer_selected_options",
+            joinColumns = @JoinColumn(name = "UserTestAnswer_id")
+    )
+    private List<String> selectedOptions; // MULTIPLE uchun ["A", "C"]
+    private String writtenAnswer; // WRITTEN uchun
+
 
     private boolean isCorrect;
 }
