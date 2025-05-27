41780b7887d2dfd7272dff2e2ae6184da075804f
Takhir <guvalakat1603@gmail.com>
2025-05-27 10:25:40 +0500

    Changing to V2

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/UserTestAnswerFinisherChild.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/UserTestAnswerFinisherChild.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserTestAnswerFinisherChild {
     public String testQuestionId;
-    public String selectedOption;
+    public List<String> selectedOptions;
 }
 
