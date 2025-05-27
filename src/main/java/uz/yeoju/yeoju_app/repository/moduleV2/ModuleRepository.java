176ea089e015dd568a51b26f65de48f24b6deddd
Takhir <guvalakat1603@gmail.com>
2025-05-27 10:27:47 +0500

    existsByIdAndModuleTestIdIsNotNull()

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/ModuleRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/ModuleRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.moduleV2.Module;
 
 public interface ModuleRepository extends JpaRepository<Module,String> {
+    boolean existsByIdAndModuleTestIdIsNotNull(String moduleId);
 }
