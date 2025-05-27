410bc3ca31df91f96f1f3ac2a19e7a5f949ffdc9
Takhir <guvalakat1603@gmail.com>
2025-05-27 10:24:44 +0500

    Changing to V2

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleDto.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,4 +13,6 @@ public class ModuleDto {
     private String module_id;
     private String module_title;
     private String module_theme;
+    private List<TopicFileOfLineV2Dto> topic_files;
+    private CourseTestDto module_test;
 }
