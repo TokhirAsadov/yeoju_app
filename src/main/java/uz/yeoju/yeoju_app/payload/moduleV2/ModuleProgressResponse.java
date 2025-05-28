33525e894273dde8bf147cdc48d16d85c01598a2
Takhir <guvalakat1603@gmail.com>
2025-05-28 11:57:28 +0500

    changing

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleProgressResponse.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleProgressResponse.java
@@ -1,11 +1,12 @@
 package uz.yeoju.yeoju_app.payload.moduleV2;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -16,4 +17,20 @@ public class ModuleProgressResponse {
     private boolean completed;
     private String done;
     private String theme;
+    @JsonProperty("topic_files")
+    private List<TopicFileOfLineV2Dto> topicFiles;
+
+    @JsonProperty("module_test")
+    private ModuleTestDto moduleTest;
+
+    @JsonProperty("test_result_info")
+    private Map<String, Object> testResultInfo; // ✅ test natijalari uchun
+
+    public ModuleProgressResponse(String moduleId, String title, boolean completed, String done, String theme) {
+        this.moduleId = moduleId;
+        this.title = title;
+        this.completed = completed;
+        this.done = done;
+        this.theme = theme;
+    }
 }
