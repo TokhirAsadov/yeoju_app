eace36477696d57ca59e2fbf606f65457f4f865d
Takhir <guvalakat1603@gmail.com>
2025-05-29 09:46:53 +0500

    field is added

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/TestQuestionResponseV2.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/TestQuestionResponseV2.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.moduleV2.TestType;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 public class TestQuestionResponseV2 {
     public String id;
+    public TestType type;
     @JsonIgnore
     public String courseTestId;
     public String questionText;
