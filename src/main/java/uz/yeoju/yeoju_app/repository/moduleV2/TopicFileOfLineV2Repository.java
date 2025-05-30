13628f531df1c89ba3ba19336129957f63c59fdc
Takhir <guvalakat1603@gmail.com>
2025-05-30 10:59:21 +0500

    findAllByModulesId

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/TopicFileOfLineV2Repository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/TopicFileOfLineV2Repository.java
@@ -2,9 +2,11 @@ package uz.yeoju.yeoju_app.repository.moduleV2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import uz.yeoju.yeoju_app.entity.moduleV2.Module;
 import uz.yeoju.yeoju_app.entity.moduleV2.TopicFileOfLineV2;
 import uz.yeoju.yeoju_app.payload.resDto.module.GetFileOfLine;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -13,4 +15,6 @@ public interface TopicFileOfLineV2Repository extends JpaRepository<TopicFileOfLi
 
     @Query(value = "select t.id,t.name,t.type,t.fileType,t.url from TopicFileOfLineV2 t join TopicFileOfLineV2_LessonModule t_l on t.id = t_l.TopicFileOfLineV2_id where t_l.lessons_id=?1 order by t.createdAt",nativeQuery = true)
     Set<GetFileOfLine> getFilesByLessonModuleId(String lessonModuleId);
+
+    List<TopicFileOfLineV2> findAllByModulesId(String modules_id);
 }
