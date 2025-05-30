05d9997e5477a5179029b79c949e158f455aa90e
Takhir <guvalakat1603@gmail.com>
2025-05-30 10:58:32 +0500

    createV2()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testQuestion/TestQuestionService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testQuestion/TestQuestionService.java
@@ -3,9 +3,12 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.testQu
 import org.springframework.data.domain.Pageable;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.moduleV2.TestQuestionCreator;
+import uz.yeoju.yeoju_app.payload.moduleV2.TestQuestionCreatorV2;
 
 public interface TestQuestionService {
+    @Deprecated
     ApiResponse create(TestQuestionCreator creator);
+    ApiResponse createV2(TestQuestionCreatorV2 creator);
     ApiResponse findAll(Pageable pageable);
     ApiResponse findById(String testQuestionId);
     boolean deleteById(String testQuestionId);
