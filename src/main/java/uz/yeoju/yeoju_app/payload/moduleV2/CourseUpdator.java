de3e800279d9862a4358b7c0e5d8d5c1db61a330
Takhir <guvalakat1603@gmail.com>
2025-05-31 12:15:55 +0500

    CourseUpdator

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CourseUpdator.java
@@ -0,0 +1,16 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseUpdator {
+    public String id;
+    public String title;
+    public Set<String> facultiesIds;
+}
