e3393c297d0951887e22a722a11f4783d77032c1
Takhir <guvalakat1603@gmail.com>
2025-05-31 12:17:09 +0500

    GetTeacherLessonInModule

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/GetTeacherLessonInModule.java
@@ -0,0 +1,11 @@
+package uz.yeoju.yeoju_app.payload.moduleV2;
+
+public interface GetTeacherLessonInModule {
+    String getTeacherId();
+    String getTeacherFullName();
+    String getLessonId();
+    String getLessonName();
+    String getEducationType();
+    String getEducationLanguage();
+    Integer getLevel();
+}
