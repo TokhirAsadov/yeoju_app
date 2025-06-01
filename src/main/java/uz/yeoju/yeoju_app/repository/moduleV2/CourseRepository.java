a2ad0002c3da57b7d6d95f4a7c8d017de0fdf57a
Takhir <guvalakat1603@gmail.com>
2025-06-01 19:11:16 +0500

    getCourseByStudentIdAndEducationYearId

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/CourseRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/CourseRepository.java
@@ -1,13 +1,22 @@
 package uz.yeoju.yeoju_app.repository.moduleV2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.yeoju.yeoju_app.entity.moduleV2.Course;
 
 import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course,String> {
      List<Course> findAllByPlanId(String planId);
-     List<Course> findAllByPlanEducationYearIdAndPlanGroupsId(String educationYearId, String groupId);
+     @Query(value = "SELECT c.* FROM Course c\n" +
+             "join PlanOfSubjectV2 p on c.plan_id = p.id\n" +
+             "JOIN Course_Faculty cf ON c.id = cf.course_id\n" +
+             "join Faculty f on cf.faculties_id = f.id\n" +
+             "join groups g on f.id = g.faculty_id\n" +
+             "join Student s on g.id = s.group_id\n" +
+             "WHERE s.user_id=?1 and p.educationYear_id=?2",nativeQuery = true)
+     List<Course> getCourseByStudentIdAndEducationYearId(String studentId, String educationYearId);
+//     List<Course> findAllByPlanEducationYearIdAndPlanGroupsId(String educationYearId, String groupId);
      boolean existsByIdAndFinalTestIdIsNotNull(String id);
      Course findByFinalTestId(String id);
 }
