6704d65405275b078d8e6212193e08f32c574c21
Takhir <guvalakat1603@gmail.com>
2025-06-01 18:29:33 +0500

    Min Max

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CTestCreator.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CTestCreator.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,5 +14,7 @@ public class CTestCreator {
     public String title;
     public String courseId;
     public String moduleId;
+    @Min(value = 50, message = "Minimum 50% bo'lishi kerak")
+    @Max(value = 100, message = "Minimum 100% bo'lishi kerak")
     public Double passingPercentage;
 }
