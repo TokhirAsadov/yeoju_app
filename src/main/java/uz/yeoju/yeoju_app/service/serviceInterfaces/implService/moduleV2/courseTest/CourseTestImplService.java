109e5ea1fa535ed1ae3244c1fac6e9ad97903812
Takhir <guvalakat1603@gmail.com>
2025-06-01 19:11:37 +0500

    done

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/courseTest/CourseTestImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/courseTest/CourseTestImplService.java
@@ -3,11 +3,14 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.course
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import uz.yeoju.yeoju_app.entity.moduleV2.CourseTest;
+import uz.yeoju.yeoju_app.entity.moduleV2.Course;
+import uz.yeoju.yeoju_app.entity.moduleV2.Module;
+import uz.yeoju.yeoju_app.entity.moduleV2.Test;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.moduleV2.CTestCreator;
 import uz.yeoju.yeoju_app.repository.moduleV2.CourseRepository;
 import uz.yeoju.yeoju_app.repository.moduleV2.CourseTestRepository;
+import uz.yeoju.yeoju_app.repository.moduleV2.ModuleRepository;
 
 import java.util.Optional;
 
@@ -15,25 +18,55 @@ import java.util.Optional;
 public class CourseTestImplService implements CourseTestService{
     private final CourseTestRepository courseTestRepository;
     private final CourseRepository courseRepository;
+    private final ModuleRepository moduleRepository;
 
-    public CourseTestImplService(CourseTestRepository courseTestRepository, CourseRepository courseRepository) {
+    public CourseTestImplService(CourseTestRepository courseTestRepository, CourseRepository courseRepository, ModuleRepository moduleRepository) {
         this.courseTestRepository = courseTestRepository;
         this.courseRepository = courseRepository;
+        this.moduleRepository = moduleRepository;
     }
 
     @Override
+    @Transactional
     public ApiResponse create(CTestCreator creator) {
-        if (!courseRepository.existsById(creator.courseId)) {
-            return new ApiResponse(false,"Course not found");
+        if (creator.courseId != null ) {
+            if (!courseRepository.existsById(creator.courseId)) {
+                return new ApiResponse(false,"Course not found");
+            }
+            if (courseRepository.existsByIdAndFinalTestIdIsNotNull(creator.courseId)) {
+                return new ApiResponse(false,"Test already exists for this course");
+            }
+            Test test = new Test();
+            test.setTitle(creator.title);
+            if (creator.passingPercentage != null) {
+                test.setPassingPercentage(creator.passingPercentage);
+            }
+            Test save = courseTestRepository.save(test);
+            courseRepository.findById(creator.courseId).ifPresent(i -> {
+                i.setFinalTest(save);
+            });
+            return new ApiResponse(true,"Test created successfully",save.getId());
         }
-        if (courseTestRepository.existsByCourseId(creator.courseId)) {
-            return new ApiResponse(false,"Test already exists for this course");
+        if (creator.moduleId != null) {
+            if (!moduleRepository.existsById(creator.moduleId)) {
+                return new ApiResponse(false,"Module not found");
+            }
+            if (moduleRepository.existsByIdAndModuleTestIdIsNotNull(creator.moduleId)) {
+                return new ApiResponse(false,"Test already exists for this module");
+            }
+            Test test = new Test();
+            test.setTitle(creator.title);
+            if (creator.passingPercentage != null) {
+                test.setPassingPercentage(creator.passingPercentage);
+            }
+            Test save = courseTestRepository.save(test);
+            moduleRepository.findById(creator.moduleId).ifPresent(i -> {
+                i.setModuleTest(save);
+            });
+            return new ApiResponse(true,"Test created successfully",save.getId());
         }
-        CourseTest courseTest = new CourseTest();
-        courseTest.setTitle(creator.title);
-        courseTest.setCourse(courseRepository.getById(creator.courseId));
-        CourseTest save = courseTestRepository.save(courseTest);
-        return new ApiResponse(true,"Test created successfully",save.getId());
+        return new ApiResponse(false,"Course or module id is required to create a test");
+
     }
 
     @Override
@@ -43,16 +76,28 @@ public class CourseTestImplService implements CourseTestService{
 
     @Override
     public ApiResponse findById(String courseTestId) {
-        CourseTest courseTest = courseTestRepository.findById(courseTestId)
+        Test test = courseTestRepository.findById(courseTestId)
                 .orElseThrow(() -> new RuntimeException("Course test not found by id="+courseTestId));
-        return new ApiResponse(true,"Course test by id="+courseTestId,courseTest);
+        return new ApiResponse(true,"Course test by id="+courseTestId, test);
     }
 
     @Override
     @Transactional
     public boolean delete(String id) {
-        Optional<CourseTest> optional = courseTestRepository.findById(id);
+        Optional<Test> optional = courseTestRepository.findById(id);
         if (optional.isPresent()) {
+            Course course = courseRepository.findByFinalTestId(id);
+            if (course != null) {
+                course.setFinalTest(null);
+                courseRepository.save(course);
+            }
+
+            Module module = moduleRepository.findByModuleTestId(id);
+            if (module != null) {
+                module.setModuleTest(null);
+                moduleRepository.save(module);
+            }
+
             courseTestRepository.deleteById(id);
             return true;
         } else {
