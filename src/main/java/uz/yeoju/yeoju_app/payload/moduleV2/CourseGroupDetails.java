fc2920ac85243d21d3013da9af42981237b4a8a8
Takhir <guvalakat1603@gmail.com>
2025-06-02 10:05:10 +0500

    new fields

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CourseGroupDetails.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CourseGroupDetails.java
@@ -2,12 +2,10 @@ package uz.yeoju.yeoju_app.payload.moduleV2;
 
 import lombok.Data;
 
-import java.util.List;
 
 @Data
 public class CourseGroupDetails {
     private CourseDto course;
-    private List<GroupPlanV2DTO> groups;
-//    @JsonIgnore
-//    private Course courseObject; // bu keyin parse qilinadi
+    private JsonObject educationType;
+    private JsonObject educationLanguage;
 }
