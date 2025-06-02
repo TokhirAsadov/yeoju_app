10b46662c8af4af19de4d434324210c80671da61
Takhir <guvalakat1603@gmail.com>
2025-06-02 10:04:58 +0500

    new field

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CourseCreator.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/CourseCreator.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CourseCreator {
     public String title;
     public String planId;
+    public Set<String> facultiesIds;
 }
