3ee02b17beb408832794a07c474ecc1504cb2861
Takhir <guvalakat1603@gmail.com>
2025-06-03 11:45:57 +0500

    @Valid

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/courseTest/CourseTestService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/courseTest/CourseTestService.java
@@ -4,8 +4,10 @@ import org.springframework.data.domain.Pageable;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.moduleV2.CTestCreator;
 
+import javax.validation.Valid;
+
 public interface CourseTestService {
-    ApiResponse create(CTestCreator creator);
+    ApiResponse create(@Valid CTestCreator creator);
     ApiResponse findAll(Pageable pageable);
     ApiResponse findById(String courseTestId);
     boolean delete(String id);
