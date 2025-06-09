39b6d84036376f32c1870f325b032b868e022feb
Takhir <guvalakat1603@gmail.com>
2025-06-09 10:57:23 +0500

    finishCourseTest()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/userTestAnswer/UserTestAnswerImplService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/userTestAnswer/UserTestAnswerImplService.java
@@ -36,5 +36,53 @@ public class UserTestAnswerImplService implements UserTestAnswerService{
         this.testOptionRepository = testOptionRepository;
     }
 
+    @Override
+    public ApiResponse finishCourseTest(UserTestAnswerFinisher finisher) {
+        if (!userRepository.existsById(finisher.userId)) {
+            return new ApiResponse(false, "User not found by id = " + finisher.userId);
+        }
+
+        User user = userRepository.getById(finisher.userId);
+
+        for (UserTestAnswerFinisherChild answer : finisher.answers) {
+            if (!testQuestionRepository.existsById(answer.testQuestionId)) {
+                throw new RuntimeException("Test question not found by id = " + answer.testQuestionId);
+            }
+
+            if (userTestAnswerRepository.existsByUserIdAndQuestionId(finisher.userId, answer.testQuestionId)) {
+                throw new RuntimeException("Bunday javob allaqachon mavjud!");
+            }
+
+            TestQuestion question = testQuestionRepository.getById(answer.testQuestionId);
+
+            // Tanlangan variantlar (ID lar orqali TestOption obyektlari)
+            List<TestOption> selectedOptions = testOptionRepository.findAllById(answer.getSelectedOptionsIds());
+
+            // To'g'ri javoblarni topish
+            List<String> correctOptionIds = question.getOptions().stream()
+                    .filter(TestOption::getCorrect) // correct == true
+                    .map(option -> option.getId().trim())
+                    .collect(Collectors.toList());
+
+            // Foydalanuvchi tanlagan variantlar
+            List<String> selectedOptionIds = selectedOptions.stream()
+                    .map(option -> option.getId().trim())
+                    .collect(Collectors.toList());
+
+            // To'g'rilikni tekshirish (Set sifatida solishtirish)
+            boolean isCorrect = new HashSet<>(correctOptionIds).equals(new HashSet<>(selectedOptionIds));
+
+            // Javobni saqlash
+            UserTestAnswer userTestAnswer = new UserTestAnswer();
+            userTestAnswer.setUser(user);
+            userTestAnswer.setQuestion(question);
+            userTestAnswer.setSelectedOptions(selectedOptions);
+            userTestAnswer.setCorrect(isCorrect);
+
+            userTestAnswerRepository.save(userTestAnswer);
+        }
+
+        return new ApiResponse(true, "User test answers saved successfully");
+    }
 
 }
