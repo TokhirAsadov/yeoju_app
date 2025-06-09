802d5acb5ad629f97f470a5439e89399c0edf6bc
Takhir <guvalakat1603@gmail.com>
2025-06-09 09:41:18 +0500

    UserTestAnswerService

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/userTestAnswer/UserTestAnswerService.java
@@ -0,0 +1,11 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.userTestAnswer;
+
+import org.springframework.data.domain.Pageable;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.moduleV2.TestResultDto;
+import uz.yeoju.yeoju_app.payload.moduleV2.UserTestAnswerCreator;
+import uz.yeoju.yeoju_app.payload.moduleV2.UserTestAnswerFinisher;
+
+public interface UserTestAnswerService {
+
+}
