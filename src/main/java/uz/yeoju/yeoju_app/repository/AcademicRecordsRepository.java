cdfb0765576bc09b93f26646d13954ef74568b0a
Takhir <guvalakat1603@gmail.com>
2025-06-20 20:37:03 +0500

    findAllByQaydRaqami()

--- src/main/java/uz/yeoju/yeoju_app/repository/AcademicRecordsRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/AcademicRecordsRepository.java
@@ -3,6 +3,8 @@ package uz.yeoju.yeoju_app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.dekanat.AcademicRecords;
 
+import java.util.List;
+
 public interface AcademicRecordsRepository extends JpaRepository<AcademicRecords, Long> {
-    // Custom query methods can be defined here if needed
+    List<AcademicRecords> findAllByQaydRaqami(String qaydRaqami);
 }
