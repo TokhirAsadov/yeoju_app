bc550ed4de861deb202ee5d02b96f7c64c43aab9
Takhir <guvalakat1603@gmail.com>
2025-06-20 20:33:16 +0500

    saveRecords()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/academicRecords/AcademicRecordsService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/dekanat/academicRecords/AcademicRecordsService.java
@@ -1,4 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.dekanat.academicRecords;
 
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
 public interface AcademicRecordsService {
+    ApiResponse saveRecords(MultipartHttpServletRequest request);
 }
