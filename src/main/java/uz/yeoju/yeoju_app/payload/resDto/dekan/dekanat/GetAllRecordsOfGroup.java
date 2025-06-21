7af874361c0713b15ce0702a25ef3764b896d452
Takhir <guvalakat1603@gmail.com>
2025-06-21 12:09:29 +0500

    GetAllRecordsOfGroup

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/dekan/dekanat/GetAllRecordsOfGroup.java
@@ -0,0 +1,14 @@
+package uz.yeoju.yeoju_app.payload.resDto.dekan.dekanat;
+
+import org.springframework.beans.factory.annotation.Value;
+import uz.yeoju.yeoju_app.entity.dekanat.AcademicRecords;
+
+import java.util.List;
+
+public interface GetAllRecordsOfGroup {
+    String getUserId();
+    String getLogin();
+
+    @Value("#{academicRecordsRepository.findAllByQaydRaqami(target.login)}")
+    List<AcademicRecords> getRecords();
+}
