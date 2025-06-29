186ffc5c6811c6ea0e88ebdaa145bc1ae32e8b58
Takhir <guvalakat1603@gmail.com>
2025-06-29 14:06:57 +0500

    create()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testV2/TestV2Service.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testV2/TestV2Service.java
@@ -1,4 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.testV2.testV2;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.moduleV2.testV2.TestV2Creator;
+
 public interface TestV2Service {
+    ApiResponse create(TestV2Creator creator);
 }
