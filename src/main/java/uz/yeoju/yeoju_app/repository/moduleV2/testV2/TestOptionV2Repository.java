a5e1c8be66a96441998a4f0927828aa7f767d1c5
Takhir <guvalakat1603@gmail.com>
2025-06-29 13:31:52 +0500

    TestOptionV2Repository

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/TestOptionV2Repository.java
@@ -0,0 +1,7 @@
+package uz.yeoju.yeoju_app.repository.moduleV2.testV2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.moduleV2.testV2.TestOptionV2;
+
+public interface TestOptionV2Repository extends JpaRepository<TestOptionV2,String> {
+}
