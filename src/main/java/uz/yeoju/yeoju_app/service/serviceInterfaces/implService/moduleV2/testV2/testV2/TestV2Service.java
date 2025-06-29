893b64216069ca89260b766bad991fa01ce27619
Takhir <guvalakat1603@gmail.com>
2025-06-29 16:44:27 +0500

    findByStudentIdAndEducationYearIdV2()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testV2/TestV2Service.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testV2/TestV2Service.java
@@ -3,6 +3,9 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.testV2
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.moduleV2.testV2.TestV2Creator;
 
+import javax.validation.Valid;
+
 public interface TestV2Service {
-    ApiResponse create(TestV2Creator creator);
+    ApiResponse create(@Valid TestV2Creator creator);
+    ApiResponse findByStudentIdAndEducationYearIdV2(String studentId, String educationYearId);
 }
