4907df7416f6b4db2ecc09d601e0ac3237915ff2
Takhir <guvalakat1603@gmail.com>
2025-06-29 14:11:44 +0500

    checkEnoughQuestions()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testQuestionV2/TestQuestionV2Service.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testQuestionV2/TestQuestionV2Service.java
@@ -1,8 +1,10 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.testV2.testQuestionV2;
 
+import uz.yeoju.yeoju_app.entity.moduleV2.TestType;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.moduleV2.testV2.TestQuestionV2Creator;
 
 public interface TestQuestionV2Service {
     ApiResponse createTestQuestionV2(TestQuestionV2Creator creator);
+    ApiResponse checkEnoughQuestions(String courseId, int count, TestType type);
 }
