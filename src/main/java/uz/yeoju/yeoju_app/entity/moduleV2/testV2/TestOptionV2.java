5d7f7fbc3f800ae389cab5440f4910c3147399d8
Takhir <guvalakat1603@gmail.com>
2025-06-29 13:30:15 +0500

    TestOptionV2

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/moduleV2/testV2/TestOptionV2.java
@@ -0,0 +1,27 @@
+package uz.yeoju.yeoju_app.entity.moduleV2.testV2;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.moduleV2.TestQuestion;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestOptionV2 extends AbsEntity {
+    private String text;    // Variant matni
+
+    @JsonIgnore
+    private Boolean correct; // Shu variant to‘g‘rimi yoki yo‘q
+
+    @ManyToOne
+    @JoinColumn(name = "question_v2_id")
+    private TestQuestionV2 questionV2;
+}
