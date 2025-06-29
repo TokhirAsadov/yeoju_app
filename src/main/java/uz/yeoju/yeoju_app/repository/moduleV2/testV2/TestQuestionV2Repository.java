552420943fd5f453f39df6682cf389fc4dabdf1e
Takhir <guvalakat1603@gmail.com>
2025-06-29 14:11:35 +0500

    countByCourseIdAndType()

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/TestQuestionV2Repository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/TestQuestionV2Repository.java
@@ -1,8 +1,10 @@
 package uz.yeoju.yeoju_app.repository.moduleV2.testV2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.moduleV2.TestType;
 import uz.yeoju.yeoju_app.entity.moduleV2.testV2.TestQuestionV2;
 
 public interface TestQuestionV2Repository extends JpaRepository<TestQuestionV2,String> {
 
+    long countByCourseIdAndType(String courseId, TestType type);
 }
