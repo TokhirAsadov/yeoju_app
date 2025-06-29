e36f417bef636dfd9d2d7b6ff9b581befd5a7027
Takhir <guvalakat1603@gmail.com>
2025-06-29 14:07:14 +0500

    existsByCourseId()

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/TestV2Repository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/TestV2Repository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.yeoju.yeoju_app.entity.moduleV2.testV2.TestV2;
 
 public interface TestV2Repository extends JpaRepository<TestV2,String> {
+    boolean existsByCourseId(String courseId);
 }
