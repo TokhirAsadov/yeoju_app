fe512dc333b46bda0445c4576c53db9d8ea62f64
Takhir <guvalakat1603@gmail.com>
2025-06-29 13:51:27 +0500

    createTestQuestionV2()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testQuestionV2/TestQuestionV2Service.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testQuestionV2/TestQuestionV2Service.java
@@ -1,4 +1,8 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.testV2.testQuestionV2;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.moduleV2.testV2.TestQuestionV2Creator;
+
 public interface TestQuestionV2Service {
+    ApiResponse createTestQuestionV2(TestQuestionV2Creator creator);
 }
