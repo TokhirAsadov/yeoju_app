93051dcbaa9c6f22233d81542c6a5f34e34d06ca
Takhir <guvalakat1603@gmail.com>
2025-06-30 09:59:16 +0500

    field

--- src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleDto.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/ModuleDto.java
@@ -3,6 +3,7 @@ package uz.yeoju.yeoju_app.payload.moduleV2;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.payload.moduleV2.testV2.QuestionsV2Dto;
 
 import java.util.List;
 
@@ -15,4 +16,5 @@ public class ModuleDto {
     private String module_theme;
     private List<TopicFileOfLineV2Dto> topic_files;
     private CourseTestDto module_test;
+    private List<QuestionsV2Dto> questions;
 }
