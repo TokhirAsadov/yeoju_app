4f3b632177333c9de3a3d2f6cd011359ce6901fc
Takhir <guvalakat1603@gmail.com>
2025-06-30 09:58:27 +0500

    TestOptionV2Dto

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/moduleV2/testV2/TestOptionV2Dto.java
@@ -0,0 +1,10 @@
+package uz.yeoju.yeoju_app.payload.moduleV2.testV2;
+
+import lombok.Data;
+
+@Data
+public class TestOptionV2Dto {
+    public String id;
+    public String text; // Variant matni
+    public Boolean correct;
+}
