4fb9cf7bd26e7fa4b97c0e7501ec6c7ed6851e3a
Takhir <guvalakat1603@gmail.com>
2025-07-31 19:17:41 +0500

    calculateUserScore()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/userTestAnswerV2/UserTestAnswerV2Service.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/userTestAnswerV2/UserTestAnswerV2Service.java
@@ -1,4 +1,7 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.testV2.userTestAnswerV2;
 
+import uz.yeoju.yeoju_app.payload.moduleV2.TestResultDto;
+
 public interface UserTestAnswerV2Service {
+    TestResultDto calculateUserScore(String studentUserId, String testId);
 }
