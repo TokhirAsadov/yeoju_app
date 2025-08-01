5ed7b42bd4516d0f24694faf742bdea156bf22f7
Takhir <guvalakat1603@gmail.com>
2025-08-01 09:49:18 +0500

    findByGroupIdAndEducationYearIdV2()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testV2/TestV2Service.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testV2/TestV2Service.java
@@ -1,11 +1,15 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.testV2.testV2;
 
+import org.springframework.data.domain.Pageable;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.moduleV2.testV2.TestV2Creator;
+import uz.yeoju.yeoju_app.payload.moduleV2.testV2.TestV2Updater;
 
 import javax.validation.Valid;
 
 public interface TestV2Service {
     ApiResponse create(@Valid TestV2Creator creator);
     ApiResponse findByStudentIdAndEducationYearIdV2(String studentId, String educationYearId);
+    ApiResponse findByGroupIdAndEducationYearIdV2(String groupId, String educationYearId);
+
 }
