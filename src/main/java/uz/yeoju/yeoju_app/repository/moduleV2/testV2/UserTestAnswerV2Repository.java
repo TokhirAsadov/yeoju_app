f07a421dc47f1dc9c6c5c88109c799007949e43f
Takhir <guvalakat1603@gmail.com>
2025-08-01 09:52:49 +0500

    clear

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/UserTestAnswerV2Repository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/UserTestAnswerV2Repository.java
@@ -1,14 +1,15 @@
 package uz.yeoju.yeoju_app.repository.moduleV2.testV2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import uz.yeoju.yeoju_app.entity.User;
-import uz.yeoju.yeoju_app.entity.moduleV2.testV2.TestQuestionV2;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uz.yeoju.yeoju_app.entity.moduleV2.testV2.UserTestAnswerV2;
 
-import java.util.Optional;
+import java.sql.Timestamp;
+import java.util.List;
 
 public interface UserTestAnswerV2Repository extends JpaRepository<UserTestAnswerV2,String> {
-    boolean existsByUserIdAndQuestionV2Id(String userId, String questionV2Id);
 
-    Optional<UserTestAnswerV2> findByUserAndQuestionV2(User user, TestQuestionV2 questionV2);
+
 }
