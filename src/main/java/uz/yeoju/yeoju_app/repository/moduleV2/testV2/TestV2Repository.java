5f1aa61ee9c2c835a97c33d41febca1f9185f4e7
Takhir <guvalakat1603@gmail.com>
2025-08-01 09:51:00 +0500

    findByModuleId()

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/TestV2Repository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/TestV2Repository.java
@@ -5,7 +5,6 @@ import uz.yeoju.yeoju_app.entity.moduleV2.testV2.TestV2;
 
 public interface TestV2Repository extends JpaRepository<TestV2,String> {
     boolean existsByCourseId(String courseId);
-
     TestV2 findByCourseId(String id);
-
+    TestV2 findByModuleId(String id);
 }
