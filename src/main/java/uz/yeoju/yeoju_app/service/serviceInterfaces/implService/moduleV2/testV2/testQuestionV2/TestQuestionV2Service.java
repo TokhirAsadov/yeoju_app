d012608b8bac16725417e8520f1609e8a02bc3d2
Takhir <guvalakat1603@gmail.com>
2025-08-10 22:14:35 +0500

    updateTestQuestionV2()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testQuestionV2/TestQuestionV2Service.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/moduleV2/testV2/testQuestionV2/TestQuestionV2Service.java
@@ -1,11 +1,15 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.moduleV2.testV2.testQuestionV2;
 
+import org.springframework.data.domain.Pageable;
 import uz.yeoju.yeoju_app.entity.moduleV2.TestType;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.moduleV2.GiveScoreToWrittenUserAnswerDto;
 import uz.yeoju.yeoju_app.payload.moduleV2.testV2.TestQuestionV2Creator;
 
 public interface TestQuestionV2Service {
     ApiResponse createTestQuestionV2(TestQuestionV2Creator creator);
     ApiResponse checkEnoughQuestions(String courseId, int count, TestType type);
     ApiResponse getCountOfTypeOfQuestionsByCourseId(String courseId);
+    ApiResponse updateTestQuestionV2(String questionId, TestQuestionV2Creator creator);
+
 }
