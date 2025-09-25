f810b1c740d8250c52137a9a3343b74c57962642
Takhir <guvalakat1603@gmail.com>
2025-09-25 10:19:11 +0500

    getKafedras()

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/GetAllKafedraTeachersStatistics.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/GetAllKafedraTeachersStatistics.java
@@ -1,5 +1,12 @@
 package uz.yeoju.yeoju_app.payload.resDto.timeTableDB;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+
 public interface GetAllKafedraTeachersStatistics {
     String getDayInWeek();
+
+    @Value("#{@kafedraTeachersStatisticsRepository.findStatisticsByDay(target.dayInWeek)}")
+    List<GetKafedraStatistics> getKafedras();
 }
