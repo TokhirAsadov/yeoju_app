b9be475e40af43f6302143b7a84b13e473986a6b
Takhir <guvalakat1603@gmail.com>
2025-09-29 10:53:47 +0500

    KafedraTeachersStatisticsService

---
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/dailyStatistics/KafedraTeachersStatisticsService.java
@@ -0,0 +1,12 @@
+package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.kafedra.dailyStatistics;
+
+import org.springframework.http.ResponseEntity;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.payload.resDto.timeTableDB.GetKafedraStatistics;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface KafedraTeachersStatisticsService {
+    
+}
