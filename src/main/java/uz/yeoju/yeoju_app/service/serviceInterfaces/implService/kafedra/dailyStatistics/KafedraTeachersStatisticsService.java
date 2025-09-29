e1f8cd2286df1476c90b82c7a3f0dcc4c4cad7dd
Takhir <guvalakat1603@gmail.com>
2025-09-29 10:56:18 +0500

    getByWeekRange()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/dailyStatistics/KafedraTeachersStatisticsService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/dailyStatistics/KafedraTeachersStatisticsService.java
@@ -4,13 +4,11 @@ import org.springframework.http.ResponseEntity;
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.resDto.timeTableDB.GetKafedraStatistics;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface KafedraTeachersStatisticsService {
     void scheduleForSaveDailyStatistics();
     void scheduleForSaveDailyStatisticsByDate(Integer year, Integer month, Integer day, Integer week, Integer weekday);
-    LocalDate[] getWeekRange(int year, int week);
     ApiResponse getAllKafedrasTeachersStatistics(int year, int week);
     ApiResponse getKafedrasTeachersStatistics(int year, int week, String kafedraId);
     ApiResponse getKafedrasTeachersStatisticsByDay(String day);
