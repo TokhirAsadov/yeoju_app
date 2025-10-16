7e643c34f26ffa8d5ffa1859caaa2e4ec19325b9
Takhir <guvalakat1603@gmail.com>
2025-10-16 12:00:38 +0500

    getRoomStatistics()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/admin/AdminService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/admin/AdminService.java
@@ -2,10 +2,15 @@ package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.admin;
 
 import uz.yeoju.yeoju_app.payload.ApiResponse;
 import uz.yeoju.yeoju_app.payload.admin.ForUserSaveDto;
+import uz.yeoju.yeoju_app.payload.resDto.admin.AdminRoomStatisticsForCheckRooms;
+
+import java.util.List;
 
 public interface AdminService {
     ApiResponse saveOrUpdateUser(ForUserSaveDto dto);
     ApiResponse getUserForUpdate(String param);
     ApiResponse getUserForUpdateById(String id);
     ApiResponse getInformationAboutCountOfUsers();
+
+    List<AdminRoomStatisticsForCheckRooms> getRoomStatistics(String rfid);
 }
