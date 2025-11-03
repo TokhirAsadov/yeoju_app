8badc3cb44e14019a4afee6bed495f619ffffec7
Takhir <guvalakat1603@gmail.com>
2025-11-03 10:07:53 +0500

    runDailyTeachersStatisticsJob

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/dailyStatistics/StatisticsScheduler.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/dailyStatistics/StatisticsScheduler.java
@@ -6,13 +6,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class StatisticsScheduler {
     private final KafedraTeachersStatisticsService kafedraTeachersStatisticsService;
+    private final DailyTeachersStatisticsService dailyTeachersStatisticsService;
 
-    public StatisticsScheduler(KafedraTeachersStatisticsService kafedraTeachersStatisticsService) {
+    public StatisticsScheduler(KafedraTeachersStatisticsService kafedraTeachersStatisticsService, DailyTeachersStatisticsService dailyTeachersStatisticsService) {
         this.kafedraTeachersStatisticsService = kafedraTeachersStatisticsService;
+        this.dailyTeachersStatisticsService = dailyTeachersStatisticsService;
     }
 
     @Scheduled(cron = "0 0 22 * * ?" , zone = "Asia/Tashkent")
     public void runDailyStatisticsJob() {
         kafedraTeachersStatisticsService.scheduleForSaveDailyStatistics();
+    }
+    @Scheduled(cron = "0 30 22 * * ?" , zone = "Asia/Tashkent")
+    public void runDailyTeachersStatisticsJob() {
+        dailyTeachersStatisticsService.scheduleForSaveDailyTeachersStatistics();
     }
 }
