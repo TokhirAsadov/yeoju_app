75ebf155b33a2aed64605166edc10bf9f91e3b51
Takhir <guvalakat1603@gmail.com>
2025-11-03 10:00:56 +0500

    scheduleForSaveDailyTeachersStatisticsByDate() method is created

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/dailyStatistics/DailyTeachersStatisticsService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/dailyStatistics/DailyTeachersStatisticsService.java
@@ -1,4 +1,5 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.kafedra.dailyStatistics;
 
 public interface DailyTeachersStatisticsService {
+    void scheduleForSaveDailyTeachersStatisticsByDate(Integer year, Integer month, Integer day, Integer week, Integer weekday);
 }
