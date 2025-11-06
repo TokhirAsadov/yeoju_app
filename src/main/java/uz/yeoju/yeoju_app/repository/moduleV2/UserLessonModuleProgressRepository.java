485a9b15eacd8058dee7401d6f6d5838faa58081
Takhir <guvalakat1603@gmail.com>
2025-11-06 09:52:46 +0500

    existsByUserAndModuleAndCompletedTrue()

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/UserLessonModuleProgressRepository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/UserLessonModuleProgressRepository.java
@@ -1,7 +1,14 @@
 package uz.yeoju.yeoju_app.repository.moduleV2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.moduleV2.Module;
 import uz.yeoju.yeoju_app.entity.moduleV2.UserLessonModuleProgress;
 
+import java.util.List;
+
 public interface UserLessonModuleProgressRepository extends JpaRepository<UserLessonModuleProgress,String> {
+    boolean existsByUserAndModuleAndCompletedTrue(User user, Module module);
+
+
 }
