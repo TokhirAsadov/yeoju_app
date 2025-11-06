341ed5891b7e88434e1ff5ac59b941bed0875cfd
Takhir <guvalakat1603@gmail.com>
2025-11-06 11:19:48 +0500

    Add method to retrieve daily teacher statistics by day

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/dailyStatistics/DailyTeachersStatisticsService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/dailyStatistics/DailyTeachersStatisticsService.java
@@ -1,6 +1,10 @@
 package uz.yeoju.yeoju_app.service.serviceInterfaces.implService.kafedra.dailyStatistics;
 
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+
 public interface DailyTeachersStatisticsService {
     void scheduleForSaveDailyTeachersStatistics();
     void scheduleForSaveDailyTeachersStatisticsByDate(Integer year, Integer month, Integer day, Integer week, Integer weekday);
+
+    ApiResponse getDailyTeacherStatisticsByDay(String teacherId, Integer year, Integer month, Integer day);
 }
