05a8a9f277dd907385b90ade36946702aa700c69
Takhir <guvalakat1603@gmail.com>
2025-11-13 14:02:53 +0500

    DailyTeacherMissedLessonRepository

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/timetableDB/DailyTeacherMissedLessonRepository.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.repository.timetableDB;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.timetableDB.DailyTeacherMissedLesson;
+
+
+public interface DailyTeacherMissedLessonRepository extends JpaRepository<DailyTeacherMissedLesson, String> {
+
+}
