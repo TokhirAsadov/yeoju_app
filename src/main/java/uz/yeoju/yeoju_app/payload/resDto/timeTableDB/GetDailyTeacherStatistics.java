e8a9d7d6121a844e50d12a4aa613bc724cf89cb4
Takhir <guvalakat1603@gmail.com>
2025-11-18 12:16:08 +0500

    new field is added

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/GetDailyTeacherStatistics.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/timeTableDB/GetDailyTeacherStatistics.java
@@ -1,5 +1,9 @@
 package uz.yeoju.yeoju_app.payload.resDto.timeTableDB;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+
 public interface GetDailyTeacherStatistics {
     String getTeacherId();
     String getId();
@@ -10,4 +14,7 @@ public interface GetDailyTeacherStatistics {
     Integer getWeekday();
     Integer getTotalAttended();
     Integer getTotalNotAttended();
+
+    @Value("#{@dailyTeacherMissedLessonRepository.getTeacherMissedLessonByDay(target.teacherId, target.year, target.month, target.day)}")
+    List<GetDailyTeacherMissedLesson> getMissedLessons();
 }
