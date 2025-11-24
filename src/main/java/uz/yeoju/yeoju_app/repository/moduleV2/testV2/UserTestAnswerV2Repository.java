60a58630dd4c4fc54e03ec50d8f3ae3e6e156d9c
Takhir <guvalakat1603@gmail.com>
2025-11-24 13:54:54 +0500

    methods

--- src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/UserTestAnswerV2Repository.java
+++ src/main/java/uz/yeoju/yeoju_app/repository/moduleV2/testV2/UserTestAnswerV2Repository.java
@@ -10,6 +10,14 @@ import java.sql.Timestamp;
 import java.util.List;
 
 public interface UserTestAnswerV2Repository extends JpaRepository<UserTestAnswerV2,String> {
+    List<UserTestAnswerV2> findAllByUserIdAndTestV2Id(String userId, String testId);
 
+    List<UserTestAnswerV2> findAllByTestV2Id(String testId);
+
+    List<UserTestAnswerV2> findAllByShouldBeDeletedTrueAndCreatedAtBefore(Timestamp twoHoursAgo);
+
+    @Modifying
+    @Query(value = "DELETE FROM UserTestAnswerSelectedOptionsV2 WHERE test_option_v2_id IN (SELECT id FROM TestOptionV2 WHERE question_v2_id = :questionId)", nativeQuery = true)
+    void deleteAllSelectedOptionsByQuestionId(@Param("questionId") String questionId);
 
 }
