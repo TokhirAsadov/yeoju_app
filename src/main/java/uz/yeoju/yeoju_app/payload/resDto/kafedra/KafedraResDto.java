8f1c99b15b3ac547e885dcf69da4585e3d99b00b
Takhir <guvalakat1603@gmail.com>
2025-11-26 10:44:08 +0500

    new field is send

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/kafedra/KafedraResDto.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/kafedra/KafedraResDto.java
@@ -1,8 +1,13 @@
 package uz.yeoju.yeoju_app.payload.resDto.kafedra;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface KafedraResDto {
     String getId();
     String getName();
     String getRoom();
     String getPhone();
+
+    @Value("#{@kafedraRepository.getKafedraMudiriData(target.id)}")
+    GetKafedraMudiriData getKafedraMudiri();
 }
