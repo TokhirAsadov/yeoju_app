34ad1aa3ba3a8ac0618168eca0dfd765834a9e27
Takhir <guvalakat1603@gmail.com>
2025-11-26 10:43:34 +0500

    GetKafedraMudiriData

---
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/kafedra/GetKafedraMudiriData.java
@@ -0,0 +1,9 @@
+package uz.yeoju.yeoju_app.payload.resDto.kafedra;
+
+public interface GetKafedraMudiriData {
+    String getId();
+    String getFirstName();
+    String getLastName();
+    String getMiddleName();
+    String getFullName();
+}
