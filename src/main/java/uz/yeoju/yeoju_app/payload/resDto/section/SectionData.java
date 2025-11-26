d30dcf9f2a88bebb7fe5e54e50c8e8a03676cc74
Takhir <guvalakat1603@gmail.com>
2025-11-26 15:11:18 +0500

    new field

--- src/main/java/uz/yeoju/yeoju_app/payload/resDto/section/SectionData.java
+++ src/main/java/uz/yeoju/yeoju_app/payload/resDto/section/SectionData.java
@@ -1,9 +1,14 @@
 package uz.yeoju.yeoju_app.payload.resDto.section;
 
+import org.springframework.beans.factory.annotation.Value;
+import uz.yeoju.yeoju_app.payload.resDto.kafedra.GetKafedraMudiriData;
+
 public interface SectionData {
     String getId();
 
     String getName();
 
+    @Value("#{@sectionRepository.getSectionOwner(target.id)}")
+    GetKafedraMudiriData getKafedraMudiri();
 
 }
