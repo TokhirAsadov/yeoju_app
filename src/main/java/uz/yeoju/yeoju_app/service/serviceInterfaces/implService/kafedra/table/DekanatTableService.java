1c279cd1e121492d4fd20292f79f0e269110b28c
Takhir <guvalakat1603@gmail.com>
2025-11-28 14:59:55 +0500

    findByDekanatId()

--- src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/table/DekanatTableService.java
+++ src/main/java/uz/yeoju/yeoju_app/service/serviceInterfaces/implService/kafedra/table/DekanatTableService.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public interface DekanatTableService {
     ApiResponse saveFileToSystem(User user, MultipartHttpServletRequest request, Integer year, String month,String dekanatId, String id);
+    ApiResponse findByDekanatId(String dekanatId);
 
 
 }
