99c4a7fc9992234ce4a4c706edea3d1b9abc6769
Takhir <guvalakat1603@gmail.com>
2025-11-29 10:41:27 +0500

    TableOfDekanat

---
+++ src/main/java/uz/yeoju/yeoju_app/entity/kafedra/TableOfDekanat.java
@@ -0,0 +1,25 @@
+package uz.yeoju.yeoju_app.entity.kafedra;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.yeoju.yeoju_app.entity.dekanat.Dekanat;
+import uz.yeoju.yeoju_app.entity.temp.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TableOfDekanat extends AbsEntity {
+    private Integer year;
+    private String month;
+    @ManyToOne
+    private Dekanat dekanat;
+    private String fileName;
+    private String fileType;
+    private String contentType;
+
+}
