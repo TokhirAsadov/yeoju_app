96a32f547957b62bac732aa5f43fe6ccc6ea63a3
Takhir <guvalakat1603@gmail.com>
2025-11-29 10:47:01 +0500

    TableOfDekanatController

---
+++ src/main/java/uz/yeoju/yeoju_app/controller/kafedra/TableOfDekanatController.java
@@ -0,0 +1,26 @@
+package uz.yeoju.yeoju_app.controller.kafedra;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import uz.yeoju.yeoju_app.controller.BaseUrl;
+import uz.yeoju.yeoju_app.entity.User;
+import uz.yeoju.yeoju_app.entity.kafedra.TableOfKafedra;
+import uz.yeoju.yeoju_app.payload.ApiResponse;
+import uz.yeoju.yeoju_app.secret.CurrentUser;
+import uz.yeoju.yeoju_app.service.serviceInterfaces.implService.kafedra.table.DekanatTableService;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(BaseUrl.BASE_URL+"/dekanat-table")
+@RequiredArgsConstructor
+public class TableOfDekanatController {
+    private final DekanatTableService service;
+
+
+}
