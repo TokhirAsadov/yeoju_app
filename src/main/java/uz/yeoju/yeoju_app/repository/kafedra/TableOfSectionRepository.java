59036e1fddb81c4e9fd40776455395cbb97f2a15
Takhir <guvalakat1603@gmail.com>
2025-11-29 10:42:05 +0500

    TableOfSectionRepository

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfSectionRepository.java
@@ -0,0 +1,10 @@
+package uz.yeoju.yeoju_app.repository.kafedra;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.kafedra.TableOfSection;
+
+import java.util.List;
+
+public interface TableOfSectionRepository extends JpaRepository<TableOfSection,String> {
+
+}
