54ef60dbf5fbc3098c38f7429915937a7fce0c5a
Takhir <guvalakat1603@gmail.com>
2025-11-29 10:43:11 +0500

    TableOfDekanatRepository

---
+++ src/main/java/uz/yeoju/yeoju_app/repository/kafedra/TableOfDekanatRepository.java
@@ -0,0 +1,10 @@
+package uz.yeoju.yeoju_app.repository.kafedra;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.yeoju.yeoju_app.entity.kafedra.TableOfDekanat;
+
+import java.util.List;
+
+public interface TableOfDekanatRepository extends JpaRepository<TableOfDekanat,String> {
+
+}
